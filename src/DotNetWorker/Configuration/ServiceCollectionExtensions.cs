6cb6a52a8a70e75219796980d6ba4f6e34322ba5
sarah <35204912+satvu@users.noreply.github.com>
2020-07-28 17:14:04 -0500

    function instance factory interface (#4)
    add function instance factory interface and a default implementation

--- src/DotNetWorker/Configuration/ServiceCollectionExtensions.cs
+++ src/DotNetWorker/Configuration/ServiceCollectionExtensions.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Azure.Functions.DotNetWorker.Configuration
             services.AddSingleton<IParameterConverter, HttpRequestDataConverter>();
             services.AddSingleton<IParameterConverter, JsonPocoConverter>();
             services.AddSingleton<ParameterConverterManager>();
+            services.AddSingleton<IFunctionInstanceFactory, DefaultFunctionInstanceFactory>();
             services.AddSingleton<FunctionBroker>();
             services.AddSingleton(ch =>
             {
---
+++ src/DotNetWorker/IFunctionInstanceFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Functions.DotNetWorker
+{
+    public interface IFunctionInstanceFactory
+    {
+        object CreateInstance(Type type);
+    }
+}

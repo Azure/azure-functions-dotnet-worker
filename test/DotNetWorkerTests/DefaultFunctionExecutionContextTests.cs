d1d4fe9a615345d7c8ef5807e34e27697ce219a4
Sarah Vu <t-sarvu@microsoft.com>
2020-08-05 10:15:13 -0500

    some initial setup of tests for this pr

---
+++ test/DotNetWorkerTests/DefaultFunctionExecutionContextTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Functions.DotNetWorker;
+using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.DotNetWorkerTests
+{
+    public class DefaultFunctionExecutionContextTests
+    {
+        Mock<IServiceScopeFactory> _mockServiceScopeFactory = new Mock<IServiceScopeFactory>(MockBehavior.Strict);
+        InvocationRequest _invocationRequest;
+        DefaultFunctionExecutionContext _defaultFunctionExecutionContext;
+
+        public DefaultFunctionExecutionContextTests()
+        {
+            _invocationRequest = new InvocationRequest();
+            _defaultFunctionExecutionContext = new DefaultFunctionExecutionContext(_mockServiceScopeFactory.Object, _invocationRequest);
+        }
+
+        [Fact]
+        public void InstanceServicesCreatedSuccessfullyTest()
+        {
+            Mock<IServiceScope> instanceServicesScope = new Mock<IServiceScope>();
+            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
+            instanceServicesScope.SetupAllProperties();
+            instanceServicesScope.Setup(p => p.ServiceProvider).Returns(serviceProvider.Object);
+            _mockServiceScopeFactory.Setup(f => f.CreateScope()).Returns(instanceServicesScope.Object);
+            var services = _defaultFunctionExecutionContext.InstanceServices;
+            Assert.NotNull(services);
+        }
+
+        [Fact]
+        public void InstanceServicesDisposedSuccessfullyTest()
+        {
+            _defaultFunctionExecutionContext.Dispose();
+        }
+    }
+
+}
---
+++ test/DotNetWorkerTests/FunctionBrokerTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Xunit;
+using Microsoft.Azure.Functions.DotNetWorker;
+using Microsoft.Azure.Functions.DotNetWorker.Pipeline;
+using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Functions.DotNetWorkerTests
+{
+    public class FunctionBrokerTests
+    {
+        FunctionBroker _functionBroker;
+        Mock<FunctionExecutionDelegate> _mockFunctionExecutionDelegate = new Mock<FunctionExecutionDelegate>();
+        Mock<IFunctionExecutionContextFactory> _mockFunctionExecutionContextFactory = new Mock<IFunctionExecutionContextFactory>();
+
+        public FunctionBrokerTests()
+        {
+            _functionBroker = new FunctionBroker(_mockFunctionExecutionDelegate.Object, _mockFunctionExecutionContextFactory.Object);
+        }
+
+        [Fact]
+        public async void DisposeContextSuccessfullyTest()
+        {
+            InvocationRequest invocationRequest = new InvocationRequest();
+            _mockFunctionExecutionDelegate.Setup(p => p(It.IsAny<FunctionExecutionContext>())).Returns(Task.CompletedTask);
+            var result = await _functionBroker.InvokeAsync(invocationRequest);
+        }
+
+    }
+}

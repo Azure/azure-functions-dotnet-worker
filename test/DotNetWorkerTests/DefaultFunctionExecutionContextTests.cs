5ccea58ddc92bb6a4d11a27fe5945cbff48aee08
Sarah Vu <t-sarvu@microsoft.com>
2020-08-05 12:23:27 -0500

    function execution context tests

--- test/DotNetWorkerTests/DefaultFunctionExecutionContextTests.cs
+++ test/DotNetWorkerTests/DefaultFunctionExecutionContextTests.cs
@@ -10,33 +10,54 @@ namespace Microsoft.Azure.Functions.DotNetWorkerTests
 {
     public class DefaultFunctionExecutionContextTests
     {
-        Mock<IServiceScopeFactory> _mockServiceScopeFactory = new Mock<IServiceScopeFactory>(MockBehavior.Strict);
         InvocationRequest _invocationRequest;
         DefaultFunctionExecutionContext _defaultFunctionExecutionContext;
+        IServiceScopeFactory _serviceScopeFactory;
+        IServiceProvider _serviceProvider;
 
         public DefaultFunctionExecutionContextTests()
         {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<SingletonService>();
+            serviceCollection.AddTransient<TransientService>();
+            serviceCollection.AddScoped<ScopedService>();
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+            _serviceScopeFactory = _serviceProvider.GetService<IServiceScopeFactory>();
             _invocationRequest = new InvocationRequest();
-            _defaultFunctionExecutionContext = new DefaultFunctionExecutionContext(_mockServiceScopeFactory.Object, _invocationRequest);
+            _defaultFunctionExecutionContext = new DefaultFunctionExecutionContext(_serviceScopeFactory, _invocationRequest);
         }
 
         [Fact]
         public void InstanceServicesCreatedSuccessfullyTest()
         {
-            Mock<IServiceScope> instanceServicesScope = new Mock<IServiceScope>();
-            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
-            instanceServicesScope.SetupAllProperties();
-            instanceServicesScope.Setup(p => p.ServiceProvider).Returns(serviceProvider.Object);
-            _mockServiceScopeFactory.Setup(f => f.CreateScope()).Returns(instanceServicesScope.Object);
             var services = _defaultFunctionExecutionContext.InstanceServices;
             Assert.NotNull(services);
+            Assert.NotNull(services.GetService<SingletonService>());
+            Assert.NotNull(services.GetService<TransientService>());
+            Assert.NotNull(services.GetService<ScopedService>());
         }
 
         [Fact]
         public void InstanceServicesDisposedSuccessfullyTest()
         {
+            var services = _defaultFunctionExecutionContext.InstanceServices;
             _defaultFunctionExecutionContext.Dispose();
+            Assert.Throws<ObjectDisposedException>(services.GetService<SingletonService>);
+            Assert.Throws<ObjectDisposedException>(services.GetService<TransientService>);
+            Assert.Throws<ObjectDisposedException>(services.GetService<ScopedService>);
         }
-    }
 
+        // service classes for testing
+        private class SingletonService : IDisposable
+        {
+            public SingletonService() { }
+            public bool IsDisposed { get; private set; }
+            public void Dispose()
+            {
+                IsDisposed = true;
+            }
+        }
+        private class TransientService : SingletonService { }
+        private class ScopedService : SingletonService { }
+    }
 }

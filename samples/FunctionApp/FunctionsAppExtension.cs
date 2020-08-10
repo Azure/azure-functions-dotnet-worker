b1b20f301971cf9b3a35f8f199baddc6627cb1eb
Sarah Vu <t-sarvu@microsoft.com>
2020-08-10 14:53:09 -0500

    wip extension

---
+++ samples/FunctionApp/FunctionsAppExtension.cs
@@ -0,0 +1,16 @@
+﻿using FunctionApp;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+
+namespace FunctionApp
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //builder.Services.AddSingleton<IFunctionProvider, function>();
+        }
+    }
+}

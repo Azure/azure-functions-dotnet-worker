eaecf14528ffe5d45a0de384e7cac29b5f85eaa2
Sarah Vu <t-sarvu@microsoft.com>
2020-08-11 23:28:40 -0500

    wip generation function metadata

---
+++ SourceGenerator/AttributeExtensions.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace SourceGenerator
+{
+    internal static class AttributeExtensions
+    {
+        /// <summary>
+        /// {Name}Attribute -> name
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <returns></returns>
+        public static string ToAttributeFriendlyName(this Attribute attribute)
+        {
+            return ToAttributeFriendlyName(attribute.GetType().Name);
+        }
+
+        public static string ToAttributeFriendlyName(this CustomAttribute attribute)
+        {
+            return ToAttributeFriendlyName(attribute.AttributeType.Name);
+        }
+
+        private static string ToAttributeFriendlyName(string name)
+        {
+            const string suffix = nameof(Attribute);
+            name = name.Substring(0, name.Length - suffix.Length);
+            return name.ToLowerFirstCharacter();
+        }
+
+        private static readonly HashSet<string> _supportedAttributes = new HashSet<string>
+         {
+             "BlobTriggerAttribute",
+             "QueueTriggerAttribute",
+             "EventHubTriggerAttribute",
+             "TimerTriggerAttribute",
+             "ServiceBusTriggerAttribute",
+             "CosmosDBTriggerAttribute"
+         };
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <returns></returns>
+        public static bool IsWebJobsAttribute(this CustomAttribute attribute)
+        {
+            return attribute.AttributeType.Resolve().CustomAttributes.Any(a => a.AttributeType.FullName == "Microsoft.Azure.WebJobs.Description.BindingAttribute")
+                || _supportedAttributes.Contains(attribute.AttributeType.FullName);
+        }
+
+        /// <summary>
+        /// For every binding (which is what the returned JObject represents) there are 3 special keys:
+        ///     "name" -> that is the parameter name, not set by this function
+        ///     "type" -> that is the binding type. This is derived from the Attribute.Name itself. <see cref="AttributeExtensions.ToAttributeFriendlyName(Attribute)"/>
+        /// a side from these 3, all the others are direct serialization of all of the attribute's properties.
+        /// The mapping however isn't 1:1 in terms of the naming. Therefore, <see cref="NormalizePropertyName(string, PropertyInfo)"/>
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <returns></returns>
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="attribute"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public static T GetValue<T>(this Attribute attribute, string propertyName)
+        {
+            var property = attribute.GetType().GetProperty(propertyName);
+            if (property != null)
+            {
+                return (T)property.GetValue(attribute);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
+        public static void SetValue(this Attribute attribute, string propertyName, object propertyValue)
+        {
+            var property = attribute.GetType().GetProperty(propertyName);
+            if (property != null)
+            {
+                property.SetValue(attribute, propertyValue);
+            }
+        }
+
+        private static bool TryGetPropertyValue(PropertyInfo property, object propertyValue, out string value)
+        {
+            value = null;
+#if NET46
+            if (property.PropertyType.IsEnum)
+#else
+            if (property.PropertyType.GetTypeInfo().IsEnum)
+#endif
+            {
+                value = Enum.GetName(property.PropertyType, propertyValue).ToLowerFirstCharacter();
+                return true;
+            }
+            return false;
+        }
+
+        private static void CheckIfPropertyIsSupported(string attributeName, PropertyInfo property)
+        {
+            var propertyName = property.Name;
+            if (attributeName == "TimerTriggerAttribute")
+            {
+                if (propertyName == "ScheduleType")
+                {
+                    throw new NotImplementedException($"Property '{propertyName}' on attribute '{attributeName}' is not supported in Azure Functions.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// These exceptions are coming from how the script runtime is reading function.json
+        /// See https://github.com/Azure/azure-webjobs-sdk-script/tree/dev/src/WebJobs.Script/Binding
+        /// If there are no exceptions for a given property name on a given attribute, then return it's name with a lowerCase first character.
+        /// </summary>
+        /// <param name="attributeName"></param>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        private static string NormalizePropertyName(Attribute attribute, PropertyInfo property)
+        {
+            var attributeName = attribute.GetType().Name;
+            var propertyName = property.Name;
+
+            if (attributeName == "BlobTriggerAttribute")
+            {
+                if (propertyName == "BlobPath")
+                {
+                    return "path";
+                }
+            }
+            else if (attributeName == "EventHubTriggerAttribute" &&
+                attribute.GetType().Assembly.GetName().Version.Major == 2)
+            {
+                if (propertyName == "EventHubName")
+                {
+                    return "path";
+                }
+            }
+            else if (attributeName == "ServiceBusTriggerAttribute")
+            {
+                if (propertyName == "Access")
+                {
+                    return "accessRights";
+                }
+            }
+            else if (attributeName == "TimerTriggerAttribute")
+            {
+                if (propertyName == "ScheduleExpression")
+                {
+                    return "schedule";
+                }
+            }
+            else if (attributeName == "ApiHubFileTrigger")
+            {
+                if (propertyName == "ConnectionStringSetting")
+                {
+                    return "connection";
+                }
+            }
+
+            return propertyName.ToLowerFirstCharacter();
+        }
+    }
+}
---
+++ SourceGenerator/MethodInfoExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace SourceGenerator
+{
+    internal static class MethodInfoExtensions
+    {
+        /// <summary>
+        /// A method is an SDK method if it has a FunctionNameAttribute AND at least one parameter has an SDK attribute or the method has a NoAutomaticTriggerAttribute.
+        /// </summary>
+        /// <param name="method">method to check if an SDK method or not.</param>
+        /// <returns>true if <paramref name="method"/> is a WebJobs SDK method. False otherwise.</returns>
+        public static bool IsWebJobsSdkMethod(this MethodDefinition method)
+        {
+            return method.HasFunctionNameAttribute() && method.HasValidWebJobSdkTriggerAttribute();
+        }
+
+        public static bool HasValidWebJobSdkTriggerAttribute(this MethodDefinition method)
+        {
+            var hasNoAutomaticTrigger = method.HasNoAutomaticTriggerAttribute();
+            var hasTrigger = method.HasTriggerAttribute();
+            return (hasNoAutomaticTrigger || hasTrigger) && !(hasNoAutomaticTrigger && hasTrigger);
+        }
+
+        public static bool HasFunctionNameAttribute(this MethodDefinition method)
+        {
+            return method.CustomAttributes.FirstOrDefault(d => d.AttributeType.FullName == "Microsoft.Azure.WebJobs.FunctionNameAttribute") != null;
+        }
+
+        public static bool HasNoAutomaticTriggerAttribute(this MethodDefinition method)
+        {
+            return method.CustomAttributes.FirstOrDefault(a => a.AttributeType.FullName == "Microsoft.Azure.WebJobs.NoAutomaticTriggerAttribute") != null;
+        }
+
+        public static bool HasTriggerAttribute(this MethodDefinition method)
+        {
+            return method.Parameters.Any(p => p.IsWebJobSdkTriggerParameter());
+        }
+
+        /// <summary>
+        /// Gets a function name from a <paramref name="method"/>
+        /// </summary>
+        /// <param name="method">method has to be a WebJobs SDK method. <see cref="IsWebJobsSdkMethod(MethodInfo)"/></param>
+        /// <returns>Function name.</returns>
+        public static string GetSdkFunctionName(this MethodDefinition method)
+        {
+            if (!method.IsWebJobsSdkMethod())
+            {
+                throw new ArgumentException($"{nameof(method)} has to be a WebJob SDK function");
+            }
+
+            string functionName = method.CustomAttributes.FirstOrDefault(a => a.AttributeType.Name == "FunctionNameAttribute")?.ConstructorArguments[0].Value.ToString();
+            if (functionName != null)
+            {
+                return functionName;
+            }
+            else
+            {
+                throw new InvalidOperationException("Missing FunctionNameAttribute");
+            }
+        }
+
+        /// <summary>
+        /// A method is disabled is any of it's parameters have [Disabled] attribute
+        /// or if the method itself or class have the [Disabled] attribute. The overloads
+        /// are stringified so that the ScriptHost will do its job.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns>a boolean true or false if the outcome is fixed, a string if the ScriptHost should interpret it</returns>
+        public static object GetDisabled(this MethodDefinition method)
+        {
+            var customAttribute = method.Parameters.Select(p => p.GetDisabledAttribute()).Where(a => a != null).FirstOrDefault() ??
+                method.GetDisabledAttribute() ??
+                method.DeclaringType.GetDisabledAttribute();
+
+            if (customAttribute != null)
+            {
+                var attribute = customAttribute.ToReflection();
+
+                // With a SettingName defined, just put that as string. The ScriptHost will evaluate it.
+                var settingName = attribute.GetValue<string>("SettingName");
+                if (!string.IsNullOrEmpty(settingName))
+                {
+                    return settingName;
+                }
+
+                var providerType = attribute.GetValue<Type>("ProviderType");
+                if (providerType != null)
+                {
+                    return providerType;
+                }
+
+                // With neither settingName or providerType, no arguments were given and it should always be true
+
+                return true;
+            }
+
+            // No attribute means not disabled
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        public static CustomAttribute GetDisabledAttribute(this MethodDefinition method)
+        {
+            return method.CustomAttributes.FirstOrDefault(a => a.AttributeType.FullName == "Microsoft.Azure.WebJobs.DisableAttribute");
+        }
+
+        /// <summary>
+        /// A method has an unsupported attributes if it has any of the following:
+        ///     1) [Disabled("%settingName%")]
+        ///     2) [Disabled(typeof(TypeName))]
+        /// However this [Disabled("settingName")] is valid.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static bool HasUnsuportedAttributes(this MethodDefinition method, out string error)
+        {
+            error = string.Empty;
+            var disabled = method.GetDisabled();
+            if (disabled is string disabledStr &&
+                disabledStr.StartsWith("%") &&
+                disabledStr.EndsWith("%"))
+            {
+                error = "'%' expressions are not supported for 'Disable'. Use 'Disable(\"settingName\") instead of 'Disable(\"%settingName%\")'";
+                return true;
+            }
+            else if (disabled is Type)
+            {
+                error = "the constructor 'DisableAttribute(Type)' is not supported.";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

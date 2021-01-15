7c671a784035ab1295576eee683171d898b4abc3
Brett Samblanet <brettsam@microsoft.com>
2021-01-15 12:32:45 -0800

    fixing packing

--- sdk/Sdk/Abstractions/SdkFunctionMetadata.cs
+++ sdk/Sdk/Abstractions/SdkFunctionMetadata.cs
@@ -7,15 +7,15 @@ namespace Microsoft.Azure.Functions.Worker.Sdk
     // Copy of FunctionMetadata, but using internal type to simplify dependencies.
     internal class SdkFunctionMetadata
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string ScriptFile { get; set; }
+        public string? ScriptFile { get; set; }
 
-        public string FunctionDirectory { get; set; }
+        public string? FunctionDirectory { get; set; }
 
-        public string EntryPoint { get; set; }
+        public string? EntryPoint { get; set; }
 
-        public string Language { get; set; }
+        public string? Language { get; set; }
 
         public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
 

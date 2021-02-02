e0d910bb5509d1fef59c0b4d0170fbc351485b6d
Brett Samblanet <brettsam@microsoft.com>
2021-02-02 07:46:02 -0800

    storage

--- tools/start-emulators.ps1
+++ tools/start-emulators.ps1
@@ -1,6 +1,9 @@
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
 
-Write-Host "Starting CosmosDB Emulator"
-Start-CosmosDbEmulator
+Write-Host "Starting CosmosDB emulator with -NoWait"
+Start-CosmosDbEmulator -NoWait
 $cosmosStatus = Get-CosmosDbEmulatorStatus
 Write-Host "Cosmos status: $cosmosStatus"
+
+Write-Host "Starting Storage emulator"
+AzureStorageEmulator.exe start

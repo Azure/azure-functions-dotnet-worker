f3727843a20a85ce7aa45aa5b354d7d7ed0ff48c
Marie Hoeger <marie.hoeger@gmail.com>
2021-02-17 09:36:03 -0800

    E2E Http Tests (#135)
    * Added basic HTTP tests
    
    add scenario with empty
    
    Update name
    
    fix typo
    
    * remove nuget.config file

---
+++ test/E2ETests/E2EApps/E2EApp/Http/FailingHttpFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using Grpc.Core;
+using Microsoft.Azure.Functions.Worker.Pipeline;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.E2EApp
+{
+    public static class FailingHttpFunctions
+    {
+        [FunctionName(nameof(ExceptionFunction))]
+        public static HttpResponseData ExceptionFunction(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionExecutionContext context)
+        {
+            context.Logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+            throw new Exception("This should never succeed!");
+        }
+    }
+}

b6599c91d674968761ef2e85afe52f52f0eb0b64
Marie Hoeger <mahoeger@microsoft.com>
2021-02-16 18:44:35 -0800

    Added basic HTTP tests
    add scenario with empty
    
    Update name
    
    fix typo

---
+++ test/E2ETests/E2EApps/E2EApp/Http/BasicHttpFunctions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using Grpc.Core;
+using Microsoft.Azure.Functions.Worker.Pipeline;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.E2EApp
+{
+    public static class BasicHttpFunctions
+    {
+        [FunctionName(nameof(HelloFromQuery))]
+        public static HttpResponseData HelloFromQuery(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionExecutionContext context)
+        {
+            context.Logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+            var queryName = HttpUtility.ParseQueryString(req.Url.Query)["name"];
+            
+            if (!string.IsNullOrEmpty(queryName))
+            {
+                var response = new HttpResponseData(HttpStatusCode.OK);
+                response.Body = "Hello " + queryName;
+                return response;
+            }
+            else
+            {
+                return new HttpResponseData(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [FunctionName(nameof(HelloFromJsonBody))]
+        public static HttpResponseData HelloFromJsonBody(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionExecutionContext context)
+        {
+            context.Logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+            var body = Encoding.UTF8.GetString(req.Body.Value.Span);
+
+            if (!string.IsNullOrEmpty(body))
+            {
+                var serliazedBody = (CallerName)JsonSerializer.Deserialize(body, typeof(CallerName));
+                var response = new HttpResponseData(HttpStatusCode.OK);
+                response.Body = "Hello " + serliazedBody.Name;
+                return response;
+            }
+            else
+            {
+                return new HttpResponseData(HttpStatusCode.BadRequest);
+            }
+        }
+        
+        public class CallerName
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ test/E2ETests/E2ETests/HttpEndToEndTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.Tests.E2ETests
+{
+    [Collection(Constants.FunctionAppCollectionName)]
+    public class HttpEndToEndTests
+    {
+        private readonly FunctionAppFixture _fixture;
+
+        public HttpEndToEndTests(FunctionAppFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Theory]
+        [InlineData("HelloFromQuery", "?name=Test", HttpStatusCode.OK, "Hello Test")]
+        [InlineData("HelloFromQuery", "?name=John&lastName=Doe", HttpStatusCode.OK, "Hello John")]
+        [InlineData("HelloFromQuery", "?emptyProperty=&name=Jane", HttpStatusCode.OK, "Hello Jane")]
+        [InlineData("ExceptionFunction", "", HttpStatusCode.InternalServerError, "")]
+        [InlineData("HelloFromQuery", "", HttpStatusCode.BadRequest, "")]
+        public async Task HttpTriggerTests(string functionName, string queryString, HttpStatusCode expectedStatusCode, string expectedMessage)
+        {
+            HttpResponseMessage response = await HttpHelpers.InvokeHttpTrigger(functionName, queryString);
+            string actualMessage = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+
+            if (!string.IsNullOrEmpty(expectedMessage))
+            {
+                Assert.False(string.IsNullOrEmpty(actualMessage));
+                Assert.Contains(expectedMessage, actualMessage);
+            }
+        }
+
+        [Theory]
+        [InlineData("HelloFromJsonBody", "{\"Name\": \"Whitney\"}", "application/json", HttpStatusCode.OK, "Hello Whitney")]
+        [InlineData("HelloFromJsonBody", "{\"Name\": \"麵🍜\"}", "application/json", HttpStatusCode.OK, "Hello 麵🍜")]
+        [InlineData("HelloFromJsonBody", "{\"Name\": \"Bob\"}", "application/octet-stream", HttpStatusCode.OK, "Hello Bob")]
+        public async Task HttpTriggerTestsMediaTypeDoNotMatter(string functionName, string body, string mediaType, HttpStatusCode expectedStatusCode, string expectedBody)
+        {
+            HttpResponseMessage response = await HttpHelpers.InvokeHttpTriggerWithBody(functionName, body, expectedStatusCode, mediaType);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+            Assert.Equal(expectedBody, responseBody);
+        }
+
+        [Fact(Skip = "TODO: https://github.com/Azure/azure-functions-dotnet-worker/issues/133")]
+        public async Task HttpTriggerWithCookieTests()
+        {
+            HttpResponseMessage response = await HttpHelpers.InvokeHttpTrigger("HttpTriggerSetsCookie");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            List<string> cookies = response.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value.ToList();
+        }
+
+        [Fact (Skip = "TODO: https://github.com/Azure/azure-functions-dotnet-worker/issues/134")]
+        public async Task HttpTriggerBindingDataTests()
+        {
+
+        }
+    }
+}

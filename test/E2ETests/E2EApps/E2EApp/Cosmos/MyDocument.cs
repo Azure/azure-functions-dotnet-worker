6057a99d9da7eaab80fac0ffa0e06fec6c76aa13
Brett Samblanet <brettsam@microsoft.com>
2021-02-18 11:40:56 -0800

    updating serialization for e2e

--- test/E2ETests/E2EApps/E2EApp/Cosmos/MyDocument.cs
+++ test/E2ETests/E2EApps/E2EApp/Cosmos/MyDocument.cs
@@ -1,16 +1,22 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-﻿namespace Microsoft.Azure.Functions.Worker.E2EApp.Cosmos
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Azure.Functions.Worker.E2EApp.Cosmos
 {
     public class MyDocument
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
 
+        [JsonPropertyName("text")]
         public string Text { get; set; }
 
+        [JsonPropertyName("number")]
         public int Number { get; set; }
 
+        [JsonPropertyName("boolean")]
         public bool Boolean { get; set; }
     }
 }

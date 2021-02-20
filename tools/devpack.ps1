6337ee74436811447f630caae41a8188f1a50750
Brett Samblanet <brettsam@microsoft.com>
2021-02-20 08:05:04 -0800

    updating devpack for more output

--- tools/devpack.ps1
+++ tools/devpack.ps1
@@ -4,7 +4,6 @@
     $E2E
 )
 
-
 # Packs the SDK locally, and (by default) updates the Sample to use this package, then builds.
 # Specify --E2E to instead target the E2E test app.
 
@@ -20,12 +19,12 @@ $localPack = "$rootPath\local"
 Write-Host
 Write-Host "---Updating project with local SDK pack---"
 Write-Host "Packing SDK to $localPack"
-& "dotnet" "pack" "$rootPath\sdk\sdk\Sdk.csproj" "-v" "q" "-o" "$localPack" "-nologo"
+& "dotnet" "pack" "$rootPath\sdk\sdk\Sdk.csproj" "-o" "$localPack" "-nologo"
 Write-Host
 Write-Host "Updating SDK package reference in $project"
 & "dotnet" "remove" $project "package" "Microsoft.Azure.Functions.Worker.Sdk"
 & "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker.Sdk" "-s" "$localPack" "--prerelease"
 Write-Host
 Write-Host "Building $project"
-& "dotnet" "build" $project "-v" "q" "-nologo"
+& "dotnet" "build" $project "-nologo"
 Write-Host "------"

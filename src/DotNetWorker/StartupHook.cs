e1a81a8ae6844341ef8e646f477dd5dcc4353700
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-03-01 17:45:04 -0800

    Adding debugger startup hook

---
+++ src/DotNetWorker/StartupHook.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+/// <summary>
+/// This code is called by the .NET infrastructure the following MUST remain unchanged:
+///  - The type name must be StartupHook
+///  - The type must be at the top level, with no namespaces
+///  - The type must be internal
+///  - The initialization method must be named Initialize
+///  - The initialization method must be static, with a void return.
+///  For more information, see: https://github.com/dotnet/core-setup/blob/master/Documentation/design-docs/host-startup-hook.md
+/// </summary>
+internal class StartupHook
+{
+    public static void Initialize()
+    {
+        // Time to wait between checks, in ms.
+        const int SleepTime = 500;
+        const int MaxWaitCycles = (60 * 1000) / SleepTime;
+
+        Console.WriteLine("Azure Functions .NET Worker initialized in debug mode. Waiting for debugger to attach...");
+
+        static bool WaitOnDebugger(int cycle)
+        {
+            if (Debugger.IsAttached)
+            {
+                return false;
+            }
+
+            if (cycle > MaxWaitCycles)
+            {
+                Console.WriteLine("A debugger was not attached within the expected time limit. The process will continue without a debugger.");
+
+                return false;
+            }
+
+            return true;
+        }
+
+        for (int i = 0; WaitOnDebugger(i); i++)
+        {
+            Thread.Sleep(SleepTime);
+        }
+    }
+}
---
+++ test/DotNetWorkerTests/StartupHookTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.Worker.Tests
+{
+    public class StartupHookTests
+    {
+        [Fact]
+        public void ValidadeHookSetup()
+        {
+            var hookType = typeof(StartupHook);
+            var initializeMethod = hookType.GetMethod("Initialize", BindingFlags.Static | BindingFlags.Public);
+
+            Assert.Equal("StartupHook", hookType.Name);
+            Assert.Equal("StartupHook", hookType.FullName);
+            Assert.Null(hookType.Namespace);
+            Assert.False(hookType.IsVisible);
+            Assert.Equal("Microsoft.Azure.Functions.Worker", hookType.Assembly.GetName().Name);
+            Assert.NotNull(initializeMethod);
+            Assert.Empty(initializeMethod.GetParameters());
+            Assert.Equal(typeof(void), initializeMethod.ReturnType);
+        }
+    }
+}

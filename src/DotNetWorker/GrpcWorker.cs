c5639fd722716cde4494cc7393903841ea130f3e
Brett Samblanet <brettsam@microsoft.com>
2021-03-03 17:33:41 -0800

    moving grpc usage out of hostedservice (#212)

--- src/DotNetWorker/GrpcWorker.cs
+++ src/DotNetWorker/GrpcWorker.cs
@@ -2,14 +2,17 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Microsoft.Azure.Functions.Worker.Context;
 using Microsoft.Azure.Functions.Worker.Context.Features;
 using Microsoft.Azure.Functions.Worker.Grpc.Messages;
 using Microsoft.Azure.Functions.Worker.Invocation;
 using Microsoft.Azure.Functions.Worker.OutputBindings;
-using static Microsoft.Azure.Functions.Worker.Grpc.Messages.StatusResult.Types;
+using Microsoft.Extensions.Options;
+using static Microsoft.Azure.Functions.Worker.Grpc.Messages.FunctionRpc;
 using MsgType = Microsoft.Azure.Functions.Worker.Grpc.Messages.StreamingMessage.ContentOneofCase;
 
 
@@ -17,26 +20,81 @@ namespace Microsoft.Azure.Functions.Worker
 {
     internal class GrpcWorker : IWorker
     {
+        private readonly ChannelReader<StreamingMessage> _outputReader;
         private readonly ChannelWriter<StreamingMessage> _outputWriter;
+
         private readonly IFunctionsApplication _application;
+        private readonly FunctionRpcClient _rpcClient;
         private readonly IInvocationFeaturesFactory _invocationFeaturesFactory;
         private readonly IOutputBindingsInfoProvider _outputBindingsInfoProvider;
         private readonly IMethodInfoLocator _methodInfoLocator;
+        private readonly IOptions<GrpcWorkerStartupOptions> _startupOptions;
+        private Task? _writerTask;
+        private Task? _readerTask;
 
-        public GrpcWorker(IFunctionsApplication application, FunctionsHostOutputChannel outputChannel, IInvocationFeaturesFactory invocationFeaturesFactory,
-            IOutputBindingsInfoProvider outputBindingsInfoProvider, IMethodInfoLocator methodInfoLocator)
+        public GrpcWorker(IFunctionsApplication application, FunctionRpcClient rpcClient, FunctionsHostOutputChannel outputChannel, IInvocationFeaturesFactory invocationFeaturesFactory,
+            IOutputBindingsInfoProvider outputBindingsInfoProvider, IMethodInfoLocator methodInfoLocator, IOptions<GrpcWorkerStartupOptions> startupOptions)
         {
             if (outputChannel == null)
             {
                 throw new ArgumentNullException(nameof(outputChannel));
             }
 
+            _outputReader = outputChannel.Channel.Reader;
             _outputWriter = outputChannel.Channel.Writer;
 
             _application = application ?? throw new ArgumentNullException(nameof(application));
+            _rpcClient = rpcClient ?? throw new ArgumentNullException(nameof(rpcClient));
             _invocationFeaturesFactory = invocationFeaturesFactory ?? throw new ArgumentNullException(nameof(invocationFeaturesFactory));
             _outputBindingsInfoProvider = outputBindingsInfoProvider ?? throw new ArgumentNullException(nameof(outputBindingsInfoProvider));
             _methodInfoLocator = methodInfoLocator ?? throw new ArgumentNullException(nameof(methodInfoLocator));
+            _startupOptions = startupOptions ?? throw new ArgumentNullException(nameof(startupOptions));
+        }
+
+        public Task StartAsync(CancellationToken token)
+        {
+            var eventStream = _rpcClient.EventStream();
+
+            _writerTask = StartWriterAsync(eventStream.RequestStream);
+            _readerTask = StartReaderAsync(eventStream.ResponseStream);
+
+            return SendStartStreamMessageAsync(eventStream.RequestStream);
+        }
+
+        public Task StopAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendStartStreamMessageAsync(IClientStreamWriter<StreamingMessage> requestStream)
+        {
+            StartStream str = new StartStream()
+            {
+                WorkerId = _startupOptions.Value.WorkerId
+            };
+
+            StreamingMessage startStream = new StreamingMessage()
+            {
+                StartStream = str
+            };
+
+            await requestStream.WriteAsync(startStream);
+        }
+
+        public async Task StartWriterAsync(IClientStreamWriter<StreamingMessage> requestStream)
+        {
+            await foreach (StreamingMessage rpcWriteMsg in _outputReader.ReadAllAsync())
+            {
+                await requestStream.WriteAsync(rpcWriteMsg);
+            }
+        }
+
+        public async Task StartReaderAsync(IAsyncStreamReader<StreamingMessage> responseStream)
+        {
+            while (await responseStream.MoveNext())
+            {
+                await ProcessRequestAsync(responseStream.Current);
+            }
         }
 
         public Task ProcessRequestAsync(StreamingMessage request)
@@ -107,7 +165,7 @@ namespace Microsoft.Azure.Functions.Worker
             var response = new FunctionLoadResponse
             {
                 FunctionId = loadRequest.FunctionId,
-                Result = new StatusResult { Status = Status.Success }
+                Result = new StatusResult { Status = StatusResult.Types.Status.Success }
             };
 
             var responseMessage = new StreamingMessage

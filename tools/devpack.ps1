4b2f2af68d33eaf87ca3eb0073b9a76a13d1e4f2
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-03-06 22:25:08 -0800

    Nuget.Config

--- tools/devpack.ps1
+++ tools/devpack.ps1
@@ -64,8 +64,11 @@ Write-Host
 Write-Host "Adding SDK package version $version to $project"
 & "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker.Sdk" "-v" $version "-s" $localPack "-n"
 Write-Host
-Copy-Item "Nuget.Config" "$localPack/nuget.config"
-& "dotnet" "nuget" "add" "source" $localPack "--name" "local" "--configfile" "$localPack/nuget.config"
+$configFile = Split-Path "$project"
+$configFile += "/nuget.config"
+Write-Host "Config file name" $configFile
+& "dotnet" "nuget" "add" "source" $localPack "--name" "local" "--configfile" "$configFile"
 Write-Host "Building $project"
+
 & "dotnet" "build" $project "-nologo" "-p:TestBuild=true"
 Write-Host "------"

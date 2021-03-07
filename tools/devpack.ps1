80127a5c0bbe3b1a7aff8ee8283b5e27acc260bb
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-03-06 20:13:09 -0800

    Script update

--- tools/devpack.ps1
+++ tools/devpack.ps1
@@ -37,28 +37,33 @@ Write-Host "---Updating project with local SDK pack---"
 Write-Host "Packing Core .NET Worker projects to $localPack"
 & "dotnet" "pack" $sdkProject "-o" "$localPack" "-nologo" $AdditionalPackArgs
 Write-Host
+
 Write-Host "Removing SDK package reference in $project"
 & "dotnet" "remove" $project "package" "Microsoft.Azure.Functions.Worker.Sdk"
 Write-Host
+
 Write-Host "Removing Worker package reference in $project"
 & "dotnet" "remove" $project "package" "Microsoft.Azure.Functions.Worker"
 Write-Host
+
 Write-Host "Finding latest local Worker package in $localPack"
 $package = Find-Package Microsoft.Azure.Functions.Worker -Source $localPack
 $version = $package.Version
 Write-Host "Found $version"
 Write-Host
+
 Write-Host "Adding Worker package version $version to $project"
-& "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker" "-v" $version "-s" $localPack
+& "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker" "-v" $version "-s" $localPack "-n"
 Write-Host
+
 Write-Host "Finding latest local SDK package in $localPack"
-$package = Find-Package Microsoft.Azure.Functions.Worker.Sdk -Source $localPack
+$package = Find-Package "Microsoft.Azure.Functions.Worker.Sdk" -Source $localPack
 $version = $package.Version
 Write-Host "Found $version"
 Write-Host
 Write-Host "Adding SDK package version $version to $project"
-& "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker.Sdk" "-v" $version "-s" $localPack
+& "dotnet" "add" $project "package" "Microsoft.Azure.Functions.Worker.Sdk" "-v" $version "-s" $localPack "-n"
 Write-Host
 Write-Host "Building $project"
-& "dotnet" "build" $project "-nologo" "-p:TestBuild=true"
+& "dotnet" "build" $project "-nologo" "-p:TestBuild=true" "--source" $localPack
 Write-Host "------"

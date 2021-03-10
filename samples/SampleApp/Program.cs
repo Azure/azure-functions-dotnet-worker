92e14041dbefc5a2f006af6f1d8eb4ef669bf1b2
Brett Samblanet <brettsam@microsoft.com>
2021-03-10 06:09:57 -0800

    Update samples/SampleApp/Program.cs

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace SampleApp

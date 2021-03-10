de8dfaf660589874aea6240b466f318bfbc629f9
Ankit Kumar <kumar.ankit55@gmail.com>
2021-03-10 06:27:18 -0800

    Update samples and README (#272)

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -1,38 +1,24 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker.Configuration;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace SampleApp
 {
-    class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static void Main()
         {
-//  #if DEBUG
-//              Debugger.Launch();
-//  #endif
             var host = new HostBuilder()
-                .ConfigureAppConfiguration(c =>
-                {
-                    c.AddCommandLine(args);
-                })
-                .ConfigureFunctionsWorker((c, b) =>
-                {
-                    b.UseFunctionExecutionMiddleware();
-                })
+                .ConfigureFunctionsWorkerDefaults()
                 .ConfigureServices(s =>
                 {
                     s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
                 })
                 .Build();
 
-            await host.RunAsync();
+            host.Run();
         }
     }
 }

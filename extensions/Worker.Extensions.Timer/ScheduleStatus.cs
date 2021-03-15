2a4799f49874e8aed5761dd3dbd3da35e2788251
Brett Samblanet <brettsam@microsoft.com>
2021-03-15 12:09:21 -0700

    adding TimerInfo and e2e Timer test

---
+++ extensions/Worker.Extensions.Timer/ScheduleStatus.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    /// <summary>
+    /// Represents a timer schedule status.
+    /// </summary>
+    public class ScheduleStatus
+    {
+        /// <summary>
+        /// Gets or sets the last recorded schedule occurrence.
+        /// </summary>
+        public DateTime Last { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expected next schedule occurrence.
+        /// </summary>
+        public DateTime Next { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last time this record was updated. This is used to re-calculate Next
+        /// with the current Schedule after a host restart.
+        /// </summary>
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ test/E2ETests/E2EApps/E2EApp/Timer/TimerTestFunctions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.E2EApp.Queue
+{
+    public class TimerTestFunctions
+    {
+        private readonly ILogger<TimerTestFunctions> _logger;
+
+        public TimerTestFunctions(ILogger<TimerTestFunctions> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(TimerTrigger))]
+        public void TimerTrigger([TimerTrigger("0 * * * * *", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            var info = JsonSerializer.Serialize(timerInfo);
+            _logger.LogInformation($"TimerInfo: {info}");
+        }
+    }
+}

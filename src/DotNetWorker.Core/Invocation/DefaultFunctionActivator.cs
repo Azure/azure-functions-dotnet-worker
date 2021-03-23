a87e2cdcdb7c95faed66e9c0e81ebcd00d329204
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-03-23 13:34:17 -0700

    Exposing IFunctionActivator API

---
+++ src/DotNetWorker.Core/Invocation/DefaultFunctionActivator.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    internal class DefaultFunctionActivator : IFunctionActivator
+    {
+        public object? CreateInstance(Type instanceType, FunctionContext context)
+        {
+            if (instanceType is null)
+            {
+                throw new ArgumentNullException(nameof(instanceType));
+            }
+
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return ActivatorUtilities.CreateInstance(context.InstanceServices, instanceType, Array.Empty<object>());
+        }
+    }
+}
---
+++ src/DotNetWorker.Core/Invocation/FunctionActivatorExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker.Core.Invocation
+{
+    /// <summary>
+    /// Contains extension methods to work with <see cref="IFunctionActivator"/> instances.
+    /// </summary>
+    public static class FunctionActivatorExtensions
+    {
+        /// <summary>
+        /// Creates an instance of the specified generic type argument, <typeparamref name="T"/>, to be used as the target of the invocation.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to create.</typeparam>
+        /// <param name="activator">The <see cref="IFunctionActivator"/> instance to use when creating the instance.</param>
+        /// <param name="context">The <see cref="FunctionContext"/> for the invocation triggering the instance creation.</param>
+        /// <returns>The created instance.</returns>
+        public static T? CreateInstance<T>(this IFunctionActivator activator, FunctionContext context)
+           where T : class
+        {
+            if (activator is null)
+            {
+                throw new ArgumentNullException(nameof(activator));
+            }
+
+            return activator.CreateInstance(typeof(T), context) as T;
+        }
+    }
+}

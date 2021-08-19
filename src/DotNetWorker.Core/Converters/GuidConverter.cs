7005e5221a78d245fc0afae6230c2c2a1e0d890c
Shyju <connectshyju@gmail.com>
2021-08-19 11:31:06 -0700

    Adding GuidConverter which will handle binding of Guid/Guid? type parameters (#582)
    Fixes #521 
    
    Adding a new `IConverter` implementation to handle binding of `Guid` or `Guid?` type parameters.

---
+++ src/DotNetWorker.Core/Converters/GuidConverter.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker.Converters
+{
+    /// <summary>
+    /// Converter to bind Guid/Guid? type parameters.
+    /// </summary>
+    internal class GuidConverter : IConverter
+    {
+        public bool TryConvert(ConverterContext context, out object? target)
+        {
+            target = default;
+
+            if (context.Parameter.Type == typeof(Guid) || context.Parameter.Type == typeof(Guid?))
+            {
+                if (context.Source is string sourceString && Guid.TryParse(sourceString, out Guid parsedGuid))
+                {
+                    target = parsedGuid;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/DotNetWorkerTests/Converters/GuidConverterTests.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.Functions.Worker.Converters;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.Worker.Tests.Converters
+{
+    public class GuidConverterTests
+    {
+        private readonly GuidConverter _converter = new GuidConverter();
+        private readonly string _parameterName = "Id";
+
+        [Theory]
+        [InlineData("6cf8151848244ca78a169e14b4f13beb", typeof(Guid))]
+        [InlineData("6cf81518-4824-4ca7-8a16-9e14b4f13beb", typeof(Guid))]
+        [InlineData("{6cf81518-4824-4ca7-8a16-9e14b4f13beb}", typeof(Guid))]
+        [InlineData("(6cf81518-4824-4ca7-8a16-9e14b4f13beb)", typeof(Guid))]
+        [InlineData("6cf8151848244ca78a169e14b4f13beb", typeof(Guid?))]
+        [InlineData("6cf81518-4824-4ca7-8a16-9e14b4f13beb", typeof(Guid?))]
+        public void ConversionSuccessfulForValidSourceValue(object source, Type parameterType)
+        {
+            var context = new TestConverterContext(_parameterName, parameterType, source);
+            
+            var didConvert = _converter.TryConvert(context, out object target);
+
+            Assert.True(didConvert);
+            var convertedGuid = TestUtility.AssertIsTypeAndConvert<Guid>(target);
+            Assert.Equal(Guid.Parse(source.ToString()), convertedGuid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("a-string-with-four-hyphens")]
+        [InlineData(12345)]
+        [InlineData(true)]
+        [InlineData("6cf81518-4824-4ca7-8a16")]
+        [InlineData("6cf81518-4824-4ca7-8a16_9e14b4f13beb")]
+        [InlineData("ValidGuidInsideAString6cf81518-4824-4ca7-8a16-9e14b4f13beb")]
+        public void ConversionFailedForInvalidSourceValue(object source)
+        {
+            var context = new TestConverterContext(_parameterName, typeof(Guid), source);
+            
+            var didConvert = _converter.TryConvert(context, out object target);
+
+            Assert.False(didConvert);
+            Assert.Null(target);
+        }
+                
+        [Theory]
+        [InlineData("6cf81518-4824-4ca7-8a16-9e14b4f13beb", typeof(string))]
+        [InlineData("6cf81518-4824-4ca7-8a16-9e14b4f13beb", typeof(int))]
+        [InlineData("6cf81518-4824-4ca7-8a16-9e14b4f13beb", typeof(bool))]
+        public void ConversionFailedForInvalidParameterType(object source, Type parameterType)
+        {
+            var context = new TestConverterContext(_parameterName, parameterType, source);
+            
+            var didConvert = _converter.TryConvert(context, out object target);
+
+            Assert.False(didConvert);
+            Assert.Null(target);
+        }
+    }
+}

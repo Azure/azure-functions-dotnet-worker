885723e1b2d729c9d41ccf76b88adec801b8d465
Sarah Vu <sarahvu@microsoft.com>
2021-11-01 13:28:40 -0700

    trim whitespace around cookies

--- src/DotNetWorker.Grpc/Http/GrpcHttpRequestData.cs
+++ src/DotNetWorker.Grpc/Http/GrpcHttpRequestData.cs
@@ -153,8 +153,8 @@ namespace Microsoft.Azure.Functions.Worker
             for (int c = 0; c < separateCookies.Length; c++)
             {
                 var splitArray = separateCookies[c].Split("=", StringSplitOptions.RemoveEmptyEntries);
-                var name = splitArray[0];
-                var value = splitArray[1];
+                var name = splitArray[0].Trim();
+                var value = splitArray[1].Trim();
                 httpCookiesList.Add(new HttpCookie(name, value));
             }
 

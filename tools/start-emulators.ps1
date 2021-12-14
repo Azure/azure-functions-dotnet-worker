45c86430e572fcc5986921b95b126f92a9322a17
Shyju Krishnankutty <connectshyju@gmail.com>
2021-12-14 12:23:47 -0800

    Using NoUI

--- tools/start-emulators.ps1
+++ tools/start-emulators.ps1
@@ -57,7 +57,7 @@ if (!$SkipCosmosDBEmulator)
     if ($cosmosStatus -ne "Running")
     {
         Write-Host "CosmosDB emulator is not running. Starting emulator in blocking fashion(without -NoWait), with debug flag."
-        Start-CosmosDbEmulator -debug -Trace
+        Start-CosmosDbEmulator -NoUI
         $startedCosmos = $true
     }
     else

194b74161c5925959b231f55b1f26ed39f69f9fc
shrohilla <shrohilla@microsoft.com>
2022-02-09 00:01:43 +0530

    adding the support for kafka headers with latest build

--- extensions/Worker.Extensions.Kafka/src/Properties/AssemblyInfo.cs
+++ extensions/Worker.Extensions.Kafka/src/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.Kafka", "3.2.1")]
+[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.Kafka", "3.3.2")]

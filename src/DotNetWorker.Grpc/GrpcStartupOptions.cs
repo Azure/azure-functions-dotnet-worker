d6c24f413415fc80aad75ec4506387f259870e84
Sarah Vu <sarahvu@microsoft.com>
2022-04-25 12:24:06 -0700

    checkout grpcstartupoptions from main to revert

--- src/DotNetWorker.Grpc/GrpcStartupOptions.cs
+++ src/DotNetWorker.Grpc/GrpcStartupOptions.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 
-using System.Text.Json.Serialization;
-
 namespace Microsoft.Azure.Functions.Worker
 {
     internal class GrpcWorkerStartupOptions

2c06420219c56d27fa0901e98b0c2f32e7dab014
Shyju Krishnankutty <connectshyju@gmail.com>
2022-04-28 16:49:23 -0700

    Startup hook for extensions (#830)
    * Working unit test of source gen and extension runner. Still facing assembly version conflict.
    
    * Added unit test for generator & some tidying
    
    * Cleanup
    
    * Test clean up
    
    * Working unit test of source gen and extension runner. Still facing assembly version conflict.
    
    * Added unit test for generator & some tidying
    
    * Cleanup
    
    * Test clean up
    
    * Clean up.
    
    * Fixed the mess caused by rebase. 😭
    
    * Slight cleanup
    
    * Adding IncludeBuildOutput prop
    
    * Adding Sdk.Generators project to filtered solution file we use for devpack ps.
    
    * Switched to an optimized version of code to get the auto generated code(after benchmarking different versions). Also some cleanup
    
    * Renamed autogenerated type names to be more readable. Other code comment improvements.
    
    * Fixed tests to reflect the recent renaming changes
    
    * Addressing PR feedback.
    
    * 🔆 Addressing PR feedback.
    
    * 🚿 Cleanup
    
    * Using Console.Error.WriteLine instead of Console.WriteLine for error logging during extension startup failure.
    
    * String equals using explicit compariosn type and other nit fixes.
    
    * Report a Diagnostic error during build time if the startup implementation type is invalid, instead of throwing error during runtime
    
    * Added the helplink docs pages for diagnostic errors emitted from source gen.

---
+++ src/DotNetWorker.Core/ExtensionStartup/WorkerExtensionStartupAttribute.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker.Core
+{
+    /// <summary>
+    /// An assembly level attribute to inform that this assembly contains a worker extension startup implementation type.
+    /// </summary>
+
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
+    public class WorkerExtensionStartupAttribute : Attribute
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="WorkerExtensionStartupAttribute"/>.
+        /// </summary>
+        /// <param name="startupType">The type of the extension startup class implementation.</param>
+        /// <exception cref="ArgumentNullException">Throws when startupType is null.</exception>
+
+        public WorkerExtensionStartupAttribute(Type startupType)
+        {
+            StartupType = startupType ?? throw new ArgumentNullException(nameof(startupType));
+        }
+
+        /// <summary>
+        /// Gets the type of the startup class implementation.
+        /// </summary>
+        public Type StartupType { get; }
+    }
+}

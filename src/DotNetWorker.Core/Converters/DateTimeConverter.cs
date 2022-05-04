3650b65ff54195ecfadacc2c008fdfd822383891
Shyju Krishnankutty <connectshyju@gmail.com>
2022-05-04 14:09:26 -0700

    Adding DateTimeConverter which will handle binding of DateTime/DateTimeOffset type parameters (#852)
    * Adding DateTime converter and tests
    
    * Added support for .net 6 DateOnly and TimeOnly types.
    
    * Using 2 digits month part for test data.
    
    * Fixed the failing test in InputConversionFeature test to use explicit format to avoid flaky behavior on CI/CD env.
    
    * 💵 Added support for DateTimeOffSet and a bit of cleanup
    
    * Cleanup
    
    * Updating List initial capacity.

---
+++ src/DotNetWorker.Core/Converters/DateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Functions.Worker.Converters
+{
+    /// <summary>
+    /// Converter to bind to DateTime/DateTimeOffset type.
+    /// </summary>
+    internal sealed class DateTimeConverter : IInputConverter
+    {
+        public ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
+        {
+            if (!IsValidTargetType(context) || context.Source is not string source)
+            {
+                return new ValueTask<ConversionResult>(ConversionResult.Unhandled());
+            }
+
+            if ((context.TargetType == typeof(DateTimeOffset) || context.TargetType == typeof(DateTimeOffset?))
+                && DateTimeOffset.TryParse(source, out var parsedDateTimeOffset))
+            {
+                return new ValueTask<ConversionResult>(ConversionResult.Success(parsedDateTimeOffset));
+            }
+
+            if (DateTime.TryParse(source, out DateTime parsedDate))
+            {
+                return new ValueTask<ConversionResult>(ConversionResult.Success(parsedDate));
+            }
+
+            return new ValueTask<ConversionResult>(ConversionResult.Unhandled());
+        }
+
+        private static bool IsValidTargetType(ConverterContext context)
+        {
+            return context.TargetType == typeof(DateTime)
+                   || context.TargetType == typeof(DateTime?)
+                   || context.TargetType == typeof(DateTimeOffset)
+                   || context.TargetType == typeof(DateTimeOffset?);
+        }
+    }
+}

7637eed0c780b7034141cee7853cb1009fdc72b5
Madeline Gordon <magordon@microsoft.com>
2022-05-17 16:09:26 -0700

    remove backing params from TableOutputAttribute

--- extensions/Worker.Extensions.Storage.Tables/src/TableOutputAttribute.cs
+++ extensions/Worker.Extensions.Storage.Tables/src/TableOutputAttribute.cs
@@ -4,15 +4,11 @@ namespace Microsoft.Azure.Functions.Worker
 {
     public class TableOutputAttribute : OutputBindingAttribute
     {
-        private readonly string _tableName;
-        private readonly string _partitionKey;
-        private readonly string _rowKey;
-
         /// <summary>Initializes a new instance of the <see cref="TableOutputAttribute"/> class.</summary>
         /// <param name="tableName">The name of the table to which to bind.</param>
         public TableOutputAttribute(string tableName)
         {
-            _tableName = tableName;
+            TableName = tableName;
         }
 
         /// <summary>Initializes a new instance of the <see cref="TableOutputAttribute"/> class.</summary>
@@ -20,8 +16,8 @@ namespace Microsoft.Azure.Functions.Worker
         /// <param name="partitionKey">The partition key of the entity.</param>
         public TableOutputAttribute(string tableName, string partitionKey)
         {
-            _tableName = tableName;
-            _partitionKey = partitionKey;
+            TableName = tableName;
+            PartitionKey = partitionKey;
         }
 
         /// <summary>Initializes a new instance of the <see cref="TableOutputAttribute"/> class.</summary>
@@ -30,31 +26,22 @@ namespace Microsoft.Azure.Functions.Worker
         /// <param name="rowKey">The row key of the entity.</param>
         public TableOutputAttribute(string tableName, string partitionKey, string rowKey)
         {
-            _tableName = tableName;
-            _partitionKey = partitionKey;
-            _rowKey = rowKey;
+            TableName = tableName;
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
         }
 
         /// <summary>Gets the name of the table to which to bind.</summary>
         /// <remarks>When binding to a table entity, gets the name of the table containing the entity.</remarks>
-        public string TableName
-        {
-            get { return _tableName; }
-        }
+        public string TableName { get; }
 
         /// <summary>When binding to a table entity, gets the partition key of the entity.</summary>
         /// <remarks>When binding to an entire table, returns <see langword="null"/>.</remarks>
-        public string PartitionKey
-        {
-            get { return _partitionKey; }
-        }
+        public string PartitionKey { get; }
 
         /// <summary>When binding to a table entity, gets the row key of the entity.</summary>
         /// <remarks>When binding to an entire table, returns <see langword="null"/>.</remarks>
-        public string RowKey
-        {
-            get { return _rowKey; }
-        }
+        public string RowKey { get; }
 
         /// <summary>
         /// Gets or sets the app setting name that contains the Azure Storage connection string.

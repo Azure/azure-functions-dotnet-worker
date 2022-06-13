f9451855130039e1604db1fd7aa6d93dbbd846ae
Surgupta <surgupta@microsoft.com>
2022-06-13 16:10:25 -0700

    Sending worker status response message

--- src/DotNetWorker.Grpc/GrpcWorker.cs
+++ src/DotNetWorker.Grpc/GrpcWorker.cs
@@ -130,6 +130,10 @@ namespace Microsoft.Azure.Functions.Worker
             {
                 responseMessage.WorkerInitResponse = WorkerInitRequestHandler(request.WorkerInitRequest);
             }
+            else if (request.ContentCase == MsgType.WorkerStatusRequest)
+            {
+                responseMessage.WorkerStatusResponse = new WorkerStatusResponse();
+            }
             else if (request.ContentCase == MsgType.FunctionsMetadataRequest)
             {
                 responseMessage.FunctionMetadataResponse = await GetFunctionMetadataAsync(request.FunctionsMetadataRequest.FunctionAppDirectory);

be58b7ef1bc3162d8b232089564aad2040d03c9f
Shyju Krishnankutty <connectshyju@gmail.com>
2022-06-17 10:31:18 -0700

    [Samples] Added an example CosmosDB Input binding which uses property values from QueueTrigger payload. (#933)
    * Added an example of QueueTrigger function which uses Cosmos Input binding
    
    * Added comments about what this function is doing and fixed a missed `/` in the closing tag of code snippet.

---
+++ samples/Extensions/CosmosDB/CosmosDbInputBindingFunction.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Extensions.CosmosDB
+{
+    /// <summary>
+    /// Sample function app which has a azure storage queue trigger and CosmosDb input binding.
+    /// The CosmosDb input binding is using the values from the queue trigger payload
+    /// to query the cosmos db item.
+    /// </summary>
+
+    //<docsnippet_qtrigger_with_cosmosdb_inputbinding>
+    public class CosmosDbInputBindingFunction
+    {
+        private readonly ILogger _logger;
+
+        public CosmosDbInputBindingFunction(ILogger<CosmosDbInputBindingFunction> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [Function("DocByIdFromJSON")]
+        public void Run(
+            [QueueTrigger("todoqueueforlookup")] ToDoItemLookup toDoItemLookup,
+            [CosmosDBInput(databaseName: "ToDoItems",
+                           collectionName: "Items",
+                           ConnectionStringSetting = "CosmosConnection",
+                           Id ="{ToDoItemId}",
+                           PartitionKey ="{ToDoItemPartitionKeyValue}")] ToDoItem toDoItem)
+        {
+            if (toDoItem == null)
+            {
+                _logger.LogInformation("ToDo item not found");
+            }
+            else
+            {
+                _logger.LogInformation($"Found ToDo item, Description={toDoItem.Description}");
+            }
+        }
+    }
+
+    public record ToDoItemLookup(string ToDoItemId, string ToDoItemPartitionKeyValue);
+    //</docsnippet_qtrigger_with_cosmosdb_inputbinding>
+
+    //<docsnippet_qtrigger_with_cosmosdb_inputbinding_todo_model>
+    public class ToDoItem
+    {
+        public string Id { get; set; }
+        public string PartitionKey { get; set; }
+        public string Description { get; set; }
+    }
+    //</docsnippet_qtrigger_with_cosmosdb_inputbinding_todo_model>
+}

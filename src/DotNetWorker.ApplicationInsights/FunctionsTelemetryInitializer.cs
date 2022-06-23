440085f58279c1e81caa74e8bdcb35d0bbb8a95e
Brett Samblanet <brettsam@microsoft.com>
2022-06-23 09:01:25 -0700

    adding AI project

---
+++ src/DotNetWorker.ApplicationInsights/FunctionsTelemetryInitializer.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Microsoft.Azure.Functions.Worker.ApplicationInsights;
+
+namespace Microsoft.Azure.WebJobs.Logging.ApplicationInsights
+{
+    internal class FunctionsTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly string _sdkVersion;
+        private readonly string _roleInstanceName;
+
+        public FunctionsTelemetryInitializer(ISdkVersionProvider versionProvider, IRoleInstanceProvider roleInstanceProvider)
+        {
+            if (versionProvider == null)
+            {
+                throw new ArgumentNullException(nameof(versionProvider));
+            }
+
+            if (roleInstanceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(roleInstanceProvider));
+            }
+
+            _sdkVersion = versionProvider.GetSdkVersion();
+            _roleInstanceName = roleInstanceProvider.GetRoleInstanceName();
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry == null)
+            {
+                return;
+            }
+
+            telemetry.Context.Cloud.RoleInstance = _roleInstanceName;
+            telemetry.Context.GetInternalContext().SdkVersion = _sdkVersion;
+
+            if (Activity.Current is not null)
+            {
+                foreach (var tag in Activity.Current.Tags)
+                {
+                    switch (tag.Key)
+                    {
+                        case LogConstants.NameKey:
+                            telemetry.Context.Operation.Name = tag.Value;
+                            continue;
+                        default:
+                            break;
+                    }
+
+                    if (telemetry is ISupportProperties properties && !tag.Key.StartsWith("ai_"))
+                    {
+                        properties.Properties[tag.Key] = tag.Value;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ src/DotNetWorker.ApplicationInsights/FunctionsWorkerTelemetryModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace DotNetWorker.ApplicationInsights
+{
+    internal class FunctionsWorkerTelemetryModule : ITelemetryModule, IDisposable
+    {
+        private ActivityListener? _listener;
+
+        public void Initialize(TelemetryConfiguration configuration)
+        {
+            var telemetryClient = new TelemetryClient(configuration);
+
+            _listener = new ActivityListener
+            {
+                ShouldListenTo = source => source.Name.StartsWith("Microsoft.Azure.Functions.Worker"),
+                ActivityStarted = activity =>
+                {
+                    var dependency = new DependencyTelemetry("Azure.Functions", activity.OperationName, activity.OperationName, null);
+                    activity.AddTag("_depTel", dependency);
+                    dependency.Start();
+                },
+                ActivityStopped = activity =>
+                {
+                    var dependency = activity.GetTagItem("_depTel") as DependencyTelemetry;
+                    dependency.Stop();
+                    telemetryClient.TrackDependency(dependency);
+                },
+                Sample = (ref ActivityCreationOptions<ActivityContext> sampleActivity) => ActivitySamplingResult.AllData
+            };
+
+            ActivitySource.AddActivityListener(_listener);
+        }
+
+        public void Dispose()
+        {
+            _listener?.Dispose();
+        }
+    }
+}

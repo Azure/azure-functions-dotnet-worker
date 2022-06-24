7a7922374bbad0e370cddfcd59317e1add0a85fe
Zitong Yang <zityang@microsoft.com>
2022-06-24 15:22:12 +0800

    Update SignalR sample

---
+++ samples/Extensions/SignalR/SignalROutputBindingFunctions.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.IO;
+using System.Linq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace SampleApp
+{
+    public static class SignalROutputBindingFunctions
+    {
+        [Function("BroadcastToAll")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRMessageAction BroadcastToAll(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+            return new SignalRMessageAction("newMessage")
+            {
+                // broadcast to all the connected clients without specifying any connection, user or group.
+                Arguments = new[] { bodyReader.ReadToEnd() },
+            };
+        }
+
+        [Function("SendToConnection")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRMessageAction SendToConnection(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+            return new SignalRMessageAction("newMessage")
+            {
+                Arguments = new[] { bodyReader.ReadToEnd() },
+                ConnectionId = "connectionToSend",
+            };
+        }
+
+        [Function("SendToUser")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRMessageAction SendToUser(
+[HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+            return new SignalRMessageAction("newMessage")
+            {
+                Arguments = new[] { bodyReader.ReadToEnd() },
+                UserId = "userToSend",
+            };
+        }
+
+        [Function("SendToGroup")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRMessageAction SendToGroup(
+[HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+            return new SignalRMessageAction("newMessage")
+            {
+                Arguments = new[] { bodyReader.ReadToEnd() },
+                GroupName = "groupToSend"
+            };
+        }
+
+        [Function("SendToEndpoint")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRMessageAction SendToEndpoint(
+[HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, [SignalREndpointsInput("chat", ConnectionStringSetting = "SignalRConnection")] SignalREndpoint[] endpoints)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+            return new SignalRMessageAction("newMessage")
+            {
+                Arguments = new[] { bodyReader.ReadToEnd() },
+                // Only send to primary endpoint if you have configured multiple SignalR Service instances.
+                // The use of 'Endpoints' can be combined with other properties such as UserId, GroupName, ConnectionID.
+                Endpoints = endpoints.Where(e => e.EndpointType == SignalREndpointType.Primary).ToArray()
+            };
+        }
+
+        [Function("RemoveFromGroup")]
+        [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+        public static SignalRGroupAction RemoveFromGroup([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            return new SignalRGroupAction(SignalRGroupActionType.Remove)
+            {
+                GroupName = "group1",
+                UserId = "user1"
+            };
+        }
+    }
+}
---
+++ samples/Extensions/SignalR/SignalRTriggerFunctions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Extensions.SignalR
+{
+    public static class SignalRTriggerFunctions
+    {
+        [Function("OnConnected")]
+        public static void OnConnected([SignalRTrigger("chat", "connections", "connected")] SignalRInvocationContext invocationContext, FunctionContext functionContext)
+        {
+            var logger = functionContext.GetLogger("OnConnected");
+            logger.LogInformation($"Connection {invocationContext.ConnectionId} is connected");
+        }
+
+        [Function("OnDisconnected")]
+        public static void OnDisconnected([SignalRTrigger("chat", "connections", "disconnected")] SignalRInvocationContext invocationContext, FunctionContext functionContext)
+        {
+            var logger = functionContext.GetLogger("OnDisconnected");
+            logger.LogInformation($"Connection {invocationContext.ConnectionId} is disconnected. Error: {invocationContext.Error}");
+        }
+
+        [Function("OnClientMessage")]
+        public static void OnClientMessage([SignalRTrigger("Hub", "messages", "sendMessage", "content")] SignalRInvocationContext invocationContext, string content, FunctionContext functionContext)
+        {
+            var logger = functionContext.GetLogger("OnClientMessage");
+            logger.LogInformation($"Connection {invocationContext.ConnectionId} sent a message. Message content: {content}");
+        }
+    }
+}

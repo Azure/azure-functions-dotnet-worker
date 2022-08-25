57c95b4de28a5431bdff4b72d4ed0a9476bf29ee
Shyju Krishnankutty <connectshyju@gmail.com>
2022-08-24 19:29:58 -0700

    Upgrading CodeAnalysis related packages to 4.X which is needed to use IIncrementalGenerator

---
+++ sdk/Sdk.Generators/ExtensionStartupRunnerGeneratorV2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.Azure.Functions.Worker.Sdk.Generators
+{
+    [Generator]
+    public class ExtensionStartupRunnerGeneratorV2 : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}

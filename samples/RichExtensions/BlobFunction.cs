7e30af865b14a0a9e26c74762dbd43b3d1142436
Lilian Kasem <likasem@microsoft.com>
2022-08-26 17:58:24 -0700

    cleanup sample

--- samples/RichExtensions/BlobFunction.cs
+++ samples/RichExtensions/BlobFunction.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -9,12 +10,15 @@ namespace SampleApp
 {
     public static class BlobFunction
     {
-        [Function("BlobFunction")]
-        public static void Run(
+        [Function(nameof(BlobFunction))]
+        public static async Task Run(
             [BlobTrigger("test-trigger/{name}")] BlobClient client,
             FunctionContext context)
         {
-            var logger = context.GetLogger("BlobFunction");
+            var logger = context.GetLogger(nameof(BlobFunction));
+            var downloadResult = await client.DownloadContentAsync();
+            var content = downloadResult.Value.Content.ToString();
+            logger.LogInformation($"Blob content: {content}");
         }
     }
 }

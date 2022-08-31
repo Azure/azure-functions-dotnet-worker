d2cf521c76f91f8b37db554f504343cd41116b8b
Lilian Kasem <likasem@microsoft.com>
2022-08-31 12:24:11 -0700

    cleanup converter and sample

--- extensions/Worker.Extensions.Storage.Blobs/src/BlobStorageConverter.cs
+++ extensions/Worker.Extensions.Storage.Blobs/src/BlobStorageConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.Threading;
 using System.Runtime;
 using System.Threading.Tasks;
@@ -22,14 +23,14 @@ namespace Microsoft.Azure.Functions.Worker.Converters
     {
         public ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
         {
-            if (context.TargetType != typeof(BlobClient))
+            if (context.TargetType != typeof(BlobClient) || context.TargetType != typeof(System.IO.Stream))
             {
                 return new ValueTask<ConversionResult>(ConversionResult.Unhandled());
             }
 
-            object? client = null;
+            object? result = null;
 
-            var referenceData = JObject.Parse(context?.Source?.ToString());
+            var referenceData = JObject.Parse(context.Source?.ToString());
             var blob = referenceData?["Properties"]["blob_name"].ToString();
             var container = referenceData?["Properties"]["blob_container"].ToString();
             var connection = referenceData?["Properties"]["connection_string"].ToString();
@@ -43,38 +44,40 @@ namespace Microsoft.Azure.Functions.Worker.Converters
 
             switch (context.TargetType)
             {
-                // TODO: simplify creation using generics i.e.
-                // await GetBlobAsync(blobAttribute, cancellationToken, typeof(T)).ConfigureAwait(false);
+                case Type _ when context.TargetType == typeof(Stream):
+                    BlobClient blobClient = new BlobClient(connectionString, container, blob);
+                    var downloadResult = blobClient.DownloadStreaming();
+                    result = downloadResult.Value.Content;
+                    break;
+
+                // TODO: Add string & binary cases
+
                 case Type _ when context.TargetType == typeof(BlobClient):
-                    client = new BlobClient(connectionString, container, blob);
+                    result = new BlobClient(connectionString, container, blob);
                     break;
 
                 case Type _ when context.TargetType == typeof(AppendBlobClient):
-                    client = new AppendBlobClient(connectionString, container, blob);
+                    result = new AppendBlobClient(connectionString, container, blob);
                     break;
 
                 case Type _ when context.TargetType == typeof(BlockBlobClient):
-                    client = new BlockBlobClient(connectionString, container, blob);
+                    result = new BlockBlobClient(connectionString, container, blob);
                     break;
 
                 case Type _ when context.TargetType == typeof(PageBlobClient):
-                    client = new PageBlobClient(connectionString, container, blob);
-                    break;
-
-                case Type _ when context.TargetType == typeof(System.IO.Stream):
-                    BlobClient blobClient = new BlobClient(connectionString, container, blob);
-                    var result = blobClient.DownloadStreaming();
-                    var content = result.Value.Content;
-                    client = content;
+                    result = new PageBlobClient(connectionString, container, blob);
                     break;
             }
 
-            if (client is not null)
+            if (result is not null)
             {
-                return new ValueTask<ConversionResult>(ConversionResult.Success(client));
+                return new ValueTask<ConversionResult>(ConversionResult.Success(result));
             }
 
             return new ValueTask<ConversionResult>(ConversionResult.Unhandled());
         }
     }
 }
+
+// TODO: simplify creation using generics i.e.
+// await GetBlobAsync(blobAttribute, cancellationToken, typeof(T)).ConfigureAwait(false);
--- samples/RichExtensions/BlobClientFunction.cs
+++ samples/RichExtensions/BlobClientFunction.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.IO;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
@@ -8,17 +9,17 @@ using Microsoft.Extensions.Logging;
 
 namespace SampleApp
 {
-    public static class BlobFunction
+    public static class BlobClientFunction
     {
-        [Function(nameof(BlobFunction))]
+        [Function(nameof(BlobClientFunction))]
         public static async Task Run(
-            [BlobTrigger("test-trigger/{name}", Connection = "AzureWebJobsStorage")] BlobClient client,
+            [BlobTrigger("blobclient-trigger/{name}", Connection = "AzureWebJobsStorage")] BlobClient client,
             FunctionContext context)
         {
-            var logger = context.GetLogger(nameof(BlobFunction));
+            var logger = context.GetLogger(nameof(BlobClientFunction));
             var downloadResult = await client.DownloadContentAsync();
             var content = downloadResult.Value.Content.ToString();
-            logger.LogInformation($"Blob content: {content}");
+            logger.LogInformation("Blob content: {content}", content);
         }
     }
 }

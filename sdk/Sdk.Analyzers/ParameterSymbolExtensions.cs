ff5e88cc40a0ba7cc20a1b659371832a58337f8b
Shyju Krishnankutty <connectshyju@gmail.com>
2022-10-03 09:42:03 -0700

    Updated WebJobsAttributesNotSupported analyzer to handle return types.

--- sdk/Sdk.Analyzers/ParameterSymbolExtensions.cs
+++ sdk/Sdk.Analyzers/ParameterSymbolExtensions.cs
@@ -1,28 +1,26 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.Azure.Functions.Worker.Sdk.Analyzers
 {
     internal static class ParameterSymbolExtensions
     {
+        /// <summary>
+        /// Gets the first web jobs Attribute if the parameter symbol has any, else returns null.
+        /// </summary>
+        /// <param name="parameter">The parameter symbol to check.</param>
+        /// <returns>An instance of <see cref="AttributeData"/> for the WebJobs Attribute found if any, else null.</returns>
         public static AttributeData GetWebJobsAttribute(this IParameterSymbol parameter)
         {
             var parameterAttributes = parameter.GetAttributes();
 
             foreach (var parameterAttribute in parameterAttributes)
             {
-                var attributeAttributes = parameterAttribute.AttributeClass.GetAttributes();
-
-                foreach (var attribute in attributeAttributes)
+                if (parameterAttribute.IsWebJobAttribute())
                 {
-                    if (string.Equals(attribute.AttributeClass.ToDisplayString(), Constants.Types.WebJobsBindingAttribute, StringComparison.Ordinal))
-                    {
-                        return parameterAttribute;
-                    }
+                    return parameterAttribute;
                 }
             }
 

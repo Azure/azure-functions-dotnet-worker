7e7f00edf6670650b342f57c35c0e50a0ad96a34
Lilian Kasem <likasem@microsoft.com>
2022-11-03 17:26:15 -0700

    Add blob storage converter

---
+++ extensions/Worker.Extensions.Storage.Blobs/src/Constants.cs
@@ -0,0 +1,16 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    internal static class Constants
+    {
+        internal const string BlobExtensionName = "AzureStorageBlobs";
+        internal const string Connection = "Connection";
+        internal const string ContainerName = "ContainerName";
+        internal const string BlobName = "BlobName";
+
+        // Media content types
+        internal const string JsonContentType = "application/json";
+    }
+}

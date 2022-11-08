91cde0b3db5ee381f26e4acf86ae504893865a8b
Surgupta <surgupta@microsoft.com>
2022-11-07 16:15:23 -0800

    Adding test to verify SupportDeferredBinding property

--- test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
+++ test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
@@ -245,6 +245,67 @@ namespace Microsoft.Azure.Functions.SdkTests
         }
 
         [Fact]
+        public void StorageFunction_SDKTypeBindings()
+        {
+            var generator = new FunctionMetadataGenerator();
+            var module = ModuleDefinition.ReadModule(_thisAssembly.Location);
+            var typeDef = TestUtility.GetTypeDefinition(typeof(SDKTypeBindings));
+            var functions = generator.GenerateFunctionMetadata(typeDef);
+            var extensions = generator.Extensions;
+
+            Assert.Equal(1, functions.Count());
+
+            var blobToBlob = functions.Single(p => p.Name == "BlobToBlobFunction");
+
+            ValidateFunction(blobToBlob, "BlobToBlobFunction", GetEntryPoint(nameof(SDKTypeBindings), nameof(SDKTypeBindings.BlobToBlob)),
+                b => ValidateBlobTrigger(b),
+                b => ValidateBlobInput(b),
+                b => ValidateBlobOutput(b));
+
+            AssertDictionary(extensions, new Dictionary<string, string>
+            {
+                { "Microsoft.Azure.WebJobs.Extensions.Storage.Blobs", "5.2.0-alpha.20221103.1" },
+            });
+
+            void ValidateBlobTrigger(ExpandoObject b)
+            {
+                AssertExpandoObject(b, new Dictionary<string, object>
+                {
+                    { "Name", "blob" },
+                    { "Type", "blobTrigger" },
+                    { "Direction", "In" },
+                    { "path", "container2/%file%" },
+                    { "Properties", new Dictionary<String, Object>( ) { { "SupportsDeferredBinding" , "True"} } }
+                });
+            }
+
+            void ValidateBlobInput(ExpandoObject b)
+            {
+                AssertExpandoObject(b, new Dictionary<string, object>
+                {
+                    { "Name", "blobinput" },
+                    { "Type", "blob" },
+                    { "Direction", "In" },
+                    { "blobPath", "container2/%file%" },
+                    { "Properties", new Dictionary<String, Object>( ) { { "SupportsDeferredBinding" , "True"} } }
+                });
+            }
+
+            void ValidateBlobOutput(ExpandoObject b)
+            {
+                AssertExpandoObject(b, new Dictionary<string, object>
+                {
+                    { "Name", "$return" },
+                    { "Type", "blob" },
+                    { "Direction", "Out" },
+                    { "blobPath", "container1/hello.txt" },
+                    { "Connection", "MyOtherConnection" },
+                    { "Properties", new Dictionary<String, Object>() }
+                });
+            }
+        }
+
+        [Fact]
         public void TimerFunction()
         {
             var generator = new FunctionMetadataGenerator();
@@ -745,6 +806,18 @@ namespace Microsoft.Azure.Functions.SdkTests
             public object BlobToBlobs(
                 [BlobTrigger("container2/%file%")] string blob,
                 [BlobInput("container2", IsBatched = true)] IEnumerable<string> blobinput)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class SDKTypeBindings
+        {
+            [Function("BlobToBlobFunction")]
+            [BlobOutput("container1/hello.txt", Connection = "MyOtherConnection")]
+            public object BlobToBlob(
+                [BlobTrigger("container2/%file%")] string blob,
+                [BlobInput("container2/%file%")] string blobinput)
             {
                 throw new NotImplementedException();
             }

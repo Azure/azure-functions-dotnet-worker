9c3305768f742ffa4ef297ff624ad1bd910b067a
Maddie Gordon <50681653+madelinegordon@users.noreply.github.com>
2022-11-09 17:06:29 -0800

    Fix Tables extension reference (#1163)
    * changed Worker.Extension.Storage.Tables to Worker.Extensions.Tables
    
    * Update .gitignore
    
    remove accidental .gitignore update

--- extensions/Worker.Extensions.Tables/src/Properties/AssemblyInfo.cs
+++ extensions/Worker.Extensions.Tables/src/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 
 ﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.Storage.Tables", "1.0.0")]
+[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.Tables", "1.0.0")]

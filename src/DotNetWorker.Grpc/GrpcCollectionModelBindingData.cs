b36619464e5420653cea2c43d09971de85d930c3
Surgupta <surgupta@microsoft.com>
2022-12-02 16:53:58 -0800

    Updating return type of overridden method

--- src/DotNetWorker.Grpc/GrpcCollectionModelBindingData.cs
+++ src/DotNetWorker.Grpc/GrpcCollectionModelBindingData.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -15,7 +15,7 @@ namespace Microsoft.Azure.Functions.Worker.Grpc.Messages
             _modelBindingDataArray = modelBindingDataArray;
         }
 
-        public override GrpcModelBindingData[] modelBindingDataArray => _modelBindingDataArray.ModelBindingData.Select(
+        public override Core.ModelBindingData[] modelBindingDataArray => _modelBindingDataArray.ModelBindingData.Select(
                                                             p => new GrpcModelBindingData(p)).ToArray();
     }
 }

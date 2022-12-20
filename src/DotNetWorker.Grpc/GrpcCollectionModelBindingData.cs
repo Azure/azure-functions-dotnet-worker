dd19046b7856ae5a149c0bc99de322508d315e5c
Surgupta <surgupta@microsoft.com>
2022-12-20 13:42:06 -0800

    Revert "Removed partial keyword and moved linq to constructor"
    This reverts commit 683ae92390a17ebce0ddc6c9e81cdbde51b9fa69.

--- src/DotNetWorker.Grpc/GrpcCollectionModelBindingData.cs
+++ src/DotNetWorker.Grpc/GrpcCollectionModelBindingData.cs
@@ -6,19 +6,16 @@ using System.Linq;
 
 namespace Microsoft.Azure.Functions.Worker.Grpc.Messages
 {
-    internal class GrpcCollectionModelBindingData : Microsoft.Azure.Functions.Worker.Core.CollectionModelBindingData
+    internal partial class GrpcCollectionModelBindingData : Microsoft.Azure.Functions.Worker.Core.CollectionModelBindingData
     {
         private readonly CollectionModelBindingData _modelBindingDataArray;
-
-        private readonly Core.ModelBindingData[] _collectionModelBindingData;
 
         public GrpcCollectionModelBindingData(CollectionModelBindingData modelBindingDataArray)
         {
             _modelBindingDataArray = modelBindingDataArray;
-            _collectionModelBindingData = _modelBindingDataArray.ModelBindingData.Select(
-                                                p => new GrpcModelBindingData(p)).ToArray();
         }
 
-        public override Core.ModelBindingData[] ModelBindingDataArray => _collectionModelBindingData;
+        public override Core.ModelBindingData[] ModelBindingDataArray => _modelBindingDataArray.ModelBindingData.Select(
+                                                            p => new GrpcModelBindingData(p)).ToArray();
     }
 }
--- src/DotNetWorker.Grpc/GrpcModelBindingData.cs
+++ src/DotNetWorker.Grpc/GrpcModelBindingData.cs
@@ -5,23 +5,20 @@ using System;
 
 namespace Microsoft.Azure.Functions.Worker.Grpc.Messages
 {
-    internal class GrpcModelBindingData : Microsoft.Azure.Functions.Worker.Core.ModelBindingData
+    internal partial class GrpcModelBindingData : Microsoft.Azure.Functions.Worker.Core.ModelBindingData
     {
         private readonly ModelBindingData _modelBindingData;
-
-        private readonly BinaryData _content;
 
         public GrpcModelBindingData(ModelBindingData modelBindingData)
         {
             _modelBindingData = modelBindingData;
-            _content = BinaryData.FromBytes(_modelBindingData.Content.ToByteArray());
         }
 
         public override string Version => _modelBindingData.Version;
 
         public override string Source => _modelBindingData.Source;
 
-        public override BinaryData Content => _content;
+        public override BinaryData Content => BinaryData.FromBytes(_modelBindingData.Content.ToByteArray());
 
         public override string ContentType => _modelBindingData.ContentType;
     }

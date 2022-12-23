0bf39def3713b361969cf063135ec285fce7fde9
Lilian Kasem <likasem@microsoft.com>
2022-12-22 19:36:29 -0800

    Add collection and managed identity support. Try using cosmos v3

---
+++ extensions/Worker.Extensions.CosmosDB/src/Constants.cs
@@ -0,0 +1,17 @@
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    internal static class Constants
+    {
+        internal const string ExtensionName = "Microsoft.Azure.WebJobs.Extensions.CosmosDB";
+
+        internal const string ConnectionAccountName = "AzureWebJobsStorage__accountName";
+        internal const string ConnectionClientId = "AzureWebJobsStorage__clientId";
+        internal const string ConnectionClientSecret = "AzureWebJobsStorage__clientSecret";
+        internal const string ConnectionTenantId = "AzureWebJobsStorage__tenantId";
+        internal const string ConnectionCredential = "AzureWebJobsStorage__credential";
+    }
+}

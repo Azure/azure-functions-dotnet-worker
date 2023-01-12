7590df4e327e2d20fb8f2438d9f81bdb04a0c50e
Shyju Krishnankutty <connectshyju@gmail.com>
2023-01-11 17:04:52 -0800

    Cosmos DB worker paclage 4.0.0 (GA)

--- extensions/Worker.Extensions.CosmosDB/src/Properties/AssemblyInfo.cs
+++ extensions/Worker.Extensions.CosmosDB/src/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.CosmosDB", "4.0.0-preview2")]
+[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.CosmosDB", "4.0.0")]

4a28b9b1bc6e22a1c755b898cd756395eb329d34
Mélony QIN <4621560+cloudmelon@users.noreply.github.com>
2023-01-31 15:59:12 -0800

    remove the date

--- samples/Extensions/Http/HttpFunction.cs
+++ samples/Extensions/Http/HttpFunction.cs
@@ -21,7 +21,7 @@ namespace SampleApp
             //</docsnippet_logging>
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Date", "Mon, 18 Jul 2016 16:06:00 GMT");
+           
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
             response.WriteString("Welcome to .NET isolated worker !!");

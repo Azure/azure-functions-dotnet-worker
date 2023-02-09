1bf910bea09c8539a55c788daf662306038b9318
Jialin Xin <jixin@microsoft.com>
2023-02-09 10:23:49 +0800

    Init AWPS extension project

--- test/E2ETests/E2EApps/E2EApp/Timer/TimerTestFunctions.cs
+++ test/E2ETests/E2EApps/E2EApp/Timer/TimerTestFunctions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
@@ -20,6 +21,19 @@ namespace Microsoft.Azure.Functions.Worker.E2EApp.Queue
         {
             var info = JsonSerializer.Serialize(timerInfo);
             _logger.LogInformation($"TimerInfo: {info}");
+        }
+
+        [Function(nameof(TimerTrigger1))]
+        [WebPubSubOutput(Hub = "notification")]
+        public WebPubSubAction TimerTrigger1([TimerTrigger("*/10 * * * * *", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            var info = JsonSerializer.Serialize(timerInfo);
+            _logger.LogInformation($"TimerInfo: {info}");
+            return new SendToAllAction
+            {
+                Data = BinaryData.FromString($"[Isolate][DateTime: {DateTime.Now}] Temperature: 22.2{'\xB0'}C, Humidity: 44.4%"),
+                DataType = WebPubSubDataType.Text
+            };
         }
     }
 }

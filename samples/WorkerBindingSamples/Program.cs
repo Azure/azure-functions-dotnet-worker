2b78e7b03503f499ae71507b2a842d1848b86fb2
Lilian Kasem <likasem@microsoft.com>
2023-02-08 16:44:31 -0800

    [Samples] WorkerBindingSamples project with blob function examples (#1152)
    Co-authored-by: Surgupta <surgupta@microsoft.com>

---
+++ samples/WorkerBindingSamples/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ samples/WorkerBindingSamples/README.md
@@ -0,0 +1,3 @@
+# Extensions Sample
+
+This sample project provides simple examples of how to use various extensions supported by the .NET Worker.

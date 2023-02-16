54784132e8f05973b2b1f825674f47f6675e636c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-02-15 21:47:08 -0800

    PR fb

--- extensions/Worker.Extensions.ServiceBus/src/ServiceBusReceivedMessageConverter.cs
+++ extensions/Worker.Extensions.ServiceBus/src/ServiceBusReceivedMessageConverter.cs
@@ -2,7 +2,8 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Azure.Messaging.ServiceBus.Primitives;
@@ -15,27 +16,29 @@ internal class ServiceBusReceivedMessageConverter : IInputConverter
 {
     public ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
     {
-        if (context is null)
+        ConversionResult result = context?.Source switch
         {
-            return new ValueTask<ConversionResult>(ConversionResult.Unhandled());
-        }
-
-        return context.Source switch
-        {
-            ModelBindingData binding => ConvertToServiceBusReceivedMessage(context, binding),
-            _ => new ValueTask<ConversionResult>(ConversionResult.Unhandled()),
+            ModelBindingData binding => ConversionResult.Success(ConvertToServiceBusReceivedMessage(binding)),
+            CollectionModelBindingData collection when context.TargetType.IsArray => ConversionResult.Success(collection.ModelBindingDataArray.Select(ConvertToServiceBusReceivedMessage).ToArray()),
+            CollectionModelBindingData collection => ConversionResult.Success(collection.ModelBindingDataArray.Select(ConvertToServiceBusReceivedMessage).ToList()),
+            _ => ConversionResult.Unhandled()
         };
+        return new ValueTask<ConversionResult>(result);
     }
 
-    private ValueTask<ConversionResult> ConvertToServiceBusReceivedMessage(ConverterContext context, ModelBindingData binding)
+    private ServiceBusReceivedMessage ConvertToServiceBusReceivedMessage(ModelBindingData binding)
     {
         // The lock token is a 16 byte GUID
         const int lockTokenLength = 16;
 
+        if (binding.ContentType != "application/octet-stream")
+        {
+            throw new InvalidOperationException("Only binary data is supported.");
+        }
+
         ReadOnlyMemory<byte> bytes = binding.Content.ToMemory();
         ReadOnlyMemory<byte> lockTokenBytes = bytes.Slice(0, lockTokenLength);
         ReadOnlyMemory<byte> messageBytes = bytes.Slice(lockTokenLength, bytes.Length - lockTokenLength);
-        ServiceBusReceivedMessage message = ServiceBusAmqpExtensions.FromAmqpBytes(BinaryData.FromBytes(messageBytes), BinaryData.FromBytes(lockTokenBytes));
-        return new ValueTask<ConversionResult>(ConversionResult.Success(message));
+        return ServiceBusAmqpExtensions.FromAmqpBytes(BinaryData.FromBytes(messageBytes), BinaryData.FromBytes(lockTokenBytes));
     }
 }

74f79e7bd5d155364cfbb1e3c8fd16c56a870c75
Surgupta <surgupta@microsoft.com>
2023-02-16 16:44:59 -0800

    Added tests

--- test/WorkerExtensionTests/Blob/BlobStorageConverterTests.cs
+++ test/WorkerExtensionTests/Blob/BlobStorageConverterTests.cs
@@ -29,66 +29,32 @@ namespace Microsoft.Azure.Functions.WorkerExtension.Tests
 {
     public class BlobStorageConverterTests
     {
-        private const string _sourceString = "hello";
-        private static readonly byte[] _sourceBytes = Encoding.UTF8.GetBytes(_sourceString);
-        private static readonly ReadOnlyMemory<byte> _sourceMemory = new ReadOnlyMemory<byte>(_sourceBytes);
-        private BlobStorageConverter _converter;
-
-        private static readonly IEnumerable<ReadOnlyMemory<byte>> _sourceMemoryEnumerable = new RepeatedField<ReadOnlyMemory<byte>>() { _sourceMemory };
-        //private static readonly RepeatedField<string> _sourceStringEnumerable = new RepeatedField<string>() { _sourceString };
-        //private static readonly RepeatedField<double> _sourceDoubleEnumerable = new RepeatedField<double>() { 1.0 };
-        //private static readonly RepeatedField<long> _sourceLongEnumerable = new RepeatedField<long>() { 2000 };
-        //private Mock<BlobStorageConverter> mock;
         private Mock<BlobStorageConverter> mock = new(MockBehavior.Strict);
 
         public BlobStorageConverterTests()
         {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults((WorkerOptions options) =>
-                {
-                    options.Capabilities.Remove("HandlesWorkerTerminateMessage");
-                    options.Capabilities.Add("SomeNewCapability", bool.TrueString);
-                }).Build();
-
+            var host = new HostBuilder().ConfigureFunctionsWorkerDefaults((WorkerOptions options) => {}).Build();
             var _workerOptions = host.Services.GetService<IOptions<WorkerOptions>>();
             var _blobOptions = host.Services.GetService<IOptionsSnapshot<BlobStorageBindingOptions>>();
             var logger = host.Services.GetService<ILogger<BlobStorageConverter>>();
 
-            _converter = new BlobStorageConverter(_workerOptions, _blobOptions, logger);
+            //_converter = new BlobStorageConverter(_workerOptions, _blobOptions, logger);
             mock = new Mock<BlobStorageConverter>(_workerOptions, _blobOptions, logger);
             mock.CallBase = true;
         }
 
         [Fact]
-        public async Task StorageConverter()
+        public async Task ConvertAsync_SourceAsObject_Unhandled()
         {
-            var binaryData = new BinaryData("{" +
-                "\"Connection\" : \"connection\"," +
-                "\"ContainerName\" : \"container\"," +
-                "\"BlobName\" : \"BlobName\"" +
-                "}");
+            var context = new TestConverterContext(typeof(string), new Object());
 
-            object source = new GrpcModelBindingData(new Worker.Grpc.Messages.ModelBindingData() 
-            {
-                Version = "1.0",
-                Source = "AzureStorageBlobs",
-                Content = ByteString.CopyFrom(binaryData),
-                ContentType = "application/json"
-            });
-
-            var _sourceBytes1 = Encoding.UTF8.GetBytes(new Object().ToString()); //source.ToString());
-            var _sourceMemory1 = new ReadOnlyMemory<byte>(_sourceBytes1);
-
-            var context = new TestConverterContext(typeof(string), new Object()); // source);//_sourceMemory1);
-
-            var conversionResult = await _converter.ConvertAsync(context);
+            var conversionResult = await mock.Object.ConvertAsync(context);
 
             Assert.Equal(ConversionStatus.Unhandled, conversionResult.Status);
-            //TestUtility.AssertIsTypeAndConvert<string>(conversionResult.Value);
         }
 
         [Fact]
-        public async Task StorageConverterWithData()
+        public async Task ConvertAsync_SourceAsModelBindingData_Works()
         {
             var binaryData = new BinaryData("{" +
                 "\"Connection\" : \"connection\"," +
@@ -105,20 +71,16 @@ namespace Microsoft.Azure.Functions.WorkerExtension.Tests
             });
 
             var context = new TestConverterContext(typeof(string), source);
-
             mock.Setup(c => c.ToTargetTypeAsync(typeof(string),"connection", "container", "BlobName")).ReturnsAsync("abc");
-
-            mock.Setup(c => c.GetBlobStringAsync("connection", "container", "BlobName")).ReturnsAsync("abc");
 
             var conversionResult = await mock.Object.ConvertAsync(context);
 
             Assert.Equal(ConversionStatus.Succeeded, conversionResult.Status);
-            //TestUtility.AssertIsTypeAndConvert<string>(conversionResult.Value);
         }
 
 
         [Fact]
-        public void GetBindingDataContentTest()
+        public void GetBindingDataContent_GrpcModelBindingData_Works()
         {
             var binaryData = new BinaryData("{" +
                 "\"Connection\" : \"connection\"," +
@@ -134,48 +96,17 @@ namespace Microsoft.Azure.Functions.WorkerExtension.Tests
                 ContentType = "application/json"
             });
 
-           // var context = new TestConverterContext(typeof(string), new Object()); // source);//_sourceMemory1);
+            var result = mock.Object.GetBindingDataContent(grpcModelBindingData);
 
-            var conversionResult = _converter.GetBindingDataContent(grpcModelBindingData);
+            Assert.Equal(3, result.Count);
 
-            Assert.Equal(3, conversionResult.Count);
-            conversionResult.TryGetValue(Constants.Connection, out var connectionName);
+            result.TryGetValue(Constants.Connection, out var connectionName);
+            result.TryGetValue(Constants.ContainerName, out var containerName);
+            result.TryGetValue(Constants.BlobName, out var blobName);
+
             Assert.Equal("connection", connectionName);
-
-            //Assert.Equal(ConversionStatus.Unhandled, conversionResult.Status);
-            //TestUtility.AssertIsTypeAndConvert<string>(conversionResult.Value);
+            Assert.Equal("container", containerName);
+            Assert.Equal("BlobName", blobName);
         }
-
-        [Fact]
-        public async Task ToTargetTypeTest()
-        {
-            var res = await _converter.ToTargetTypeAsync(typeof(Book), "connectionName", "containerName", "blobName");
-
-            var binaryData = new BinaryData("{" +
-                "\"Connection\" : \"connection\"," +
-                "\"ContainerName\" : \"container\"," +
-                "\"BlobName\" : \"BlobName\"" +
-                "}");
-
-            var grpcModelBindingData = new GrpcModelBindingData(new Worker.Grpc.Messages.ModelBindingData()
-            {
-                Version = "1.0",
-                Source = "AzureStorageBlobs",
-                Content = ByteString.CopyFrom(binaryData),
-                ContentType = "application/json"
-            });
-
-            // var context = new TestConverterContext(typeof(string), new Object()); // source);//_sourceMemory1);
-
-            var conversionResult = _converter.GetBindingDataContent(grpcModelBindingData);
-
-            Assert.Equal(3, conversionResult.Count);
-            conversionResult.TryGetValue(Constants.Connection, out var connectionName);
-            Assert.Equal("connection", connectionName);
-
-            //Assert.Equal(ConversionStatus.Unhandled, conversionResult.Status);
-            //TestUtility.AssertIsTypeAndConvert<string>(conversionResult.Value);
-        }
-
     }
 }

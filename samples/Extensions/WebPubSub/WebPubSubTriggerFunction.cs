463df82731494d91e95cdea045df910f28915f1b
Jialin Xin <jixin@microsoft.com>
2023-02-22 15:45:47 +0800

    simple trigger sample.

--- samples/Extensions/WebPubSub/WebPubSubTriggerFunction.cs
+++ samples/Extensions/WebPubSub/WebPubSubTriggerFunction.cs
@@ -1,34 +1,17 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 
 namespace SampleApp;
 
 public class WebPubSubTriggerFunction
 {
     // TODO: write a sample to return both output and trigger response.
-
-    //[Function("Broadcast")]
-    //[WebPubSubOutput(Hub = "chat")]
-    //public static async Task<UserEventResponse> Run(
-    //[WebPubSubTrigger("chat", WebPubSubEventType.User, "message")] UserEventRequest request,
-    //BinaryData data,
-    //WebPubSubDataType dataType)
-    //{
-    //    await actions.AddAsync(WebPubSubAction.CreateSendToAllAction(
-    //        BinaryData.FromString($"[{request.ConnectionContext.UserId}] {data.ToString()}"),
-    //        dataType));
-    //    return new UserEventResponse
-    //    {
-    //        Data = BinaryData.FromString("[SYSTEM] ack"),
-    //        DataType = WebPubSubDataType.Text
-    //    };
-    //}
+    [Function("Broadcast")]
+    public static UserEventResponse Run(
+    [WebPubSubTrigger("chat", WebPubSubEventType.User, "message")] UserEventRequest request)
+    {
+        return new UserEventResponse($"[SYSTEM ACK] Received client message. From: {request.ConnectionContext.ConnectionId}, Data: {request.Data}");
+    }
 }

d16ab2d3748713abadee688b4267a857a3edb97b
Lilian Kasem <likasem@microsoft.com>
2023-02-23 13:49:52 -0800

    Revert "Remove types, tests & logic related to the SDK-binding feature (#1374)"
    This reverts commit eac5b19a7a3e7adc437a903aa3d15177a123af72.

---
+++ src/DotNetWorker.Core/ModelBindingData.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker.Core
+{
+    /// <summary>
+    /// A representation of a Microsoft.Azure.WebJobs.ParameterBindingData
+    /// </summary>
+    public abstract class ModelBindingData
+    {
+        /// <summary>
+        /// Gets the version of the binding data content
+        /// </summary>
+        public abstract string Version { get; }
+
+        /// <summary>
+        /// Gets the extension source of the binding data i.e CosmosDB, AzureStorageBlobs
+        /// </summary>
+        public abstract string Source { get; }
+
+        /// <summary>
+        /// Gets the binding data content
+        /// </summary>
+        public abstract BinaryData Content { get; }
+
+        /// <summary>
+        /// Gets the content type of the binding data content i.e. "application/json"
+        /// </summary>
+        public abstract string ContentType { get; }
+    }
+}

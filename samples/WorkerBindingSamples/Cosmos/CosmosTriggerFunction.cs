92341177b4837214f5fda42857a511ea0a9c18ba
Lilian Kasem <likasem@microsoft.com>
2023-03-15 17:25:07 -0700

    Bump cosmos webjobs ext version

--- samples/WorkerBindingSamples/Cosmos/CosmosTriggerFunction.cs
+++ samples/WorkerBindingSamples/Cosmos/CosmosTriggerFunction.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 

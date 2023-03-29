88b594d52050b0c2fecbc35c2fbb59b5804b3148
Fabio Cavalcante <facaval@microsoft.com>
2023-03-29 15:55:31 -0700

    Adding support for conversion handler injection

---
+++ extensions/Worker.Extensions.Http/src/FromBodyConverter.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Converters;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Http.Converters
+{
+    internal class FromBodyConverter : IInputConverter
+    {
+        private readonly IFromBodyConversionHandler _conversionHandler;
+
+        public FromBodyConverter(IFromBodyConversionHandler? handler = null)
+        {
+            _conversionHandler = handler ?? new DefaultFromBodyConversionHandler();
+        }
+
+        public ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
+        {
+            return _conversionHandler.ConvertAsync(context);
+        }
+    }
+}

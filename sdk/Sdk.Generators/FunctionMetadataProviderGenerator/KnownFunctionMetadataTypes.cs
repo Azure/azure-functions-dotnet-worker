3ef5a037ea4202e5ae2ee7e959a8c5bd1fd343a2
Sarah Vu <sarahvu@microsoft.com>
2023-04-19 09:06:35 -0700

    refactor in response to comments

--- sdk/Sdk.Generators/FunctionMetadataProviderGenerator/KnownFunctionMetadataTypes.cs
+++ sdk/Sdk.Generators/FunctionMetadataProviderGenerator/KnownFunctionMetadataTypes.cs
@@ -1,29 +1,45 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.CodeAnalysis;
 
 namespace Microsoft.Azure.Functions.Worker.Sdk.Generators
 {
     internal readonly struct KnownFunctionMetadataTypes
     {
-        internal readonly INamedTypeSymbol? BindingAttribute;
-        internal readonly INamedTypeSymbol? OutputBindingAttribute;
-        internal readonly INamedTypeSymbol? FunctionName;
-        internal readonly INamedTypeSymbol? BindingPropertyNameAttribute;
-        internal readonly INamedTypeSymbol? DefaultValue;
-        internal readonly INamedTypeSymbol? HttpResponse;
-        internal readonly INamedTypeSymbol? HttpTriggerBinding;
+        private readonly Lazy<INamedTypeSymbol?> _bindingAttribute;
+        private readonly Lazy<INamedTypeSymbol?> _outputBindingAttribute;
+        private readonly Lazy<INamedTypeSymbol?> _functionName;
+        private readonly Lazy<INamedTypeSymbol?> _bindingPropertyNameAttribute;
+        private readonly Lazy<INamedTypeSymbol?> _defaultValue;
+        private readonly Lazy<INamedTypeSymbol?> _httpResponse;
+        private readonly Lazy<INamedTypeSymbol?> _httpTriggerBinding;
 
         internal KnownFunctionMetadataTypes(Compilation compilation)
         {
-            BindingAttribute = compilation.GetTypeByMetadataName(Constants.Types.BindingAttribute); // TODO: Find appropriate exception and/or error message.
-            OutputBindingAttribute = compilation.GetTypeByMetadataName(Constants.Types.OutputBindingAttribute);
-            FunctionName = compilation.GetTypeByMetadataName(Constants.Types.FunctionName);
-            BindingPropertyNameAttribute = compilation.GetTypeByMetadataName(Constants.Types.BindingPropertyNameAttribute);
-            DefaultValue = compilation.GetTypeByMetadataName(Constants.Types.DefaultValue);
-            HttpResponse = compilation.GetTypeByMetadataName(Constants.Types.HttpResponse);
-            HttpTriggerBinding = compilation.GetTypeByMetadataName(Constants.Types.HttpTriggerBinding);
+            _bindingAttribute = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.BindingAttribute)); // TODO: Find appropriate exception and/or error message.
+            _outputBindingAttribute = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.OutputBindingAttribute));
+            _functionName = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.FunctionName));
+            _bindingPropertyNameAttribute = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.BindingPropertyNameAttribute));
+            _defaultValue = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.DefaultValue));
+            _httpResponse = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.HttpResponse));
+            _httpTriggerBinding = new Lazy<INamedTypeSymbol?>(() => compilation.GetTypeByMetadataName(Constants.Types.HttpTriggerBinding));
         }
+
+        public INamedTypeSymbol? BindingAttribute { get => _bindingAttribute.Value; }
+
+        public INamedTypeSymbol? OutputBindingAttribute { get => _outputBindingAttribute.Value; }
+
+        public INamedTypeSymbol? FunctionName {  get => _functionName.Value; }
+
+        public INamedTypeSymbol? BindingPropertyNameAttribute { get => _bindingPropertyNameAttribute.Value; }
+
+        public INamedTypeSymbol? DefaultValue { get => _defaultValue.Value; }
+
+        public INamedTypeSymbol? HttpResponse { get => _httpResponse.Value; }
+
+        public INamedTypeSymbol? HttpTriggerBinding { get => _httpTriggerBinding.Value; }
     }
 }
--- sdk/Sdk.Generators/KnownTypes.cs
+++ sdk/Sdk.Generators/KnownTypes.cs
@@ -13,39 +13,65 @@ namespace Microsoft.Azure.Functions.Worker.Sdk.Generators
     // Trimmed version of https://github.com/dotnet/roslyn/blob/main/src/Features/Core/Portable/MakeMethodAsynchronous/AbstractMakeMethodAsynchronousCodeFixProvider.KnownTypes.cs
     internal readonly struct KnownTypes
     {
-        internal readonly INamedTypeSymbol TaskType;
-        internal readonly INamedTypeSymbol TaskOfTType;
-        internal readonly INamedTypeSymbol ValueTaskType;
-        internal readonly INamedTypeSymbol ValueTaskOfTTypeOpt;
-
-        internal readonly INamedTypeSymbol IEnumerable;
-        internal readonly INamedTypeSymbol IEnumerableGeneric; // IEnumerable<T>
-        internal readonly INamedTypeSymbol IEnumerableOfKeyValuePair;
-        internal readonly INamedTypeSymbol StringType;
-        internal readonly INamedTypeSymbol ByteArray;
-        internal readonly INamedTypeSymbol ByteType;
-        internal readonly INamedTypeSymbol VoidType;
-        internal readonly INamedTypeSymbol ReadOnlyMemoryOfBytes;
-        internal readonly INamedTypeSymbol LookupGeneric;
-        internal readonly INamedTypeSymbol DictionaryGeneric;
+        private readonly Lazy<INamedTypeSymbol> _taskType;
+        private readonly Lazy<INamedTypeSymbol> _taskOfTType;
+        private readonly Lazy<INamedTypeSymbol> _valueTaskType;
+        private readonly Lazy<INamedTypeSymbol> _valueTaskOfTTypeOpt;
+        private readonly Lazy<INamedTypeSymbol> _iEnumerable;
+        private readonly Lazy<INamedTypeSymbol> _iEnumerableGeneric;
+        private readonly Lazy<INamedTypeSymbol> _iEnumerableOfKeyValuePair;
+        private readonly Lazy<INamedTypeSymbol> _stringType;
+        private readonly Lazy<INamedTypeSymbol> _byteArray;
+        private readonly Lazy<INamedTypeSymbol> _byteType;
+        private readonly Lazy<INamedTypeSymbol> _voidType;
+        private readonly Lazy<INamedTypeSymbol> _readOnlyMemoryOfBytes;
+        private readonly Lazy<INamedTypeSymbol> _lookupGeneric;
+        private readonly Lazy<INamedTypeSymbol> _dictionaryGeneric;
 
         internal KnownTypes(Compilation compilation)
         {
-            TaskType = compilation.GetTypeByMetadataName(typeof(Task).FullName)!;
-            TaskOfTType = compilation.GetTypeByMetadataName(typeof(Task<>).FullName)!;
-            ValueTaskType = compilation.GetTypeByMetadataName(typeof(ValueTask).FullName)!;
-            ValueTaskOfTTypeOpt = compilation.GetTypeByMetadataName(typeof(ValueTask<>).FullName)!;
-
-            IEnumerable = compilation.GetTypeByMetadataName(typeof(IEnumerable).FullName)!;
-            IEnumerableGeneric = compilation.GetTypeByMetadataName(typeof(IEnumerable<>).FullName)!;
-            IEnumerableOfKeyValuePair = compilation.GetTypeByMetadataName(Constants.Types.IEnumerableOfKeyValuePair)!; // TODO: Revisit using typeof instead of string constant
-            StringType = compilation.GetTypeByMetadataName(typeof(string).FullName)!;
-            ByteArray = compilation.GetTypeByMetadataName(typeof(byte[]).FullName)!;
-            ByteType = compilation.GetTypeByMetadataName(typeof(byte).FullName)!;
-            VoidType = compilation.GetTypeByMetadataName(typeof(void).FullName)!;
-            ReadOnlyMemoryOfBytes = compilation.GetTypeByMetadataName(typeof(ReadOnlyMemory<byte>).FullName)!;
-            LookupGeneric = compilation.GetTypeByMetadataName(typeof(ILookup<,>).FullName)!;
-            DictionaryGeneric = compilation.GetTypeByMetadataName(typeof(IDictionary<,>).FullName)!;
+            _taskType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(Task).FullName)!);
+            _taskOfTType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(Task<>).FullName)!);
+            _valueTaskType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(ValueTask).FullName)!);
+            _valueTaskOfTTypeOpt = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(ValueTask<>).FullName)!);
+            _iEnumerable = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(IEnumerable).FullName)!);
+            _iEnumerableGeneric = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(IEnumerable<>).FullName)!);
+            _iEnumerableOfKeyValuePair = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(Constants.Types.IEnumerableOfKeyValuePair)!);
+            _stringType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(string).FullName)!);
+            _byteArray = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(byte[]).FullName)!);
+            _byteType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(byte).FullName)!);
+            _voidType = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(void).FullName)!);
+            _readOnlyMemoryOfBytes = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(ReadOnlyMemory<byte>).FullName)!);
+            _lookupGeneric = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(ILookup<,>).FullName)!);
+            _dictionaryGeneric = new Lazy<INamedTypeSymbol>(() => compilation.GetTypeByMetadataName(typeof(IDictionary<,>).FullName)!);
         }
+
+        public INamedTypeSymbol TaskType { get => _taskType.Value; }
+
+        public INamedTypeSymbol TaskOfTType { get => _taskOfTType.Value; }
+
+        public INamedTypeSymbol ValueTaskType { get => _valueTaskType.Value; }
+
+        public INamedTypeSymbol ValueTaskOfTTypeOpt { get => _valueTaskOfTTypeOpt.Value; }
+
+        public INamedTypeSymbol IEnumerable { get => _iEnumerable.Value; }
+
+        public INamedTypeSymbol IEnumerableGeneric { get => _iEnumerableGeneric.Value; } // IEnumerable<T>
+
+        public INamedTypeSymbol IEnumerableOfKeyValuePair { get => _iEnumerableOfKeyValuePair.Value; }
+
+        public INamedTypeSymbol StringType { get => _stringType.Value; }
+
+        public INamedTypeSymbol ByteArray { get => _byteArray.Value; }
+
+        public INamedTypeSymbol ByteType { get => _byteType.Value; }
+
+        public INamedTypeSymbol VoidType { get => _voidType.Value; }
+
+        public INamedTypeSymbol ReadOnlyMemoryOfBytes { get => _readOnlyMemoryOfBytes.Value; }
+
+        public INamedTypeSymbol LookupGeneric { get => _lookupGeneric.Value; }
+
+        public INamedTypeSymbol DictionaryGeneric { get => _dictionaryGeneric.Value; }
     }
 }

01e768f506423f10827fb28573d99f07306a187b
Sarah Vu <sarahvu@microsoft.com>
2023-04-25 11:32:04 -0700

    resolve comments

--- extensions/Worker.Extensions.Http.AspNetCore/src/HttpUriProvider.cs
+++ extensions/Worker.Extensions.Http.AspNetCore/src/HttpUriProvider.cs
@@ -10,13 +10,5 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore
         public static Lazy<Uri> HttpUri = new Lazy<Uri>(() => new Uri("http://localhost:" + Utilities.GetUnusedTcpPort().ToString()));
 
         public static string HttpUriString { get; } = HttpUri.Value.ToString();
-
-        public static Uri GetHttpUri()
-        {
-            // TODO: replace local host string
-            var uriString = "http://localhost:" + Utilities.GetUnusedTcpPort().ToString();
-
-            return new Uri(uriString);
-        }
     }
 }

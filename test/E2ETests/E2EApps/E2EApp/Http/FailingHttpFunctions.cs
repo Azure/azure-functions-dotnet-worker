c918b93f6bc73e707eb6541f74a404def7f6b250
njqdev <niklas.jq@outlook.com>
2023-05-06 16:43:48 +0200

    Add exception test for POCO without binding source

--- test/E2ETests/E2EApps/E2EApp/Http/FailingHttpFunctions.cs
+++ test/E2ETests/E2EApps/E2EApp/Http/FailingHttpFunctions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 ﻿using System;
@@ -6,6 +6,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.Functions.Worker.Pipeline;
 using Microsoft.Extensions.Logging;
+using static Microsoft.Azure.Functions.Worker.E2EApp.BasicHttpFunctions;
 
 namespace Microsoft.Azure.Functions.Worker.E2EApp
 {
@@ -17,6 +18,16 @@ namespace Microsoft.Azure.Functions.Worker.E2EApp
             FunctionContext context)
         {
             var logger = context.GetLogger(nameof(ExceptionFunction));
+            logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+            throw new Exception("This should never succeed!");
+        }        
+        
+        [Function(nameof(PocoWithoutBindingSource))]
+        public static HttpResponseData PocoWithoutBindingSource(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] CallerName caller,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger(nameof(PocoWithoutBindingSource));
             logger.LogInformation(".NET Worker HTTP trigger function processed a request");
             throw new Exception("This should never succeed!");
         }

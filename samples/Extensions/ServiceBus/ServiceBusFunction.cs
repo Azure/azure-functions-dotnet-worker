28147e5e524529c254244d0c316324f73036a943
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-05-17 12:42:17 -0700

    Add sample

--- samples/Extensions/ServiceBus/ServiceBusFunction.cs
+++ samples/Extensions/ServiceBus/ServiceBusFunction.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -20,6 +21,18 @@ namespace SampleApp
 
             var message = $"Output message created at {DateTime.Now}";
             return message;
+        }
+
+        [Function("ServiceBusSdkTypeBindingFunction")]
+        [ServiceBusOutput("outputQueue", Connection = "ServiceBusConnection")]
+        public static string Run([ServiceBusTrigger("queue", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message, FunctionContext context)
+        {
+            var logger = context.GetLogger("ServiceBusFunction");
+
+            logger.LogInformation($"Message body: {message.Body}");
+            logger.LogInformation($"Message ID: {message.MessageId}");
+
+            return $"Output message created at {DateTime.Now}";
         }
     }
 }

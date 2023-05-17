10b3cc0a62da1df0d6cad65c50360bf5117d728a
sarah <35204912+satvu@users.noreply.github.com>
2023-05-16 17:03:39 -0700

    Add attribute to signal that a binding supports function level retry (#1457)

---
+++ extensions/Worker.Extensions.Abstractions/src/KnownBindingCapabilities.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Abstractions
+{
+    /// <summary>
+    /// Class containing constant values representing known binding capabilities.
+    /// </summary>
+    public static class KnownBindingCapabilities
+    {
+        /// <summary>
+        /// Signals that a binding has function level retry support.
+        /// </summary>
+        public const string FunctionLevelRetry = "FunctionLevelRetry";
+    }
+}

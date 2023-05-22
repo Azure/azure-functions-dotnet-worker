c20ac617d4f96562a210be745956d2840be2afed
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-05-22 10:39:40 -0700

    Add ability to bind to SBReceivedMessage (#1313)

---
+++ extensions/Worker.Extensions.ServiceBus/src/ServiceBusExtensionStartup.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Core;
+using Microsoft.Extensions.Azure;
+
+[assembly: WorkerExtensionStartup(typeof(ServiceBusExtensionStartup))]
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    public class ServiceBusExtensionStartup : WorkerExtensionStartup
+    {
+        public override void Configure(IFunctionsWorkerApplicationBuilder applicationBuilder)
+        {
+            if (applicationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(applicationBuilder));
+            }
+
+            applicationBuilder.Services.AddAzureClientsCore(); // Adds AzureComponentFactory
+        }
+    }
+}

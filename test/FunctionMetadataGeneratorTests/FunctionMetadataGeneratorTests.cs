bcd31071d438287c352a2fbc72eae2cc29404049
Aishwarya Bhandari <aibhandari@microsoft.com>
2023-05-23 16:41:48 -0700

    fixing unit test

--- test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
+++ test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
@@ -268,7 +268,7 @@ namespace Microsoft.Azure.Functions.SdkTests
 
             AssertDictionary(extensions, new Dictionary<string, string>
             {
-                { "Microsoft.Azure.WebJobs.Extensions.Storage.Blobs", "5.1.0-beta.1" },
+                { "Microsoft.Azure.WebJobs.Extensions.Storage.Blobs", "5.1.1" },
             });
 
             var blobClientToBlobStringFunction = functions.Single(p => p.Name == "BlobClientToBlobStringFunction");
@@ -355,7 +355,7 @@ namespace Microsoft.Azure.Functions.SdkTests
 
             AssertDictionary(extensions, new Dictionary<string, string>
             {
-                { "Microsoft.Azure.WebJobs.Extensions.Storage.Blobs", "5.1.0-beta.1" },
+                { "Microsoft.Azure.WebJobs.Extensions.Storage.Blobs", "5.1.1" },
             });
 
             var blobStringToBlobClientEnumerable = functions.Single(p => p.Name == "BlobStringToBlobClientEnumerable");

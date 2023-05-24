8726761fbdb6d27b96a9b0fc04bfa794685a5e7e
Jacob Viau <javia@microsoft.com>
2023-05-24 14:57:50 -0700

    Remove Worker.Core assembly from DOTNET_STARTUP_HOOKS at startup (#1539)

--- src/DotNetWorker.Core/StartupHook.cs
+++ src/DotNetWorker.Core/StartupHook.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 /// <summary>
@@ -16,12 +19,17 @@ using System.Threading;
 /// </summary>
 internal class StartupHook
 {
+    const string StartupHooksEnvVar = "DOTNET_STARTUP_HOOKS";
+    private static readonly string _startupSeparator = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ";" : ":";
+    private static readonly string? _assemblyName = typeof(StartupHook).Assembly.GetName().Name;
+
     public static void Initialize()
     {
         // Time to wait between checks, in ms.
         const int SleepTime = 500;
         const int MaxWaitCycles = (60 * 1000) / SleepTime;
 
+        RemoveSelfFromStartupHooks();
         string? debuggerWaitEnabled = Environment.GetEnvironmentVariable("FUNCTIONS_ENABLE_DEBUGGER_WAIT");
         string? jsonOutputEnabled = Environment.GetEnvironmentVariable("FUNCTIONS_ENABLE_JSON_OUTPUT");
 #if NET5_0_OR_GREATER
@@ -61,5 +69,22 @@ internal class StartupHook
                 Thread.Sleep(SleepTime);
             }
         }
+    }
+
+    internal static void RemoveSelfFromStartupHooks()
+    {
+        string? startupHooks = Environment.GetEnvironmentVariable(StartupHooksEnvVar);
+        if (string.IsNullOrEmpty(startupHooks))
+        {
+            // If this call happened, we are clearly part of this environment variable.
+            // This is mostly to make strict-nulls happy.
+            return;
+        }
+
+        // netstandard2.0 has no StringSplitOptions overload.
+        IEnumerable<string> parts = startupHooks.Split(_startupSeparator[0])
+            .Where(x => !string.Equals(x, _assemblyName, StringComparison.Ordinal));
+        string newValue = string.Join(_startupSeparator, parts);
+        Environment.SetEnvironmentVariable(StartupHooksEnvVar, newValue);
     }
 }

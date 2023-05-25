601d44cc1e13262237053c3af46c94c1156122c9
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2023-05-25 14:35:54 -0700

    Table converter (#1506)
    * Add E2E tests for blob SDK type bindings (#1360)
    
    * Add analyzer for SupportsDeferredBindingAttribute (#1367)
    
    * Update SupportsDeferredBinding diagnostic code & update docs (#1377)
    
    * Revert "Remove types, tests & logic related to the SDK-binding feature (#1374)"
    
    This reverts commit eac5b19a7a3e7adc437a903aa3d15177a123af72.
    
    * Added unit tests for BlobStorageConverter (#1370)
    
    * Adding test project
    * Added tests
    
    * Initial changes
    
    * Updating metadata
    
    * Adding a test
    
    * Code refactoring
    
    * Cosmos DB converter for SDK-type support and samples (#1406)
    
    * Execution flow added
    
    * Update pipeline variables to include tags (#1427)
    
    * Update extension variables to not use build number with tags (#1429)
    
    * Execution flow
    
    * cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup execution flow
    
    * Added test
    
    * Adding tests
    
    * code cleanup
    
    * Code cleanup
    
    * code refactor
    
    * Added logic to check type before sending to converter
    
    * Updated tests
    
    * Removing advertised converters from options
    
    * Adding options back
    
    * Changes to check type
    
    * updating type check logic
    
    * correction in metadata generation
    
    * code cleanup
    
    * Test cleanup
    
    * Added changes to support poco
    
    * Metadata generation changes
    
    * Changes on Invocation side
    
    * correcting type check and tests
    
    * Test for poco invocation support
    
    * Added tests for poco invocation flows
    
    * Metadata generator minor update
    
    * Grpc definition update
    
    * Added poco and poco collection check
    
    * cleanup metadata generation
    
    * Adding support for type collection
    
    * code cleanup
    
    * Fixing metadata gen
    
    * Test fix
    
    * Grpc definition fix
    
    * Removing BlobContainerClient advertisement
    
    * Readding containerclient
    
    * Updated metadata generator
    
    * Changes as per latest design
    
    * code cleanup
    
    * updatin tests input
    
    * code cleanup
    
    * Cleanup Metadata generation
    
    * correcting converters fallback logic
    
    * Removing converter advertisement on cosmos trigger
    
    * Fixing failing test
    
    * test check in
    
    * Undo testing changes in sample app
    
    * spacing fix
    
    * updated tests
    
    * non secret changes
    
    * Addressing PR feedback
    
    * e2e test work
    
    * adding comments
    
    * Removed json deserialization attribute
    
    * Rename to AllowConverterFallback
    
    * Addressing feedback
    
    * Metada generation cleanup
    
    * Test cleanup
    
    * Addressing feedback
    
    * Updated grpcFunctionDefinition for multiple inputConverterAttribute
    
    * Removing cosmos converter registration
    
    * Address PR feedback
    
    * Address PR feedback
    
    * Test fix
    
    * Minor
    
    * addressing comments
    
    * getting e2e working
    
    * Implement bypass deferred binding (#1462)
    
    Implement bypass deferred binding
    
    * Build issue fix
    
    * comments
    
    * Update test/E2ETests/E2EApps/E2EApp/Table/TableInputBindingFunctions.cs
    
    Co-authored-by: Lilian Kasem <likasem@microsoft.com>
    
    * Adding check for PR in yml file (#1546)
    
    * addressing some more comments
    
    * dependency
    
    * one more dependency
    
    * trying to compile lol
    
    * seeing if tables project compiles with dependency
    
    * removing local instance
    
    * see if this compiles
    
    * trying to remove test
    
    * nvm adding it back
    
    * adding keys to nuget.config
    
    * trying to get feed
    
    * trying again
    
    * more yml
    
    * fixing spacing
    
    * ci.yml
    
    * Add ability to bind to SBReceivedMessage (#1313)
    
    * another change
    
    * trying again
    
    * formatting changes
    
    * changes
    
    * updating worker.sdk package
    
    * fixing unit test
    
    * getting unit tests to pass
    
    * fixing test
    
    ---------
    
    Co-authored-by: Lilian Kasem <likasem@microsoft.com>
    Co-authored-by: Surgupta <surgupta@microsoft.com>
    Co-authored-by: JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>

---
+++ extensions/Worker.Extensions.Tables/src/TypeConverters/TableEntityEnumerableConverter.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Microsoft.Azure.Functions.Worker.Converters;
+using Microsoft.Azure.Functions.Worker.Core;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+using Microsoft.Azure.Functions.Worker.Extensions.Tables.Config;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Tables.TypeConverters
+{
+    /// <summary>
+    /// Converter to bind Table type parameters.
+    /// </summary>
+    [SupportsDeferredBinding]
+    [SupportedConverterType(typeof(IEnumerable<TableEntity>))]
+    internal class TableEntityEnumerableConverter : TableConverterBase<IEnumerable<TableEntity>>
+    {
+        public TableEntityEnumerableConverter(IOptionsSnapshot<TablesBindingOptions> tableOptions, ILogger<TableEntityEnumerableConverter> logger)
+            : base(tableOptions, logger)
+        {
+        }
+
+        public override async ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
+        {
+            if (!CanConvert(context))
+            {
+                return ConversionResult.Unhandled();
+            }
+            try
+            {
+                var modelBindingData = context?.Source as ModelBindingData;
+                var tableData = GetBindingDataContent(modelBindingData);
+
+                var result = await ConvertModelBindingData(tableData);
+
+                if (result is not null)
+                {
+                    return ConversionResult.Success(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return ConversionResult.Failed(ex);
+            }
+
+            return ConversionResult.Unhandled();
+        }
+        
+        private async Task<IEnumerable<TableEntity>> ConvertModelBindingData(TableData content)
+        {
+            if (string.IsNullOrEmpty(content.TableName))
+            {
+                throw new ArgumentNullException(nameof(content.TableName));
+            }
+
+            if (content.RowKey != null && (content.Take > 0 || content.Filter != null))
+            {
+                throw new InvalidOperationException($"Row key {content.RowKey} cannot have a value if {content.Take} or {content.Filter} are defined");
+            }
+
+            return await GetEnumerableTableEntity(content);
+        }
+
+        private async Task<IEnumerable<TableEntity>> GetEnumerableTableEntity(TableData content)
+        {
+            var tableClient = GetTableClient(content.Connection, content.TableName!);
+            string? filter = content.Filter;
+
+            if (!string.IsNullOrEmpty(content.PartitionKey))
+            {
+                var partitionKeyPredicate = TableClient.CreateQueryFilter($"PartitionKey eq {content.PartitionKey}");
+                filter = !string.IsNullOrEmpty(content.Filter) ? $"{partitionKeyPredicate} and {content.Filter}" : partitionKeyPredicate;
+            }
+
+            int? maxPerPage = null;
+            if (content.Take > 0)
+            {
+                maxPerPage = content.Take;
+            }
+
+            int countRemaining = content.Take;
+
+            var entities = tableClient.QueryAsync<TableEntity>(
+               filter: filter,
+               maxPerPage: maxPerPage).ConfigureAwait(false);
+
+            List<TableEntity> bindingDataContent = new List<TableEntity>();
+
+            await foreach (var entity in entities)
+            {
+                countRemaining--;
+                bindingDataContent.Add(entity);
+                if (countRemaining == 0)
+                {
+                    break;
+                }
+            }
+            return bindingDataContent;
+        }
+    }
+}
+

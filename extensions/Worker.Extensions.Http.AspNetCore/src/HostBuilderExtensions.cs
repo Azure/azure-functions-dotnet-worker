072da918badbb0160650d0dc9a63a258984b91c0
Brett Samblanet <brettsam@microsoft.com>
2023-06-07 14:29:29 -0700

    adding ConfigureWebFunctionsWorker

--- extensions/Worker.Extensions.Http.AspNetCore/src/HostBuilderExtensions.cs
+++ extensions/Worker.Extensions.Http.AspNetCore/src/HostBuilderExtensions.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,14 +15,36 @@ namespace Microsoft.Extensions.Hosting
     /// </summary>
     public static class HostBuilderExtensions
     {
-
-        // TODO: This should be modified to extend IFunctionsWorkerApplicationBuilder.
         /// <summary>
         /// Configures the worker to use the ASP.NET Core integration, enabling advanced HTTP features.
         /// </summary>
         /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
         /// <returns>The same instance of the <see cref="IHostBuilder"/> for chaining.</returns>
-        public static IHostBuilder ConfigureAspNetCoreIntegration(this IHostBuilder builder)
+        public static IHostBuilder ConfigureWebFunctionsWorker(this IHostBuilder builder)
+        {
+            return builder.ConfigureWebFunctionsWorker(_ => { });
+        }
+
+        /// <summary>
+        /// Configures the worker to use the ASP.NET Core integration, enabling advanced HTTP features.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
+        /// <param name="configureWorker">The worker configure callback.</param>
+        /// <returns>The same instance of the <see cref="IHostBuilder"/> for chaining.</returns>
+        public static IHostBuilder ConfigureWebFunctionsWorker(this IHostBuilder builder, Action<IFunctionsWorkerApplicationBuilder> configureWorker)
+        {
+            builder.ConfigureFunctionsWorkerDefaults(workerAppBuilder =>
+            {
+                workerAppBuilder.UseAspNetCoreIntegration();
+                configureWorker?.Invoke(workerAppBuilder);
+            });
+
+            builder.ConfigureAspNetCoreIntegration();
+
+            return builder;
+        }
+
+        internal static IHostBuilder ConfigureAspNetCoreIntegration(this IHostBuilder builder)
         {
             builder.ConfigureWebHostDefaults(webBuilder =>
             {

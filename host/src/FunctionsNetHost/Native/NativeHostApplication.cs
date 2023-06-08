bc81eb1b0577a14c286b82ba334d00d0a6a70477
Shyju Krishnankutty <connectshyju@gmail.com>
2023-06-08 13:32:57 -0700

    Revert "Get the NativeApplication instance from pointer and call the methods instead of static method call."
    This reverts commit 2f54a1cdff58d2e47388063f4b029d3b874f36ed.

--- host/src/FunctionsNetHost/Native/NativeHostApplication.cs
+++ host/src/FunctionsNetHost/Native/NativeHostApplication.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using FunctionsNetHost.Grpc;
-using Microsoft.Azure.Functions.Worker.Grpc.Messages;
-
 namespace FunctionsNetHost
 {
     public sealed class NativeHostApplication
@@ -30,14 +27,6 @@ namespace FunctionsNetHost
             _workerHandle = grpcHandle;
 
             WorkerLoadStatusSignalManager.Instance.Signal.Set();
-        }
-
-        public unsafe void SendOutgoingMessage(byte* streamingMessage, int streamingMessageSize)
-        {
-            var span = new ReadOnlySpan<byte>(streamingMessage, streamingMessageSize);
-            var outboundMessageToHost = StreamingMessage.Parser.ParseFrom(span);
-
-            _ = MessageChannel.Instance.SendOutboundAsync(outboundMessageToHost);
         }
     }
 }

49be0626b060c4b332144af9042e3906aee3581c
Lilian Kasem <likasem@microsoft.com>
2023-06-13 10:59:12 -0700

    CosmosDB Extension v4.3.0 (#1625)

--- extensions/Worker.Extensions.CosmosDB/src/Properties/AssemblyInfo.cs
+++ extensions/Worker.Extensions.CosmosDB/src/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.CosmosDB", "4.2.0")]
+[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.CosmosDB", "4.3.0")]

8d35960e2316c84c6bd57fd1dd4d688c7da0aa41
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2023-06-16 16:52:22 -0700

    [SDK Type-Binding] EventGrid Converter (#1589)
    * changes
    
    * changes
    
    * fixing error
    
    * changes
    
    * fixing unit test
    
    * fixing unit test
    
    * fix tests
    
    * fixing metadata
    
    * changes
    
    * making build work
    
    * fixing function.metadata
    
    * addressing comments
    
    * removing dependency
    
    * changes
    
    * comments
    
    * changing state to failed if contextSource if null
    
    * addressing comments
    
    * addressing comments
    
    * comments
    
    * adding nre
    
    * nre

---
+++ extensions/Worker.Extensions.EventGrid/src/TypeConverters/EventGridEventConverter.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.EventGrid;
+using Microsoft.Azure.Functions.Worker.Converters;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.EventGrid.TypeConverters
+{
+    /// <summary>
+    /// Converter to bind to EventGridEvent or EventGridEvent[] parameter.
+    /// </summary>
+    [SupportedConverterType(typeof(EventGridEvent))]
+    [SupportedConverterType(typeof(EventGridEvent[]))]
+    internal class EventGridEventConverter : IInputConverter
+    {
+        public ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
+        {
+            if (context is null)
+            {
+                return new(ConversionResult.Failed(new ArgumentNullException(nameof(context))));
+            }
+
+            if (context?.TargetType != typeof(EventGridEvent) && context?.TargetType != typeof(EventGridEvent[]))
+            {
+                return new(ConversionResult.Unhandled());
+            }
+
+            return EventGridHelper.DeserializeToTargetType(context);
+        }
+    }
+}

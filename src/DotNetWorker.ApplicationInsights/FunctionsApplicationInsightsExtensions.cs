84f83cde22df0a9359367f85bd3dfcb8fa63dd0b
Jacob Viau <javia@microsoft.com>
2023-06-28 13:45:22 -0700

    Remove explicit AppInsights.WorkerService reference and APIs

--- src/DotNetWorker.ApplicationInsights/FunctionsApplicationInsightsExtensions.cs
+++ src/DotNetWorker.ApplicationInsights/FunctionsApplicationInsightsExtensions.cs
@@ -3,65 +3,27 @@
 
 using System;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.ApplicationInsights.WorkerService;
 using Microsoft.Azure.Functions.Worker.ApplicationInsights;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace Microsoft.Azure.Functions.Worker
 {
     public static class FunctionsApplicationInsightsExtensions
     {
-        /// <summary>
-        /// Adds Application Insights support by internally calling <see cref="ApplicationInsightsExtensions.AddApplicationInsightsTelemetryWorkerService(IServiceCollection)"/>.
-        /// </summary>
-        /// <param name="builder">The <see cref="IFunctionsWorkerApplicationBuilder"/></param>
-        /// <param name="configureOptions">Action to configure ApplicationInsights services.</param>
-        /// <returns>The <see cref="IFunctionsWorkerApplicationBuilder"/></returns>
-        public static IFunctionsWorkerApplicationBuilder AddApplicationInsights(this IFunctionsWorkerApplicationBuilder builder, Action<ApplicationInsightsServiceOptions>? configureOptions = null)
+        public static IServiceCollection ConfigureFunctionsApplicationInsights(this IServiceCollection services)
         {
-            builder.AddCommonServices();
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryModule, FunctionsTelemetryModule>());
-
-            builder.Services.AddApplicationInsightsTelemetryWorkerService(options =>
+            if (services == null)
             {
-                configureOptions?.Invoke(options);
-            });
+                throw new ArgumentNullException(nameof(services));
+            }
 
-            return builder;
-        }
-
-        /// <summary>
-        /// Adds the <see cref="ApplicationInsightsLoggerProvider"/> and disables the Functions host passthrough logger.
-        /// </summary>
-        /// <param name="builder">The <see cref="IFunctionsWorkerApplicationBuilder"/></param>
-        /// <param name="configureOptions">Action to configure ApplicationInsights logger.</param>
-        /// <returns>The <see cref="IFunctionsWorkerApplicationBuilder"/></returns>
-        public static IFunctionsWorkerApplicationBuilder AddApplicationInsightsLogger(this IFunctionsWorkerApplicationBuilder builder, Action<ApplicationInsightsLoggerOptions>? configureOptions = null)
-        {
-            builder.AddCommonServices();
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryInitializer, FunctionsTelemetryInitializer>());
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryModule, FunctionsTelemetryModule>());
 
             // Lets the host know that the worker is sending logs to App Insights. The host will now ignore these.
-            builder.Services.Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerApplicationInsightsLoggingEnabled"] = bool.TrueString);
-
-            builder.Services.AddLogging(logging =>
-            {
-                logging.AddApplicationInsights(options =>
-                {
-                    configureOptions?.Invoke(options);
-                });
-            });
-
-            return builder;
-        }
-
-        private static IFunctionsWorkerApplicationBuilder AddCommonServices(this IFunctionsWorkerApplicationBuilder builder)
-        {
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryInitializer, FunctionsTelemetryInitializer>());
-
-            return builder;
+            services.Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerApplicationInsightsLoggingEnabled"] = bool.TrueString);
+            return services;
         }
     }
 }

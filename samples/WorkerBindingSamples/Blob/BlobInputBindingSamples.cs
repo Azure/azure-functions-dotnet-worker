ba75fcde6946e1a9e9b0139141d21f508344e73a
Lilian Kasem <likasem@microsoft.com>
2023-06-28 15:54:32 -0700

    Update blob samples

--- samples/WorkerBindingSamples/Blob/BlobInputBindingSamples.cs
+++ samples/WorkerBindingSamples/Blob/BlobInputBindingSamples.cs
@@ -4,6 +4,7 @@
 using System.Net;
 using System.Text;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -19,6 +20,22 @@ namespace SampleApp
             _logger = logger;
         }
 
+        [Function(nameof(BlobInputContainerClientFunction))]
+        public async Task<HttpResponseData> BlobInputContainerClientFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [BlobInput("input-container")] BlobContainerClient client)
+        {
+            _logger.LogInformation("Blobs within container:");
+
+            var resultSegment = client.GetBlobsAsync();
+            await foreach (BlobItem blob in resultSegment)
+            {
+                _logger.LogInformation(blob.Name);
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+
         [Function(nameof(BlobInputClientFunction))]
         public async Task<HttpResponseData> BlobInputClientFunction(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
@@ -26,9 +43,7 @@ namespace SampleApp
         {
             var downloadResult = await client.DownloadContentAsync();
             var content = downloadResult.Value.Content.ToString();
-
             _logger.LogInformation("Blob content: {content}", content);
-
             return req.CreateResponse(HttpStatusCode.OK);
         }
 
@@ -39,7 +54,6 @@ namespace SampleApp
         {
             using var blobStreamReader = new StreamReader(stream);
             _logger.LogInformation("Blob content: {content}", await blobStreamReader.ReadToEndAsync());
-
             return req.CreateResponse(HttpStatusCode.OK);
         }
 
@@ -71,15 +85,66 @@ namespace SampleApp
         }
 
         [Function(nameof(BlobInputCollectionFunction))]
-        public HttpResponseData BlobInputCollectionFunction(
+        public async Task<HttpResponseData> BlobInputCollectionFunction(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            [BlobInput("input-container", IsBatched = true)] IEnumerable<BlobClient> blobs)
+            [BlobInput("input-container")] IEnumerable<BlobClient> blobs)
         {
-            _logger.LogInformation("Blobs within container:");
+            _logger.LogInformation("Content of all blobs within container:");
 
             foreach (BlobClient blob in blobs)
             {
-                _logger.LogInformation("Blob name: {blobName}, Container name: {containerName}", blob.Name, blob.BlobContainerName);
+                var downloadResult = await blob.DownloadContentAsync();
+                var content = downloadResult.Value.Content.ToString();
+                _logger.LogInformation(content);
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [Function(nameof(BlobInputCollectionSubdirectoryFunction))]
+        public async Task<HttpResponseData> BlobInputCollectionSubdirectoryFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [BlobInput("input-container/test")] IEnumerable<BlobClient> blobs)
+        {
+            _logger.LogInformation("Content of all blobs within the 'test' subdirectory in the container:");
+
+            foreach (BlobClient blob in blobs)
+            {
+                var downloadResult = await blob.DownloadContentAsync();
+                var content = downloadResult.Value.Content.ToString();
+                _logger.LogInformation(content);
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [Function(nameof(BlobInputStreamArrayFunction))]
+        public async Task<HttpResponseData> BlobInputStreamArrayFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [BlobInput("input-container")] Stream[] blobContent)
+        {
+            _logger.LogInformation("Content of all blobs within container:");
+
+            foreach (var item in blobContent)
+            {
+                using var blobStreamReader = new StreamReader(item);
+                _logger.LogInformation(await blobStreamReader.ReadToEndAsync());
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+        [Function(nameof(BlobInputBytesArrayFunction))]
+        public HttpResponseData BlobInputBytesArrayFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [BlobInput("input-container")] byte[][] blobContent)
+        {
+            _logger.LogInformation("Content of all blobs within container:");
+
+            foreach (var item in blobContent)
+            {
+                _logger.LogInformation(Encoding.Default.GetString(item));
             }
 
             return req.CreateResponse(HttpStatusCode.OK);
@@ -88,7 +153,7 @@ namespace SampleApp
         [Function(nameof(BlobInputStringArrayFunction))]
         public HttpResponseData BlobInputStringArrayFunction(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            [BlobInput("input-container", IsBatched = true)] string[] blobContent)
+            [BlobInput("input-container")] string[] blobContent)
         {
             _logger.LogInformation("Content of all blobs within container:");
 
@@ -103,13 +168,13 @@ namespace SampleApp
         [Function(nameof(BlobInputBookArrayFunction))]
         public HttpResponseData BlobInputBookArrayFunction(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            [BlobInput("input-container", IsBatched = true)] Book[] books)
+            [BlobInput("input-container")] Book[] books)
         {
             _logger.LogInformation("Content of all blobs within container:");
 
-            foreach (var item in books)
+            foreach (Book book in books)
             {
-                _logger.LogInformation(item.Name);
+                _logger.LogInformation(book.Name);
             }
 
             return req.CreateResponse(HttpStatusCode.OK);

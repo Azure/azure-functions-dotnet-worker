0e0f7bba23f17c29e5a4ea99bee5a23433536b68
Jacob Viau <javia@microsoft.com>
2023-07-06 16:32:34 -0700

    Add validation that ApplicationInsights SDK has been registered

--- src/DotNetWorker.ApplicationInsights/FunctionsApplicationInsightsExtensions.cs
+++ src/DotNetWorker.ApplicationInsights/FunctionsApplicationInsightsExtensions.cs
@@ -2,10 +2,15 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker.ApplicationInsights;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.Azure.Functions.Worker
 {
@@ -20,10 +25,45 @@ namespace Microsoft.Azure.Functions.Worker
 
             services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryInitializer, FunctionsTelemetryInitializer>());
             services.TryAddEnumerable(ServiceDescriptor.Singleton<ITelemetryModule, FunctionsTelemetryModule>());
+            services.AddOptions<FunctionsApplicationInsightsOptions>()
+                .Validate<IServiceProvider>(
+                    (_, sp) => sp.GetService<TelemetryClient>() is not null,
+                    "Application Insights SDK has not been added. Please add and configure the Application Insights SDK. See https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service for more information.");
+
+            services.AddHostedService<ApplicationInsightsValidationService>();
 
             // Lets the host know that the worker is sending logs to App Insights. The host will now ignore these.
             services.Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerApplicationInsightsLoggingEnabled"] = bool.TrueString);
             return services;
+        }
+
+        /// <summary>
+        /// Options for configuring Functions Application Insights.
+        /// </summary>
+        /// <remarks>
+        /// This is a private nested class as we have no public options to expose (yet). This is just a vessel to trigger validating that
+        /// an Application Insights SDK has also been configured.
+        /// When we do have options to configure, this can be moved to be a public top-level class.
+        /// </remarks>
+        private class FunctionsApplicationInsightsOptions
+        {
+        }
+
+        /// <summary>
+        /// This services is for a singular purpose: trigger validation of <see cref="FunctionsApplicationInsightsOptions" /> on startup.
+        /// </summary>
+        private class ApplicationInsightsValidationService : IHostedService
+        {
+            private readonly FunctionsApplicationInsightsOptions _options;
+
+            public ApplicationInsightsValidationService(IOptions<FunctionsApplicationInsightsOptions> options)
+            {
+                _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            }
+
+            public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+            public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
         }
     }
 }
--- test/DotNetWorkerTests/ApplicationInsights/ApplicationInsightsConfigurationTests.cs
+++ test/DotNetWorkerTests/ApplicationInsights/ApplicationInsightsConfigurationTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker.ApplicationInsights;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace Microsoft.Azure.Functions.Worker.Tests.ApplicationInsights;
@@ -11,34 +13,54 @@ namespace Microsoft.Azure.Functions.Worker.Tests.ApplicationInsights;
 public class ApplicationInsightsConfigurationTests
 {
     [Fact]
-    public void AddApplicationInsights_AddsDefaults()
+    public void AddApplicationInsights_SdkNotAdded_Throws()
     {
         var builder = new HostBuilder().ConfigureServices(
             services => services.ConfigureFunctionsApplicationInsights());
+
+        try
+        {
+            builder.Build().Run();
+        }
+        catch (OptionsValidationException)
+        {
+        }
+    }
+
+    [Fact]
+    public void AddApplicationInsights_AddsDefaults()
+    {
+        var builder = new HostBuilder().ConfigureServices(
+            services => services
+                .AddApplicationInsightsTelemetryWorkerService()
+                .ConfigureFunctionsApplicationInsights());
         Verify(builder);
     }
 
     [Fact]
     public void AddingServiceAndLogger_OnlyAddsServicesOnce()
     {
-        var builder = new HostBuilder().ConfigureServices(services =>
-                services.ConfigureFunctionsApplicationInsights().ConfigureFunctionsApplicationInsights());
+        var builder = new HostBuilder().ConfigureServices(
+            services => services
+                .AddApplicationInsightsTelemetryWorkerService()
+                .ConfigureFunctionsApplicationInsights()
+                .ConfigureFunctionsApplicationInsights());
         Verify(builder);
     }
 
     private static void Verify(IHostBuilder builder)
     {
-        IEnumerable<ServiceDescriptor> initializers = null;
-        IEnumerable<ServiceDescriptor> modules = null;
+        IEnumerable<Type> initializers = null;
+        IEnumerable<Type> modules = null;
 
         builder.ConfigureServices(services =>
         {
-            initializers = services.Where(s => s.ServiceType == typeof(ITelemetryInitializer));
-            modules = services.Where(s => s.ServiceType == typeof(ITelemetryModule));
+            initializers = services.Where(s => s.ServiceType == typeof(ITelemetryInitializer)).Select(x => x.ImplementationType);
+            modules = services.Where(s => s.ServiceType == typeof(ITelemetryModule)).Select(x => x.ImplementationType);
         });
 
         builder.Build();
-        Assert.Collection(initializers, t => Assert.Equal(typeof(FunctionsTelemetryInitializer), t.ImplementationType));
-        Assert.Collection(modules, t => Assert.Equal(typeof(FunctionsTelemetryModule), t.ImplementationType));
+        Assert.Contains(typeof(FunctionsTelemetryInitializer), initializers);
+        Assert.Contains(typeof(FunctionsTelemetryModule), modules);
     }
 }

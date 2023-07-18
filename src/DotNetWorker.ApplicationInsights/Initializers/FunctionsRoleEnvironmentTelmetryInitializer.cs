a17eefbd802caf7a59dc0591a24d6cf2033f0a9b
Brett Samblanet <brettsam@microsoft.com>
2023-07-18 14:02:07 -0700

    App Insights: improving property mappings; adding role details (#1755)

---
+++ src/DotNetWorker.ApplicationInsights/Initializers/FunctionsRoleEnvironmentTelmetryInitializer.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace Microsoft.Azure.Functions.Worker.ApplicationInsights.Initializers
+{
+    // This class was taken largely from https://raw.githubusercontent.com/Microsoft/ApplicationInsights-dotnet-server/91016d62f3181e10d4cf589ef8fd64dadb6b54a2/Src/WindowsServer/WindowsServer.Shared/AzureWebAppRoleEnvironmentTelemetryInitializer.cs, 
+    // but refactored so that it did not use WEBSITE_HOSTNAME, which is determined to be unreliable for functions during slot swaps.
+
+    /// <summary>
+    /// A telemetry initializer that will gather Azure Web App Role Environment context information.
+    /// </summary>    
+    internal class FunctionsRoleEnvironmentTelemetryInitializer : ITelemetryInitializer
+    {
+        internal const string AzureWebsiteName = "WEBSITE_SITE_NAME";
+        internal const string AzureWebsiteSlotName = "WEBSITE_SLOT_NAME";
+        internal const string AzureWebsiteCloudRoleName = "WEBSITE_CLOUD_ROLENAME";
+        private const string DefaultProductionSlotName = "production";
+        private const string WebAppSuffix = ".azurewebsites.net";
+
+        private readonly ConcurrentDictionary<string, string> _siteNodeNames = new(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Initializes <see cref="ITelemetry" /> device context.
+        /// </summary>
+        /// <param name="telemetry">The telemetry to initialize.</param>
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry == null)
+            {
+                return;
+            }
+
+            var siteSlotName = new Lazy<string?>(() =>
+            {
+                // We cannot cache these values as the environment variables can change on the fly.
+                return GetAzureWebsiteUniqueSlotName();
+            });
+
+            var websiteCloudRoleName = Environment.GetEnvironmentVariable(AzureWebsiteCloudRoleName);
+
+            if (!string.IsNullOrEmpty(websiteCloudRoleName))
+            {
+                telemetry.Context.Cloud.RoleName = websiteCloudRoleName;
+            }
+            else
+            {
+                telemetry.Context.Cloud.RoleName = siteSlotName.Value;
+            }
+
+            var internalContext = telemetry.Context.GetInternalContext();
+            if (!string.IsNullOrEmpty(siteSlotName.Value))
+            {
+                internalContext.NodeName = _siteNodeNames.GetOrAdd(siteSlotName.Value!, p =>
+                {
+                    // maintain previous behavior of node having the full url
+                    return p += WebAppSuffix;
+                });
+            }
+        }
+
+        /// <summary>
+        /// Gets a value that uniquely identifies the site and slot.
+        /// </summary>
+        private static string? GetAzureWebsiteUniqueSlotName()
+        {
+            var name = Environment.GetEnvironmentVariable(AzureWebsiteName);
+            var slotName = Environment.GetEnvironmentVariable(AzureWebsiteSlotName);
+
+            if (!string.IsNullOrEmpty(slotName) &&
+                !string.Equals(slotName, DefaultProductionSlotName, StringComparison.OrdinalIgnoreCase))
+            {
+                name += $"-{slotName}";
+            }
+
+            return name?.ToLowerInvariant();
+        }
+    }
+}
---
+++ src/DotNetWorker.ApplicationInsights/Initializers/FunctionsTelemetryInitializer.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace Microsoft.Azure.Functions.Worker.ApplicationInsights.Initializers
+{
+    internal class FunctionsTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly string _roleInstanceName;
+        private readonly string? _sdkVersion;
+        private static readonly IDictionary<string, string> _mappings = new Dictionary<string, string>()
+        {
+            { "faas.execution", "InvocationId" }, // used by worker ActivitySource
+            { "AzureFunctions_InvocationId", "InvocationId" } // used by log scope
+        };
+
+        internal FunctionsTelemetryInitializer(FunctionsRoleInstanceProvider roleInstanceProvider, string? sdkVersion)
+        {
+            _roleInstanceName = roleInstanceProvider.GetRoleInstanceName();
+            _sdkVersion = sdkVersion;
+        }
+
+        public FunctionsTelemetryInitializer(FunctionsRoleInstanceProvider roleInstanceProvider) :
+            this(roleInstanceProvider, GetSdkVersion())
+        {
+        }
+
+        private static string? GetSdkVersion()
+        {
+            var version = typeof(FunctionsTelemetryInitializer).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+            return version == null ? null : $"azurefunctions-netiso: {version}";
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry is null)
+            {
+                return;
+            }
+
+            if (_sdkVersion is not null)
+            {
+                telemetry.Context.GetInternalContext().SdkVersion = _sdkVersion;
+            }
+
+            if (_roleInstanceName is not null)
+            {
+                telemetry.Context.Cloud.RoleInstance = _roleInstanceName;
+            }
+
+            if (telemetry is ISupportProperties supportProperties)
+            {
+                CopyWellKnownProperties(supportProperties);
+            }
+        }
+
+        // For parity with how the Functions host writes to App Insights, make sure we translate some
+        // well-known worker keys into the ones used by the host.
+        internal static void CopyWellKnownProperties(ISupportProperties supportProperties)
+        {
+            foreach (var mapping in _mappings)
+            {
+                if (supportProperties.Properties.TryGetValue(mapping.Key, out string propValue))
+                {
+                    supportProperties.Properties[mapping.Value] = propValue;
+                }
+            }
+        }
+    }
+}

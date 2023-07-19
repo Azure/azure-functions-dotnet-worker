aa3ffd87b86410fbe7662466f07e10bdb19d0191
Lilian Kasem <likasem@microsoft.com>
2023-07-19 10:17:10 -0700

    Update shared extension exceptions to include actual value

--- extensions/Worker.Extensions.Shared/Exceptions/InvalidBindingSourceException.cs
+++ extensions/Worker.Extensions.Shared/Exceptions/InvalidBindingSourceException.cs
@@ -13,16 +13,20 @@ namespace Microsoft.Azure.Functions.Worker.Extensions
         /// <summary>
         /// Initializes a new instance of the InvalidBindingSourceException class with a specified error message.
         /// </summary>
-        /// <param name="source">The source(s) that is supported.</param>
-        public InvalidBindingSourceException(string source) : base($"Unexpected binding source. Only '{source}' is supported.") { }
+        /// <param name="actualSource">The source that is being provided by ModelBindingData</param>
+        /// <param name="expectedSource">The source(s) that is supported.</param>
+        public InvalidBindingSourceException(string actualSource, string expectedSource)
+            : base($"Unexpected binding source '{actualSource}'. Only '{expectedSource}' is supported.") { }
 
         /// <summary>
         /// Initializes a new instance of the InvalidBindingSourceException class with a specified error message
         /// and a reference to the inner exception that is the cause of this exception.
         /// </summary>
-        /// <param name="source">The source(s) that is supported.</param>
+        /// <param name="actualSource">The source that is being provided by ModelBindingData</param>
+        /// <param name="expectedSource">The source(s) that is supported.</param>
         /// <param name="innerException">The exception that is the cause of the current exception
-        /// or a null reference if no inner exception is specified..</param>
-        public InvalidBindingSourceException(string source, Exception? innerException) : base($"Unexpected binding source. Only '{source}' is supported.", innerException) { }
+        /// or a null reference if no inner exception is specified.</param>
+        public InvalidBindingSourceException(string actualSource, string expectedSource, Exception? innerException)
+            : base($"Unexpected binding source '{actualSource}'. Only '{expectedSource}' is supported.", innerException) { }
     }
 }

2d51624b3ffaeb9e0ec0f30871fc59bec5f0dcc2
Lilian Kasem <likasem@microsoft.com>
2023-07-24 13:48:25 -0700

    Add test without ext dependencies

--- test/Sdk.Analyzers.Tests/BindingTypeCodeRefactoringProviderTests.cs
+++ test/Sdk.Analyzers.Tests/BindingTypeCodeRefactoringProviderTests.cs
@@ -25,14 +25,88 @@ namespace Sdk.Analyzers.Tests
             ReferenceSource.FromAssembly(Assembly.Load("Microsoft.Azure.Functions.Worker.Core, Version=1.13.0.0").Location),
             ReferenceSource.FromAssembly(Assembly.Load("Microsoft.Azure.Functions.Worker.Extensions.Abstractions, Version=1.3.0.0")),
 
-            ReferenceSource.FromAssembly(Assembly.Load("Azure.Data.Tables, Version=12.8.0.0").Location),
-            ReferenceSource.FromAssembly(Assembly.Load("Microsoft.Azure.Functions.Worker.Extensions.Tables, Version=1.2.0.0").Location),
-
-            ReferenceSource.FromAssembly(Assembly.Load("Azure.Messaging.ServiceBus, Version=7.14.0.0").Location),
-            ReferenceSource.FromAssembly(Assembly.Load("Microsoft.Azure.Functions.Worker.Extensions.ServiceBus, Version=5.10.0.0").Location),
+            // Uncomment when reenabling tables test
+            // ReferenceSource.FromAssembly(Assembly.Load("Azure.Data.Tables, Version=12.8.0.0").Location),
+            // ReferenceSource.FromAssembly(Assembly.Load("Microsoft.Azure.Functions.Worker.Extensions.Tables, Version=1.2.0.0").Location)
         };
 
-        [Theory (Skip = "Pending tables release")]
+        [Theory]
+        [InlineData("string", 0)]
+        [InlineData("bool", 1)]
+        public void TestBinding_SuggestsCodeRefactor(string supportedType, int index)
+        {
+            string testCode = @"
+                using System;
+                using Microsoft.Azure.Functions.Worker;
+                using Microsoft.Azure.Functions.Worker.Core;
+                using Microsoft.Azure.Functions.Worker.Converters;
+                using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+                namespace Microsoft.Azure.Functions.Worker
+                {
+                    [ConverterFallbackBehavior(ConverterFallbackBehavior.Disallow)]
+                    [InputConverter(typeof(TestConverter))]
+                    public class TestTriggerAttribute : TriggerBindingAttribute
+                    {
+                    }
+
+                    [SupportsDeferredBinding]
+                    [SupportedTargetType(typeof(string))]
+                    [SupportedTargetType(typeof(bool))]
+                    public class TestConverter
+                    {
+                    }
+                }
+
+                namespace FunctionApp
+                {
+                    public class SomeFunction
+                    {
+                        [Function(nameof(SomeFunction))]
+                        public void Run([TestTrigger()] [|int message|])
+                        {
+                        }
+                    }
+                }";
+
+            string expectedCode = $@"
+                using System;
+                using Microsoft.Azure.Functions.Worker;
+                using Microsoft.Azure.Functions.Worker.Core;
+                using Microsoft.Azure.Functions.Worker.Converters;
+                using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+                namespace Microsoft.Azure.Functions.Worker
+                {{
+                    [ConverterFallbackBehavior(ConverterFallbackBehavior.Disallow)]
+                    [InputConverter(typeof(TestConverter))]
+                    public class TestTriggerAttribute : TriggerBindingAttribute
+                    {{
+                    }}
+
+                    [SupportsDeferredBinding]
+                    [SupportedTargetType(typeof(string))]
+                    [SupportedTargetType(typeof(bool))]
+                    public class TestConverter
+                    {{
+                    }}
+                }}
+
+                namespace FunctionApp
+                {{
+                    public class SomeFunction
+                    {{
+                        [Function(nameof(SomeFunction))]
+                        public void Run([TestTrigger()] {supportedType} message)
+                        {{
+                        }}
+                    }}
+                }}";
+
+            TestCodeRefactoring(testCode, expectedCode, index);
+        }
+
+        [Theory (Skip="Pending Tables release")]
         [InlineData("TableClient", 0)]
         [InlineData("TableEntity", 1)]
         public void TableInput_SuggestsCodeRefactor(string supportedType, int index)
@@ -64,46 +138,6 @@ namespace Sdk.Analyzers.Tests
                     {{
                         [Function(nameof(SomeFunction))]
                         public static void Run([TableInput(""input"")] {supportedType} message)
-                        {{
-                        }}
-                    }}
-                }}";
-
-            TestCodeRefactoring(testCode, expectedCode, index);
-        }
-
-        [Theory (Skip="Pending ServiceBusReceivedMessage release")]
-        [InlineData("ServiceBusReceivedMessage", 0)]
-        [InlineData("ServiceBusReceivedMessage[]", 1)]
-        public void ServiceBusTrigger_SuggestsCodeRefactor(string supportedType, int index)
-        {
-            string testCode = @"
-                using System;
-                using Azure.Messaging.ServiceBus;
-                using Microsoft.Azure.Functions.Worker;
-
-                namespace FunctionApp
-                {
-                    public static class SomeFunction
-                    {
-                        [Function(nameof(SomeFunction))]
-                        public static void Run([ServiceBusTrigger(""input"")] [|string message|])
-                        {
-                        }
-                    }
-                }";
-
-            string expectedCode = $@"
-                using System;
-                using Azure.Messaging.ServiceBus;
-                using Microsoft.Azure.Functions.Worker;
-
-                namespace FunctionApp
-                {{
-                    public static class SomeFunction
-                    {{
-                        [Function(nameof(SomeFunction))]
-                        public static void Run([ServiceBusTrigger(""input"")] {supportedType} message)
                         {{
                         }}
                     }}

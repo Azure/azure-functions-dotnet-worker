3829bbd22d3252306a9c2b9a6cd181e642af6142
Fabio Cavalcante <facaval@microsoft.com>
2023-07-26 11:44:00 -0700

    Implementation of the FromBody binding converter for the ASP.NET Core Integration

---
+++ extensions/Worker.Extensions.Http.AspNetCore/src/FromBodyConverstionFeature.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Azure.Functions.Worker.Extensions.Http.Converters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore
+{
+    internal sealed class FromBodyConverstionFeature : IFromBodyConversionFeature
+    {
+        public static FromBodyConverstionFeature Instance { get; } = new();
+
+        public async ValueTask<object?> ConvertAsync(FunctionContext context, Type targetType)
+        {
+            var httpContext = context.GetHttpContext()
+                ?? throw new InvalidOperationException($"The '{nameof(FromBodyConverstionFeature)} expects an '{nameof(HttpContext)}' instance in the current context.");
+
+            var metadata = httpContext.RequestServices
+                           .GetService<IModelMetadataProvider>()?
+                           .GetMetadataForType(targetType);
+
+            if (metadata is null)
+            {
+                context.GetLogger<FromBodyConverstionFeature>()
+                    .LogWarning("Unable to resolve a model metadata provider for the target type ({TargetType}).", targetType);
+
+                return null;
+            }
+
+            var binderModelName = metadata.BinderModelName ?? string.Empty;
+
+            var modelBinder = (httpContext.RequestServices
+               .GetService<IModelBinderFactory>()?
+               .CreateBinder(new ModelBinderFactoryContext
+               {
+                   Metadata = metadata,
+                   BindingInfo = new BindingInfo()
+                   {
+                       BinderModelName = binderModelName,
+                       BindingSource = BindingSource.Body,
+                       BinderType = metadata.BinderType,
+                       PropertyFilterProvider = metadata.PropertyFilterProvider,
+                   },
+                   CacheToken = null
+               })) 
+               ?? throw new InvalidOperationException($"Unable to resolve a request body model binder for the target type ({metadata.BinderType}.");
+
+            var modelBindingContext = new DefaultModelBindingContext
+            {
+                ModelMetadata = metadata,
+                ModelName = binderModelName,
+                BindingSource = BindingSource.Body,
+                ModelState = new ModelStateDictionary(),
+                ActionContext = new ActionContext
+                {
+                    HttpContext = httpContext
+                }
+            };
+
+            await modelBinder.BindModelAsync(modelBindingContext);
+
+            if (modelBindingContext.Result.IsModelSet)
+            {
+                return modelBindingContext.Result.Model;
+            }
+            else if (!modelBindingContext.ModelState.IsValid)
+            {
+                var messageBuilder = new StringBuilder();
+                foreach (var key in modelBindingContext.ModelState.Keys)
+                {
+                    var dictionary = modelBindingContext.ModelState[key];
+
+                    if (dictionary == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (var error in dictionary.Errors)
+                    {
+                        messageBuilder.AppendLine(error.ErrorMessage);
+                    }
+                }
+
+                throw new InvalidOperationException(messageBuilder.ToString());
+            }
+
+            context.GetLogger<FromBodyConverstionFeature>()
+                .LogWarning("Unable to bind the request body to the target type ({TargetType}).", targetType);
+
+            return null;
+        }
+    }
+}

2dc54c30dd0edecd7036a6f93360e8660b785942
Fabio Cavalcante <facaval@microsoft.com>
2023-07-26 11:44:00 -0700

    Implementation of the FromBody binding converter for the ASP.NET Core Integration

---
+++ samples/AspNetIntegration/BodyBindingHttpTrigger.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
+
+namespace AspNetIntegration
+{
+    public class BodyBindingHttpTrigger
+    {
+        [Function(nameof(BodyBindingHttpTrigger))]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [FromBody] Person person)
+        {
+            return new OkObjectResult(person);
+        }
+    }
+
+    public record Person(string Name, int Age);
+}

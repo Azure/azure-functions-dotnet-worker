c042783460b5f2a2e0f2004b621d2fd97c33de61
Takaaki Suzuki <xin9le@live.jp>
2023-08-09 11:30:07 +0900

    Adds FunctionMiddlewareAttribute<T>

---
+++ src/DotNetWorker.Core/Pipeline/FunctionMiddlewareAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.Functions.Worker.Middleware
+{
+    /// <summary>
+    /// Represents IFunctionsWorkerMiddleware that operates on an attribute-based.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class FunctionMiddlewareAttribute<T> : Attribute
+        where T : class, IFunctionsWorkerMiddleware
+    { }
+}

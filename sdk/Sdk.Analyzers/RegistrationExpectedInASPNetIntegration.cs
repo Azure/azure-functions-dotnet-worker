d9c1665873cfe5621afef37ed216a1023ccd9678
Surgupta <surgupta@microsoft.com>
2023-09-20 13:50:54 -0700

    Add more tests and code cleanup

---
+++ sdk/Sdk.Analyzers/RegistrationExpectedInASPNetIntegration.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.Azure.Functions.Worker.Sdk.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class RegistrationExpectedInASPNetIntegration : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DiagnosticDescriptors.CorrectRegistrationExpectedInAspNetIntegration);
+
+        private const string ExpectedRegistrationMethod = "ConfigureFunctionsWebApplication";
+        private const string AspNetExtensionAssemblyName = "Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore";
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
+            context.RegisterSymbolAction(AnalyzeMethod, SymbolKind.Namespace);
+        }
+
+        private static void AnalyzeMethod(SymbolAnalysisContext context)
+        {
+            bool isAspNetAssembly = context.Compilation.ReferencedAssemblyNames.Where(assembly => assembly.Name.Equals(AspNetExtensionAssemblyName)).Any();
+            SyntaxReference syntaxReference = context.Symbol.DeclaringSyntaxReferences.FirstOrDefault();
+            string code = syntaxReference.SyntaxTree.GetText().ToString();
+
+            if (isAspNetAssembly && !code.Contains(ExpectedRegistrationMethod))
+            {
+                var location = Location.Create(syntaxReference.SyntaxTree, syntaxReference.Span);
+                var diagnostic = Diagnostic.Create(DiagnosticDescriptors.CorrectRegistrationExpectedInAspNetIntegration, location, ExpectedRegistrationMethod);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
--- test/Sdk.Analyzers.Tests/RegistrationExpectedInAspNetIntegrationTests.cs
+++ test/Sdk.Analyzers.Tests/RegistrationExpectedInAspNetIntegrationTests.cs
@@ -1,29 +1,29 @@
 ﻿using Xunit;
-using AnalyzerTest = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerTest<Microsoft.Azure.Functions.Worker.Sdk.Analyzers.RegistrationInASPNetIntegration, Microsoft.CodeAnalysis.Testing.Verifiers.XUnitVerifier>;
-using Verify = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<Microsoft.Azure.Functions.Worker.Sdk.Analyzers.RegistrationInASPNetIntegration>;
+using AnalyzerTest = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerTest<Microsoft.Azure.Functions.Worker.Sdk.Analyzers.RegistrationExpectedInASPNetIntegration, Microsoft.CodeAnalysis.Testing.Verifiers.XUnitVerifier>;
+using Verify = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<Microsoft.Azure.Functions.Worker.Sdk.Analyzers.RegistrationExpectedInASPNetIntegration>;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Testing;
 using System.Collections.Immutable;
 
 namespace Sdk.Analyzers.Tests
 {
-    public class RegistrationAspNetIntegrationTests
+    public class RegistrationExpectedInAspNetIntegrationTests
     {
         private const string ExpectedRegistrationMethod = "ConfigureFunctionsWebApplication";
 
         [Fact]
-        public async Task ASPNetIntegration_MissingRegistration_Diagnostics_Expected()
+        public async Task AspNetIntegration_MissingRegistration_Diagnostics_Expected()
         {
             string testCode = @"
-                using System.Linq;
-                using System.Threading.Tasks;
-                using Microsoft.Azure.Functions.Worker;
-                using Microsoft.Extensions.DependencyInjection;
-                using Microsoft.Extensions.Hosting;
-                using Microsoft.Extensions.Logging;
-
                 namespace AspNetIntegration
                 {
+                    using System.Linq;
+                    using System.Threading.Tasks;
+                    using Microsoft.Azure.Functions.Worker;
+                    using Microsoft.Extensions.DependencyInjection;
+                    using Microsoft.Extensions.Hosting;
+                    using Microsoft.Extensions.Logging;
+
                     class Program
                     {
                         static void Main(string[] args)
@@ -53,13 +53,78 @@ namespace Sdk.Analyzers.Tests
 
             test.ExpectedDiagnostics.Add(Verify.Diagnostic()
                             .WithSeverity(Microsoft.CodeAnalysis.DiagnosticSeverity.Error)
+                            .WithSpan(2, 17, 22, 18)
+                            .WithArguments(ExpectedRegistrationMethod));
+
+            await test.RunAsync();
+        }
+
+
+        [Fact]
+        public async Task AspNetIntegrationWithTrigger_MissingRegistration_Diagnostics_Expected()
+        {
+            string testCode = @"
+                namespace AspNetIntegration
+                {
+                    using System.Linq;
+                    using System.Threading.Tasks;
+                    using Microsoft.Azure.Functions.Worker;
+                    using Microsoft.Extensions.DependencyInjection;
+                    using Microsoft.Extensions.Hosting;
+                    using Microsoft.Extensions.Logging;
+
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            var host = new HostBuilder()
+                                .ConfigureFunctionsWorkerDefaults()
+                                .Build();
+
+                            host.Run();
+                        }
+                    }
+                }
+
+                namespace AspNetIntegration
+                {
+                    using Microsoft.Azure.Functions.Worker;
+                    using Microsoft.Azure.Functions.Worker.Http;
+
+                    public class FunctionHttpTrigger
+                    {
+                        [Function(nameof(FunctionHttpTrigger))]
+                        public void Run([HttpTrigger(AuthorizationLevel.Anonymous, ""post"")] HttpRequestData req)
+                        {
+                        }
+                    }
+                }";
+
+            var test = new AnalyzerTest
+            {
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net60.WithPackages(ImmutableArray.Create(
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker", "1.19.0"),
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker.Sdk", "1.14.1"),
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker.Extensions.Storage.Blobs", "6.0.0"),
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore", "1.0.0"),
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker.Extensions.Abstractions", "5.0.0"),
+                    new PackageIdentity("Microsoft.Extensions.Hosting.Abstractions", "6.0.0"),
+                    new PackageIdentity("Microsoft.Azure.Functions.Worker.Extensions.Http", "3.1.0")
+                    )),
+
+                TestCode = testCode
+            };
+
+            test.ExpectedDiagnostics.Add(Verify.Diagnostic()
+                            .WithSeverity(Microsoft.CodeAnalysis.DiagnosticSeverity.Error)
+                            .WithSpan(2, 17, 22, 18)
                             .WithArguments(ExpectedRegistrationMethod));
 
             await test.RunAsync();
         }
 
         [Fact]
-        public async Task NotASPNetIntegration_Diagnostics_NotExpected()
+        public async Task NotAspNetIntegration_Diagnostics_NotExpected()
         {
             string testCode = @"
                 using System;
@@ -98,7 +163,7 @@ namespace Sdk.Analyzers.Tests
         }
 
         [Fact]
-        public async Task ASPNetIntegration_WithRegistration_Diagnostics_NotExpected()
+        public async Task AspNetIntegration_WithRegistration_Diagnostics_NotExpected()
         {
             string testCode = @"
                 using System.Linq;
@@ -146,7 +211,7 @@ namespace Sdk.Analyzers.Tests
         }
 
         [Fact]
-        public async Task ASPNetIntegration_WithMiddleWare_WithRegistration_Diagnostics_NotExpected()
+        public async Task AspNetIntegration_WithMiddleWare_WithRegistration_Diagnostics_NotExpected()
         {
             string testCode = @"
                 using System.Linq;

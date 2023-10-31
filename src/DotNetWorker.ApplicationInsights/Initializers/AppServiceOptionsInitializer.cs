4e8849e632a77a421b268733c8074a418cf5d84d
Brett Samblanet <brettsam@microsoft.com>
2023-10-31 13:21:29 -0700

    [ApplicationInsights] removing env var reads from hot path (#1996)

---
+++ src/DotNetWorker.ApplicationInsights/Initializers/AppServiceOptionsInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker.ApplicationInsights.Initializers;
+using Microsoft.Extensions.Options;
+
+internal class AppServiceOptionsInitializer : IConfigureOptions<AppServiceOptions>
+{
+    internal const string AzureWebsiteName = "WEBSITE_SITE_NAME";
+    internal const string AzureWebsiteSlotName = "WEBSITE_SLOT_NAME";
+    internal const string AzureWebsiteCloudRoleName = "WEBSITE_CLOUD_ROLENAME";
+    internal const string DefaultProductionSlotName = "production";
+
+    internal static string[] EnvironmentVariablesToMonitor = new[] { AzureWebsiteName, AzureWebsiteSlotName, AzureWebsiteCloudRoleName };
+
+    public void Configure(AppServiceOptions options)
+    {
+        options.AzureWebsiteName = Environment.GetEnvironmentVariable(AzureWebsiteName);
+        options.AzureWebsiteCloudRoleName = Environment.GetEnvironmentVariable(AzureWebsiteCloudRoleName);
+
+        // Compute the slot name by appending non-production slot to the site name (i.e. mysite-staging)
+        string slotName = Environment.GetEnvironmentVariable(AzureWebsiteSlotName);
+        options.AzureWebsiteSlotName = GetAzureWebsiteUniqueSlotName(options.AzureWebsiteName, slotName);
+    }
+
+    /// <summary>
+    /// Gets a value that uniquely identifies the site and slot.
+    /// </summary>
+    private static string? GetAzureWebsiteUniqueSlotName(string? websiteName, string? slotName)
+    {
+        if (!string.IsNullOrEmpty(slotName) &&
+            !string.Equals(slotName, DefaultProductionSlotName, StringComparison.OrdinalIgnoreCase))
+        {
+            websiteName += $"-{slotName}";
+        }
+
+        return websiteName?.ToLowerInvariant();
+    }
+}

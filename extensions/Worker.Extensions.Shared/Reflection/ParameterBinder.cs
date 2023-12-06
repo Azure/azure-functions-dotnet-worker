3013ff0069bd9a3e69983ab409677a21f3ad1eab
Lilian Kasem <likasem@microsoft.com>
2023-12-06 09:38:42 -0800

    Refactor CosmosDBConverter to remove use reflection (#1924)
    Co-authored-by: Jacob Viau <javia@microsoft.com>

---
+++ extensions/Worker.Extensions.Shared/Reflection/ParameterBinder.cs
@@ -0,0 +1,114 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions
+{
+    /// <summary>
+    /// Helpers for performing parameter binding.
+    /// </summary>
+    internal static class ParameterBinder
+    {
+        private const BindingFlags DeclaredOnlyLookup = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly;
+
+        /// <summary>
+        /// Binds to a collection type.
+        /// </summary>
+        /// <param name="factory">Factory for producing an enumerable given the element type.</param>
+        /// <param name="collectionType">The collection type to bind to.</param>
+        /// <returns>The instantiated and populated collection.</returns>
+        public static async Task<object> BindCollectionAsync(Func<Type, IAsyncEnumerable<object>> factory, Type collectionType)
+        {
+            if (factory is null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            if (collectionType is null)
+            {
+                throw new ArgumentNullException(nameof(collectionType));
+            }
+
+            if (!collectionType.TryGetCollectionElementType(out Type? elementType))
+            {
+                throw new ArgumentException($"Type '{collectionType}' is not a collection type.", nameof(collectionType));
+            }
+
+            object? collection = null;
+            if (collectionType.IsConcreteType() && !collectionType.IsArray)
+            {
+                collection = Activator.CreateInstance(collectionType)!;
+            }
+            else if (IsListInterface(collectionType))
+            {
+                collection = Activator.CreateInstance(typeof(List<>).MakeGenericType(elementType!))!;
+            }
+            else if (collectionType.IsArray)
+            {
+                collection = Activator.CreateInstance(typeof(List<>).MakeGenericType(elementType!))!;
+                await BindCollectionAsync(factory(elementType!), collection);
+                IList list = (IList)collection;
+                Array arrayResult = Array.CreateInstance(elementType!, list.Count);
+                list.CopyTo(arrayResult, 0);
+                return arrayResult;
+            }
+            else
+            {
+                throw new ArgumentException($"Collection type '{collectionType}' is not supported for parameter binding.", nameof(collectionType));
+            }
+
+            await BindCollectionAsync(factory(elementType!), collection);
+            return collection;
+        }
+
+        /// <summary>
+        /// Binds to a collection.
+        /// </summary>
+        /// <param name="pageable">The pageable containing the items to populate the collection with.</param>
+        /// <param name="collection">The collection to populate.</param>
+        /// <returns>A task that completes when the collection has been populated.</returns>
+        public static async Task BindCollectionAsync(IAsyncEnumerable<object> pageable, object collection)
+        {
+            if (pageable is null)
+            {
+                throw new ArgumentNullException(nameof(pageable));
+            }
+
+            if (collection is null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            Action<object> add = GetAddMethod(collection);
+            await foreach (object item in pageable)
+            {
+                add(item);
+            }
+        }
+
+        private static bool IsListInterface(Type type)
+        {
+            return type.IsGenericType &&
+                (type.GetGenericTypeDefinition() == typeof(IList<>)
+                || type.GetGenericTypeDefinition() == typeof(ICollection<>)
+                || type.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+        }
+
+        private static Action<object> GetAddMethod(object collection)
+        {
+            if (collection is IList list)
+            {
+                return e => list.Add(e);
+            }
+
+            MethodInfo method = collection.GetType().GetMethod("Add", DeclaredOnlyLookup)
+                ?? throw new InvalidOperationException($"Could not find an 'Add' method on collection type '{collection.GetType()}'.");
+            return e => method.Invoke(collection, new[] { e });
+        }
+    }
+}
---
+++ test/Worker.Extensions.Shared.Tests/Reflection/ParameterBinderTests.cs
@@ -0,0 +1,140 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections;
+using System.Collections.ObjectModel;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Shared.Tests
+{
+    public class ParameterBinderTests
+    {
+        [Fact]
+        public Task BindCollection_ThrowsOnNullPageableFactory()
+        {
+            return Assert.ThrowsAsync<ArgumentNullException>(
+                () => ParameterBinder.BindCollectionAsync(null!, typeof(IEnumerable<string>)));
+        }
+
+        [Fact]
+        public Task BindCollection_ThrowsOnNullCollectionType()
+        {
+            return Assert.ThrowsAsync<ArgumentNullException>(
+                () => ParameterBinder.BindCollectionAsync(e => Mock.Of<IAsyncEnumerable<object>>(), null!));
+        }
+
+        [Theory]
+        [InlineData(typeof(object))] // basic non-collection
+        [InlineData(typeof(MyPoco))] // basic non-collection
+        [InlineData(typeof(string))] // special case
+        [InlineData(typeof(byte[]))] // special case
+        [InlineData(typeof(IEnumerable))] // do not support non-generic IEnumerable
+        [InlineData(typeof(ICollection))] // do not support non-generic ICollection
+        [InlineData(typeof(IList))] // do not support non-generic IList
+        [InlineData(typeof(AbstractCollection))] // do not support abstract IEnumerable
+        [InlineData(typeof(NonGenericCollection))] // do not support non-generic IEnumerable
+        public Task BindCollection_ThrowsOnUnsupportedType(Type type)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(
+                () => ParameterBinder.BindCollectionAsync(e => Mock.Of<IAsyncEnumerable<object>>(), type));
+        }
+
+        [Theory]
+        [InlineData(typeof(IEnumerable<int>))]
+        [InlineData(typeof(ICollection<int>))]
+        [InlineData(typeof(IList<int>))]
+        public async Task BindCollection_Interface_GetsList(Type type)
+        {
+            object collection = await ParameterBinder.BindCollectionAsync(GetIntEnumerable, type);
+            List<int> list = Assert.IsType<List<int>>(collection);
+            Assert.Collection(
+                list,
+                i => Assert.Equal(0, i),
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i),
+                i => Assert.Equal(4, i),
+                i => Assert.Equal(5, i));
+        }
+
+        [Theory]
+        [InlineData(typeof(MyEnumerable))]
+        [InlineData(typeof(Collection<int>))]
+        [InlineData(typeof(List<int>))]
+        [InlineData(typeof(int[]))]
+        public async Task BindCollection_Concrete_GetsType(Type type)
+        {
+            object collection = await ParameterBinder.BindCollectionAsync(GetIntEnumerable, type);
+            Assert.IsType(type, collection);
+            Assert.Collection(
+                (IEnumerable<int>)collection,
+                i => Assert.Equal(0, i),
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i),
+                i => Assert.Equal(4, i),
+                i => Assert.Equal(5, i));
+        }
+
+        [Fact]
+        public async Task BindCollection_PocoArray_GetsArray()
+        {
+            object collection = await ParameterBinder.BindCollectionAsync(GetPocoEnumerable, typeof(MyPoco[]));
+            Assert.IsType<MyPoco[]>(collection);
+            Assert.Collection(
+                (MyPoco[])collection,
+                i => Assert.Equal("0", i.Prop),
+                i => Assert.Equal("1", i.Prop),
+                i => Assert.Equal("2", i.Prop),
+                i => Assert.Equal("3", i.Prop),
+                i => Assert.Equal("4", i.Prop),
+                i => Assert.Equal("5", i.Prop));
+        }
+
+        private static async IAsyncEnumerable<object> GetIntEnumerable(Type t)
+        {
+            Assert.Equal(typeof(int), t);
+            await Task.Yield();
+            foreach (int i in Enumerable.Range(0, 6))
+            {
+                yield return i;
+            }
+        }
+
+        private static async IAsyncEnumerable<object> GetPocoEnumerable(Type t)
+        {
+            Assert.Equal(typeof(MyPoco), t);
+            await Task.Yield();
+            foreach (int i in Enumerable.Range(0, 6))
+            {
+                yield return new MyPoco(i.ToString());
+            }
+        }
+
+        private record MyPoco(string Prop);
+
+        private abstract class AbstractCollection : IEnumerable<MyPoco>
+        {
+            public IEnumerator<MyPoco> GetEnumerator() => throw new NotImplementedException();
+
+            IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+        }
+
+        private class NonGenericCollection : IEnumerable
+        {
+            public IEnumerator GetEnumerator() => throw new NotImplementedException();
+        }
+
+        private class MyEnumerable : IEnumerable<int>
+        {
+            private readonly List<int> values = new List<int>();
+
+            public void Add(int item) => values.Add(item);
+
+            public IEnumerator<int> GetEnumerator() => ((IEnumerable<int>)values).GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)values).GetEnumerator();
+        }
+    }
+}

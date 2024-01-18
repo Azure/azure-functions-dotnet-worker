77b490cd06a6656ae598ef3c7ad2f92b56080f56
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2024-01-18 12:59:36 -0800

    Revert "Adding TelemetryConfiguration."
    This reverts commit 18288c8798ef43868acabb6cba0ff99263cdceb0.

---
+++ src/DotNetWorker.ApplicationInsights/Initializers/FunctionsApplicationInsightsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Azure.Functions.Worker.ApplicationInsights.Initializers
+{
+    /// <summary>
+    /// Options for configuring Functions Application Insights.
+    /// </summary>
+    internal class FunctionsApplicationInsightsOptions
+    {
+        internal TokenCredentialOptions? TokenCredentialOptions { get; set; }
+    }
+}

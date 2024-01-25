3baa891643cba6d55416aa451d574ac150919f11
Shyju Krishnankutty <connectshyju@gmail.com>
2024-01-25 09:33:52 -0800

    Removed Guid for event source

--- src/DotNetWorker.Core/Diagnostics/WorkerEventSource.cs
+++ src/DotNetWorker.Core/Diagnostics/WorkerEventSource.cs
@@ -5,7 +5,7 @@ using System.Diagnostics.Tracing;
 
 namespace Microsoft.Azure.Functions.Worker.Core.Diagnostics
 {
-    [EventSource(Name = "Microsoft-AzureFunctions-Worker", Guid = "BD0E0962-2562-4751-8191-DAFE3750FD38")]
+    [EventSource(Name = "Microsoft-AzureFunctions-Worker")]
     internal sealed class WorkerEventSource : EventSource
     {
         [Event(1001)]

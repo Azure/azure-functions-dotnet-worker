6107a1ba21f80a728e4b6d1055f8b144b7317f1e
Shyju Krishnankutty <connectshyju@gmail.com>
2024-02-09 18:15:29 -0800

    Cleanup

--- src/DotNetWorker.Core/PreJit/Constants.cs
+++ src/DotNetWorker.Core/PreJit/Constants.cs
@@ -3,20 +3,20 @@
 
 namespace Microsoft.Azure.Functions.Worker
 {
-    public static class Constants
-    {
-        public const string PreJitFolderName = "PreJIT";
-        public const string JitTraceFileName = "coldstart.jittrace";
-        public const string OverridableAssemblyListFileName = "overridable-assemblies.txt";
-    }
+    //public static class Constants
+    //{
+    //    public const string PreJitFolderName = "PreJIT";
+    //    public const string JitTraceFileName = "coldstart.jittrace";
+    //    public const string OverridableAssemblyListFileName = "overridable-assemblies.txt";
+    //}
 
-    public static class AppDomainProperties
-    {
-        public const string TrustedPlatformAssemblies = "TRUSTED_PLATFORM_ASSEMBLIES";
-    }
+    //public static class AppDomainProperties
+    //{
+    //    public const string TrustedPlatformAssemblies = "TRUSTED_PLATFORM_ASSEMBLIES";
+    //}
 
-    public static class EnvironmentVariables
-    {
-        public const string WorkerRuntimeVersion = "FUNCTIONS_WORKER_RUNTIME_VERSION";
-    }
+    //public static class EnvironmentVariables
+    //{
+    //    public const string WorkerRuntimeVersion = "FUNCTIONS_WORKER_RUNTIME_VERSION";
+    //}
 }

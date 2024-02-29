0052526447fd7f0cf1809cf8c8b82bb1f709c0c8
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2024-02-29 09:46:30 -0800

    [ApplicationInsights] Enable AAD Auth (#2217)
    * Adding support for AAD Auth in the worker AI package.

---
+++ src/DotNetWorker.ApplicationInsights/Initializers/TelemetryConfigurationSetup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Azure.Functions.Worker.ApplicationInsights.Initializers
+{
+    internal class TelemetryConfigurationSetup : IConfigureOptions<TelemetryConfiguration>
+    {
+        private const string AppInsightsAuthenticationString = "APPLICATIONINSIGHTS_AUTHENTICATION_STRING";
+        private readonly IConfiguration _configuration;
+        
+        public TelemetryConfigurationSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(TelemetryConfiguration telemetryConfiguration)
+        {
+            string? authString = GetAuthenticationString();
+            if (authString is not null)
+            {
+                telemetryConfiguration.SetAzureTokenCredential(TokenCredentialOptions.ParseAuthenticationString(authString).CreateTokenCredential());
+            }
+        }
+
+        private string? GetAuthenticationString()
+        {
+            if (_configuration is not null && _configuration[AppInsightsAuthenticationString] is string value && value is not "")
+            {
+                return value;
+            }
+            else if (Environment.GetEnvironmentVariable(AppInsightsAuthenticationString) is string envValue && envValue is not "")
+            {
+                return envValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

b600150e3aa791b1494c614e7018bb1dcb06a6fd
Shyju Krishnankutty <connectshyju@gmail.com>
2024-03-05 11:42:31 -0800

    Publish net6 version. Update prelaunch app to use some APIs from framework assemblies

--- host/src/PrelaunchApp/Program.cs
+++ host/src/PrelaunchApp/Program.cs
@@ -1,13 +1,56 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace App
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine($".NET version:{RuntimeInformation.FrameworkDescription}");
+            // Using some types/methods from the framework assemblies.
+
+            try
+            {
+                var dictionary = new ConcurrentDictionary<string, object>();
+                dictionary.TryAdd("a", new Activity("activity1"));
+                dictionary.TryAdd("b", Enumerable.Range(1, 5));
+                dictionary.TryAdd("c", RuntimeInformation.FrameworkDescription);
+                dictionary.TryAdd("d", Environment.ProcessId);
+
+                var obj = new
+                {
+                    ItemCount = dictionary.Count,
+                    ItemKeys = dictionary.Select(i => i.Key).ToImmutableArray(),
+                    FirstItemKey = dictionary.Keys.FirstOrDefault(),
+                    Items = dictionary.OrderBy(i => i.Key).ToImmutableDictionary()
+                };
+
+                using (var stream = new MemoryStream())
+                {
+                    await JsonSerializer.SerializeAsync(stream, obj, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    stream.Position = 0;
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string item = await reader.ReadToEndAsync();
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }

5db39aae7673ee4e4a384edecdd212887f981428
satvu <sarahvu@microsoft.com>
2024-03-11 14:41:24 -0700

    fix signalr array input generation

--- sdk/Sdk.Generators/FunctionMetadataProviderGenerator/FunctionMetadataProviderGenerator.Parser.cs
+++ sdk/Sdk.Generators/FunctionMetadataProviderGenerator/FunctionMetadataProviderGenerator.Parser.cs
@@ -623,11 +623,11 @@ namespace Microsoft.Azure.Functions.Worker.Sdk.Generators
 
                 foreach (var namedArgument in attributeData.NamedArguments)
                 {
-                    if (namedArgument.Value.Value != null)
+                    if (namedArgument.Value.Kind is TypedConstantKind.Array || namedArgument.Value.Value != null)
                     {
                         if (string.Equals(namedArgument.Key, Constants.FunctionMetadataBindingProps.IsBatchedKey) && !attrProperties.ContainsKey("cardinality"))
                         {
-                            var argValue = (bool)namedArgument.Value.Value; // isBatched only takes in booleans and the generator will parse it as a bool so we can type cast this to use in the next line
+                            var argValue = (bool)namedArgument.Value.Value!; // isBatched only takes in booleans and the generator will parse it as a bool so we can type cast this to use in the next line
 
                             attrProperties["cardinality"] = argValue ? "Many" : "One";
                         }

441c7820f773f1d6a8f42ae9ae1dc7a49218323b
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2024-04-03 01:50:08 -0700

    Initial commit

---
+++ src/DotNetWorker.OpenTelemetry/ConfigureFunctionsOpenTelemetry.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+
+namespace Microsoft.Azure.Functions.Worker.OpenTelemetry
+{
+    public static class ConfigureFunctionsOpenTelemetry
+    {
+        private const string DefaultServiceName = "azureFunctions";
+        public static OpenTelemetryBuilder UseFunctionsWorkerDefaults(this OpenTelemetryBuilder builder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services
+                // Lets the host know that the worker is sending logs to App Insights. The host will now ignore these.
+                .Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerOpenTelemetryEnabled"] = bool.TrueString);
+
+            builder.ConfigureResource(r =>
+            {
+                string version = typeof(ConfigureFunctionsOpenTelemetry).Assembly.GetName().Version.ToString();
+                ConfigureResourceAttributes(r, version);
+            });
+
+            return builder;
+        }
+
+        private static string GetServiceName()
+        {
+            return Environment.GetEnvironmentVariable(ResourceAttributeConstants.SiteNameEnvVar) ?? DefaultServiceName;
+        }
+
+        private static void ConfigureResourceAttributes(ResourceBuilder r, string version)
+        {
+            r.AddService(GetServiceName(), serviceVersion: version)
+             .AddAttributes(new[]
+             {
+                new KeyValuePair<string, object>(ResourceAttributeConstants.AttributeSDKPrefix, $"{ResourceAttributeConstants.SDKPrefix}: {version}"),
+                new KeyValuePair<string, object>(ResourceAttributeConstants.AttributeProcessId, Process.GetCurrentProcess().Id.ToString())
+             });
+        }
+    }
+}
---
+++ src/DotNetWorker.OpenTelemetry/README.md
@@ -0,0 +1,5 @@
+# Microsoft.Azure.Functions.Worker.ApplicationInsights
+
+This package adds extension methods and services to configure OpenTelemetry for use in Azure Functions .NET isolated applications.
+
+This package does **not** add OpenTelemetry services directly. This must be done directly. Instead, this package only configures isolated application and resource detector.

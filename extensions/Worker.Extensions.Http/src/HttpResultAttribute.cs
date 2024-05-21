37fdbe7b0f0d46e452d8b64737c0992dae19b181
satvu <sarahvu@microsoft.com>
2024-05-21 14:34:51 -0700

    cleanup

---
+++ extensions/Worker.Extensions.Http/src/HttpResultAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    /// <summary>
+    /// Attribute used to mark an HTTP Response on an HTTP Trigger function with multiple output bindings.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class HttpResultAttribute : OutputBindingAttribute
+    {
+    }
+}

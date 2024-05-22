25260bbacd933269fc62afb2201a9aaaafafc408
Lilian Kasem <likasem@microsoft.com>
2024-05-22 10:35:36 -0700

    Introduce 'name' parameter to ConversionResult

--- src/DotNetWorker.Core/Converters/Converter/ConversionResult.cs
+++ src/DotNetWorker.Core/Converters/Converter/ConversionResult.cs
@@ -10,26 +10,25 @@ namespace Microsoft.Azure.Functions.Worker.Converters
     /// </summary>
     public readonly struct ConversionResult
     {
-        // A static ConversionResult instance which represents an unhandled conversion.
-        private static readonly ConversionResult _unhandledConversionResult = new(status: ConversionStatus.Unhandled);
-
         /// <summary>
         /// Creates a new <see cref="ConversionResult"/> instance.
         /// </summary>
         /// <param name="status">The status of conversion operation.</param>
         /// <param name="value">The value produced from the successful conversion.</param>
         /// <param name="error">The exception which caused the conversion to fail.</param>
-        private ConversionResult(ConversionStatus status, object? value = null, Exception? error = null)
+        /// <param name="name">The name of the converter.</param>
+        private ConversionResult(ConversionStatus status, object? value = null, Exception? error = null, string? name = null)
         {
             Status = status;
             Value = value;
             Error = error;
+            Name = name ?? string.Empty;
         }
 
         /// <summary>
         /// Gets the status of the conversion.
         /// </summary>
-        public ConversionStatus Status { get; } 
+        public ConversionStatus Status { get; }
 
         /// <summary>
         /// Gets the value produced from the conversion if it was successful.
@@ -42,33 +41,43 @@ namespace Microsoft.Azure.Functions.Worker.Converters
         public Exception? Error { get; }
 
         /// <summary>
+        /// Gets the name of the converter.
+        /// </summary>
+        public string? Name { get; }
+
+        /// <summary>
         /// Creates a new <see cref="ConversionResult"/> instance to represent an unhandled input conversion.
         /// </summary>
+        /// <param name="name">The name of the converter.</param>
         /// <returns>A new instance of <see cref="ConversionResult"/> where the Status property value is set to Unhandled.</returns>
-        public static ConversionResult Unhandled() => _unhandledConversionResult;
+        public static ConversionResult Unhandled(string? name = null) => new(status: ConversionStatus.Unhandled, name: name);
 
         /// <summary>
         /// Creates a new <see cref="ConversionResult"/> instance to represent a successful input conversion.
         /// </summary>
+        /// <param name="value">The value produced from the conversion.</param>
+        /// <param name="name">The name of the converter.</param>
         /// <returns>A new instance of <see cref="ConversionResult"/> to represent a successful conversion.</returns>
-        public static ConversionResult Success(object? value) => new(status: ConversionStatus.Succeeded, value: value);
+        public static ConversionResult Success(object? value, string? name = null) => new(status: ConversionStatus.Succeeded, value: value, name: name);
 
         /// <summary>
         /// Creates a new <see cref="ConversionResult"/> instance to represent a failed input conversion.
         /// </summary>
         /// <param name="exception">The exception representing the cause of the failed conversion.</param>
+        /// <param name="name">The name of the converter.</param>
         /// <exception cref="ArgumentNullException">Throws when the <paramref name="exception"/> argument is null.</exception>
         /// <returns>A new instance of <see cref="ConversionResult"/> to represent a failed input conversion.</returns>
-        public static ConversionResult Failed(Exception exception)
+        public static ConversionResult Failed(Exception exception, string? name = null)
         {
             if (exception is null)
             {
                 throw new ArgumentNullException(nameof(exception));
             }
-            
+
             return new ConversionResult(status: ConversionStatus.Failed,
                                         value: null,
-                                        error: exception);
+                                        error: exception,
+                                        name: name);
         }
     }
 }

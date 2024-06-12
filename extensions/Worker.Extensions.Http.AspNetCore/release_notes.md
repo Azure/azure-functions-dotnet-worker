efd0aefe0a6564759f4aac57be8d6a7b8293ca86
sarah <35204912+satvu@users.noreply.github.com>
2024-06-12 16:25:27 -0700

    [Bug Fix] ASP.NET pipeline hangs when function execution throws an unhandled exception (#2527)

--- extensions/Worker.Extensions.Http.AspNetCore/release_notes.md
+++ extensions/Worker.Extensions.Http.AspNetCore/release_notes.md
@@ -4,6 +4,6 @@
 - My change description (#PR/#issue)
 -->
 
-### Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore <version>
+### Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore 1.3.2
 
-- <entry>
+- Fixes a bug where the invocation hangs when the function has an unhandled exception (#2527).
--- extensions/Worker.Extensions.Http.AspNetCore/src/FunctionsMiddleware/FunctionsHttpProxyingMiddleware.cs
+++ extensions/Worker.Extensions.Http.AspNetCore/src/FunctionsMiddleware/FunctionsHttpProxyingMiddleware.cs
@@ -49,19 +49,24 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore
             // Register additional context features
             context.Features.Set<IFromBodyConversionFeature>(FromBodyConversionFeature.Instance);
 
-            await next(context);
-
-            var responseHandled = await TryHandleHttpResult(context.GetInvocationResult().Value, context, httpContext, true)
-                || await TryHandleOutputBindingsHttpResult(context, httpContext);
-            
-            if (!responseHandled)
+            try
             {
-                var logger = context.InstanceServices.GetRequiredService<ExtensionTrace>();
-                logger.NoHttpResponseReturned(context.FunctionDefinition.Name, context.InvocationId);
-            }
+                await next(context);
 
-            // Allow ASP.NET Core middleware to continue
-            _coordinator.CompleteFunctionInvocation(invocationId);
+                var responseHandled = await TryHandleHttpResult(context.GetInvocationResult().Value, context, httpContext, true)
+                    || await TryHandleOutputBindingsHttpResult(context, httpContext);
+
+                if (!responseHandled)
+                {
+                    var logger = context.InstanceServices.GetRequiredService<ExtensionTrace>();
+                    logger.NoHttpResponseReturned(context.FunctionDefinition.Name, context.InvocationId);
+                }
+            }
+            finally
+            {
+                // Allow ASP.NET Core middleware to continue
+                _coordinator.CompleteFunctionInvocation(invocationId);
+            }
         }
 
         private static async Task<bool> TryHandleHttpResult(object? result, FunctionContext context, HttpContext httpContext, bool isInvocationResult = false)

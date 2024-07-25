0cc7648d6c5cfa73e198b4531bd2678b1ff80f2d
Surgupta <surgupta@microsoft.com>
2024-07-25 10:46:01 -0700

    Updating tests

--- test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
+++ test/FunctionMetadataGeneratorTests/FunctionMetadataGeneratorTests.cs
@@ -985,7 +985,7 @@ namespace Microsoft.Azure.Functions.SdkTests
                 b => ValidateTrigger(b, cardinalityMany));
 
             AssertDictionary(extensions, new Dictionary<string, string>(){
-                { "Microsoft.Azure.WebJobs.Extensions.EventHubs", "6.3.2" }
+                { "Microsoft.Azure.WebJobs.Extensions.EventHubs", "6.3.3" }
             });
 
             void ValidateTrigger(ExpandoObject b, bool many)
@@ -1212,7 +1212,7 @@ namespace Microsoft.Azure.Functions.SdkTests
 
             AssertDictionary(extensions, new Dictionary<string, string>
             {
-                { "Microsoft.Azure.WebJobs.Extensions.EventHubs", "6.3.2" },
+                { "Microsoft.Azure.WebJobs.Extensions.EventHubs", "6.3.3" },
             });
 
             var eventHubTriggerFunction = functions.Single(p => p.Name == nameof(SDKTypeBindings_EventHubs.EventHubTriggerFunction));

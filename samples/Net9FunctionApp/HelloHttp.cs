113a26605d5785c1dc066b3f07a6a568b5766d7e
Shyju Krishnankutty <connectshyju@gmail.com>
2024-08-16 12:41:12 -0700

    Adding net9.0 TFM (#2615)
    * Adding net9.0 TFM
    
    Update build YAML to install net9
    
    pin to preview6 in global.json
    
    * Bump SDK package version
    
    * Adding Net9 sample app.
    
    * Updating the below packages to use 9x version for net9.0 tfm
     - Microsoft.Extensions.Hosting
     - Microsoft.Extensions.Hosting.Abstractions
     - System.Collections.Immutable
     - System.Diagnostics.DiagnosticSource
    
    * Move condition to ItemGroup element instead of Choose-When pattern.
    
    * Updated ApplicationInsights package version in Sample app to latest stable (1.3.0)

---
+++ samples/Net9FunctionApp/HelloHttp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Net9FunctionApp
+{
+    public class HelloHttp(ILogger<HelloHttp> logger)
+    {
+        [Function("HelloHttp")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}

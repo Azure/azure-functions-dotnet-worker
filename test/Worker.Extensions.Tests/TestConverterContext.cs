4214ff3ec2d8de3bea3ba1cc04393838fe866789
Fabio Cavalcante <facaval@microsoft.com>
2024-08-26 15:58:52 -0700

    Updating project structure to fix extensions, samples and core package dependencies

---
+++ test/Worker.Extensions.Tests/TestConverterContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Converters;
+
+namespace Microsoft.Azure.Functions.Worker.Tests.Converters
+{
+    internal class TestConverterContext : ConverterContext
+    {
+        public TestConverterContext(Type targetType, object source, FunctionContext context = null)
+        {
+            TargetType = targetType;
+            Source = source;
+            FunctionContext = context; //TODO: SharedTestFunctionContext ?? new TestFunctionContext();
+        }
+
+        public override object Source { get; }
+
+        public override FunctionContext FunctionContext { get; }
+
+        public override Type TargetType { get; }
+
+        public override IReadOnlyDictionary<string, object> Properties { get; }
+    }
+}

c37e1b65e141a3b9128ce3b4881139a811655219
byar <byarotsky@gmail.com>
2024-10-05 20:53:23 +0200

    added data flow check

--- sdk/Sdk.Analyzers/LocalSettingsJsonNotAllowedAsConfiguration.cs
+++ sdk/Sdk.Analyzers/LocalSettingsJsonNotAllowedAsConfiguration.cs
@@ -32,18 +32,18 @@ public class LocalSettingsJsonNotAllowedAsConfiguration : DiagnosticAnalyzer
             return;
         }
         
-        // Get the symbol for the method being invoked
+        // safeguard to be sure that the target of invocation is IConfigurationBuilder
         if (context.SemanticModel.GetSymbolInfo(invocation.Expression).Symbol is not IMethodSymbol methodSymbol)
         {
             return;
         }
-
         var configBuilderType = context.SemanticModel.Compilation.GetTypeByMetadataName("Microsoft.Extensions.Configuration.IConfigurationBuilder");
         if (!SymbolEqualityComparer.Default.Equals(methodSymbol.ReceiverType, configBuilderType))
         {
             return;
         }
 
+        // easiest case - local.settings.json is a string literal
         var firstArgument = arguments.First().Expression;
         if (firstArgument is LiteralExpressionSyntax { Token.ValueText: "local.settings.json" } literal)
         {
@@ -55,6 +55,7 @@ public class LocalSettingsJsonNotAllowedAsConfiguration : DiagnosticAnalyzer
             return;
         }
         
+        // it can also be a constant value
         var constantValue = context.SemanticModel.GetConstantValue(firstArgument);
         if (constantValue is { HasValue: true, Value: "local.settings.json" })
         {
@@ -63,6 +64,53 @@ public class LocalSettingsJsonNotAllowedAsConfiguration : DiagnosticAnalyzer
                 firstArgument.GetLocation());
 
             context.ReportDiagnostic(diagnostic);
+            return;
         }
+        
+        // we can try to resolve variable passed as well
+        // Use DataFlowAnalysis to resolve the value of the variable if possible
+        if (firstArgument is IdentifierNameSyntax identifier)
+        {
+            var dataFlow = context.SemanticModel.AnalyzeDataFlow(invocation);
+            if (dataFlow is { Succeeded: true })
+            {
+                // Check for variable assignments within the data flow
+                var symbol = context.SemanticModel.GetSymbolInfo(identifier).Symbol;
+                if (symbol is ILocalSymbol localSymbol)
+                {
+                    var variableDeclaration = dataFlow.DataFlowsIn.FirstOrDefault(s => SymbolEqualityComparer.Default.Equals(s, localSymbol));
+                    if (variableDeclaration != null && TryGetAssignedValue(localSymbol, context, out var assignedValue))
+                    {
+                        if (assignedValue == "local.settings.json")
+                        {
+                            var diagnostic = Diagnostic.Create(
+                                DiagnosticDescriptors.LocalSettingsJsonNotAllowedAsConfiguration,
+                                firstArgument.GetLocation());
+
+                            context.ReportDiagnostic(diagnostic);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private static bool TryGetAssignedValue(ILocalSymbol localSymbol, SyntaxNodeAnalysisContext context, out string assignedValue)
+    {
+        assignedValue = null;
+
+        // Find the variable declaration
+        foreach (var reference in localSymbol.DeclaringSyntaxReferences)
+        {
+            var syntax = reference.GetSyntax(context.CancellationToken);
+            if (syntax is VariableDeclaratorSyntax { Initializer.Value: LiteralExpressionSyntax literal })
+            {
+                // Check if the variable is initialized with "local.settings.json"
+                assignedValue = literal.Token.ValueText;
+                return true;
+            }
+        }
+
+        return false;
     }
 }

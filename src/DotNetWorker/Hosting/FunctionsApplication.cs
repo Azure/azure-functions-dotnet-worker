23d1dbadd4167437e62b66345d86e8230183d6af
Brett Samblanet <brettsam@microsoft.com>
2024-10-10 11:05:23 -0700

    Adding FunctionsApplicationBuilder

---
+++ src/DotNetWorker/Hosting/FunctionsApplication.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Extensions.Hosting
+{
+    /// <summary>
+    ///  The application used to configure an Azure Functions worker.
+    /// </summary>
+    public class FunctionsApplication
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionsApplicationBuilder"/> class with preconfigured defaults.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>The <see cref="FunctionsApplicationBuilder"/>.</returns>
+        public static FunctionsApplicationBuilder CreateBuilder(string[] args) => new(args);
+    }
+}

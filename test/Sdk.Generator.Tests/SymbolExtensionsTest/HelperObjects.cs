800bf48bddd1a75fa4396c07f0ee09ba6c7b3797
satvu <sarahvu@microsoft.com>
2024-10-16 11:45:49 -0700

    symbol extensions test

---
+++ test/Sdk.Generator.Tests/SymbolExtensionsTest/HelperObjects.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.SdkGeneratorTests
+{
+    internal class FooOutAttribute : OutputBindingAttribute
+    {
+    }
+
+    internal class FooOutAttribute2 : FooOutAttribute
+    {
+    }
+
+    internal class BarInAttribute : InputBindingAttribute
+    {
+    }
+
+    internal class BarInAttribute2 : BarInAttribute
+    {
+    }
+}

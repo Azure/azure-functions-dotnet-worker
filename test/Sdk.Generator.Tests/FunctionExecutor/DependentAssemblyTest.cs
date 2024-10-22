b92e042cf25ede2134688d349c0a3c7ef98362a5
Fabio Cavalcante <facaval@microsoft.com>
2024-10-21 18:10:56 -0700

    Tactical fix to address a test issue

--- test/Sdk.Generator.Tests/FunctionExecutor/DependentAssemblyTest.cs
+++ test/Sdk.Generator.Tests/FunctionExecutor/DependentAssemblyTest.cs
@@ -138,7 +138,7 @@ namespace Microsoft.Azure.Functions.SdkGeneratorTests
                                        
                                                private global::Microsoft.Azure.Functions.Worker.Invocation.IFunctionExecutor CreateDefaultExecutorInstance(global::Microsoft.Azure.Functions.Worker.FunctionContext context)
                                                {
-                                                   var defaultExecutorFullName = "Microsoft.Azure.Functions.Worker.Invocation.DefaultFunctionExecutor, Microsoft.Azure.Functions.Worker.Core, Version=1.19.0.0, Culture=neutral, PublicKeyToken=551316b6919f366c";
+                                                   var defaultExecutorFullName = "Microsoft.Azure.Functions.Worker.Invocation.DefaultFunctionExecutor, Microsoft.Azure.Functions.Worker.Core, Version=2.0.0.0, Culture=neutral, PublicKeyToken=551316b6919f366c";
                                                    var defaultExecutorType = global::System.Type.GetType(defaultExecutorFullName);
 
                                                    return ActivatorUtilities.CreateInstance(context.InstanceServices, defaultExecutorType) as global::Microsoft.Azure.Functions.Worker.Invocation.IFunctionExecutor;

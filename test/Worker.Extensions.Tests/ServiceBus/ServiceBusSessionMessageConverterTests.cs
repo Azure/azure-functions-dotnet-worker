3296c3fe5b6703659ad27dda73bc8f6456eaac7c
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-12-09 14:54:29 -0800

    remove failing test

--- test/Worker.Extensions.Tests/ServiceBus/ServiceBusSessionMessageConverterTests.cs
+++ test/Worker.Extensions.Tests/ServiceBus/ServiceBusSessionMessageConverterTests.cs
@@ -1,26 +1,14 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Microsoft.Azure.Functions.Worker.Converters;
+using static Microsoft.Azure.Functions.Worker.Extensions.Tests.ServiceBusMessageActionsTests;
+using Microsoft.Azure.Functions.Worker.Tests.Converters;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Azure.Messaging.ServiceBus;
-using Google.Protobuf;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Converters;
-using Microsoft.Azure.Functions.Worker.Extensions.ServiceBus;
-using Microsoft.Azure.Functions.Worker.Grpc.Messages;
-using Microsoft.Azure.Functions.Worker.Tests;
-using Microsoft.Azure.Functions.Worker.Tests.Converters;
-using Moq;
 using Xunit;
-using Google.Protobuf.Collections;
-using Microsoft.Azure.ServiceBus.Grpc;
-using static Microsoft.Azure.Functions.Worker.Extensions.Tests.ServiceBusMessageActionsTests;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 
 namespace Microsoft.Azure.Functions.Worker.Extensions.Tests
 {
@@ -98,8 +86,7 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Tests
             {
                 SessionLockedUntil = "2024-12-05T21:10:36.1193094+00:00"
             };
-            RepeatedField<string> repeatedField = new RepeatedField<string>();
-            repeatedField.Add("test");
+            IList<string> repeatedField = new List<string> { "test" };
 
             var bindingDataDictionary = new Dictionary<string, object>
             {
@@ -130,36 +117,6 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Tests
             };
             var bindingDataDictionary = new Dictionary<string, object>
             {
-                { "SessionActions", JsonSerializer.Serialize(new
-                    {
-                        SessionLockedUntil = "2024-12-05T21:10:36.1193094+00:00"
-                    })
-                }
-            };
-            var context = new TestConverterContext(typeof(ServiceBusSessionMessageActions), data, new TestFunctionContext(new TestBindingContext(bindingDataDictionary)));
-            var converter = new ServiceBusSessionMessageActionsConverter(new MockSettlementClient("test"));
-            var result = await converter.ConvertAsync(context);
-
-            Assert.Equal(ConversionStatus.Failed, result.Status);
-        }
-
-
-        [Fact]
-        public async Task ConvertAsync_Batch_ForReceivedMessage_ReturnsFailed_MultipleMessages()
-        {
-            var data = "{\"SessionLockedUntil\":\"2024-12-05T21:10:36.1193094+00:00\"}";
-
-            var jsonObject = new
-            {
-                SessionLockedUntil = "2024-12-05T21:10:36.1193094+00:00"
-            };
-            RepeatedField<string> repeatedField = new RepeatedField<string>();
-            repeatedField.Add("test");
-            repeatedField.Add("hi");
-
-            var bindingDataDictionary = new Dictionary<string, object>
-            {
-                { "SessionIdArray", repeatedField },
                 { "SessionActions", JsonSerializer.Serialize(new
                     {
                         SessionLockedUntil = "2024-12-05T21:10:36.1193094+00:00"

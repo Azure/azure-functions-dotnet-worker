3683b377038549e2c4112f9f44c8ad4ec2341d6c
Jacob Viau <javia@microsoft.com>
2025-03-19 14:12:53 -0700

    Remove unused using

--- sdk/Sdk/FunctionMetadataJsonWriter.cs
+++ sdk/Sdk/FunctionMetadataJsonWriter.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Functions.Worker.Sdk
 {

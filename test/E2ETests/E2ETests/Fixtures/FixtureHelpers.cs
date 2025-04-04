41dae065d3d066282dff7f4a354043998c1ccf32
satvu <sarahvu@microsoft.com>
2025-04-04 14:33:14 -0700

    add core tools args

--- test/E2ETests/E2ETests/Fixtures/FixtureHelpers.cs
+++ test/E2ETests/E2ETests/Fixtures/FixtureHelpers.cs
@@ -47,6 +47,8 @@ namespace Microsoft.Azure.Functions.Tests.E2ETests
             funcProcess.StartInfo.FileName = cliPath;
             funcProcess.StartInfo.ArgumentList.Add("start");
             funcProcess.StartInfo.ArgumentList.Add("--verbose");
+            funcProcess.StartInfo.ArgumentList.Add("--runtime");
+            funcProcess.StartInfo.ArgumentList.Add("default");
 
             if (enableAuth)
             {

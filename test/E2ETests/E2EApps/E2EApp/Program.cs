b9fe48caf7fa19e62e16591274874bab8f0e00c8
satvu <sarahvu@microsoft.com>
2025-04-10 14:59:07 -0700

    another one

--- test/E2ETests/E2EApps/E2EApp/Program.cs
+++ test/E2ETests/E2EApps/E2EApp/Program.cs
@@ -2,19 +2,16 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker.E2EApp;
 using Microsoft.Extensions.Hosting;
 
-namespace Microsoft.Azure.Functions.Worker.E2EApps.CosmosApp
+namespace Microsoft.Azure.Functions.Worker.E2EApp
 {
     class Program
     {
         static async Task Main(string[] args)
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults(workerApplication => {
-/*                    workerApplication.UseMiddleware<MyCustomMiddleware>();
-*/                })
+                .ConfigureFunctionsWorkerDefaults()
                 .Build();
 
             await host.RunAsync();

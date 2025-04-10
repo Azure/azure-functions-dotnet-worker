c011cd79b44e9932f9f4922fc4d948233fa1edf6
satvu <sarahvu@microsoft.com>
2025-04-10 11:21:07 -0700

    fix

--- test/E2ETests/E2EApps/E2EApp/Table/TableInputBindingFunctions.cs
+++ test/E2ETests/E2EApps/E2EApp/Table/TableInputBindingFunctions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;

2f60017e0660fe73f4a8dedbecc16e57fcdb9c65
Jacob Viau <javia@microsoft.com>
2025-09-08 10:02:31 -0700

    Allow usage of ObjectSerializer for Cosmos bindings

--- extensions/Worker.Extensions.CosmosDB/src/CosmosDBConverter.cs
+++ extensions/Worker.Extensions.CosmosDB/src/CosmosDBConverter.cs
@@ -6,7 +6,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker.Core;
@@ -16,6 +15,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.Extensions;
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+using Azure.Core.Serialization;
 
 namespace Microsoft.Azure.Functions.Worker
 {
@@ -27,9 +27,9 @@ namespace Microsoft.Azure.Functions.Worker
     {
         private readonly IOptionsMonitor<CosmosDBBindingOptions> _cosmosOptions;
         private readonly ILogger<CosmosDBConverter> _logger;
-        private static readonly JsonSerializerOptions _serializerOptions = new() { PropertyNameCaseInsensitive = true };
 
-        public CosmosDBConverter(IOptionsMonitor<CosmosDBBindingOptions> cosmosOptions, ILogger<CosmosDBConverter> logger)
+        public CosmosDBConverter(
+            IOptionsMonitor<CosmosDBBindingOptions> cosmosOptions, ILogger<CosmosDBConverter> logger)
         {
             _cosmosOptions = cosmosOptions ?? throw new ArgumentNullException(nameof(cosmosOptions));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
@@ -44,7 +44,8 @@ namespace Microsoft.Azure.Functions.Worker
             };
         }
 
-        private async ValueTask<ConversionResult> ConvertFromBindingDataAsync(ConverterContext context, ModelBindingData modelBindingData)
+        private async ValueTask<ConversionResult> ConvertFromBindingDataAsync(
+            ConverterContext context, ModelBindingData modelBindingData)
         {
             try
             {
@@ -55,7 +56,6 @@ namespace Microsoft.Azure.Functions.Worker
 
                 CosmosDBInputAttribute cosmosAttribute = GetBindingDataContent(modelBindingData);
                 object result = await ToTargetTypeAsync(context.TargetType, cosmosAttribute);
-
                 return ConversionResult.Success(result);
             }
             catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
@@ -83,52 +83,66 @@ namespace Microsoft.Azure.Functions.Worker
             };
         }
 
-        private async Task<object> ToTargetTypeAsync(Type targetType, CosmosDBInputAttribute cosmosAttribute) => targetType switch
-        {
-            Type _ when targetType == typeof(CosmosClient) => CreateCosmosClient<CosmosClient>(cosmosAttribute),
-            Type _ when targetType == typeof(Database) => CreateCosmosClient<Database>(cosmosAttribute),
-            Type _ when targetType == typeof(Container) => CreateCosmosClient<Container>(cosmosAttribute),
-            _ => await CreateTargetObjectAsync(targetType, cosmosAttribute)
-        };
+        private async Task<object> ToTargetTypeAsync(Type targetType, CosmosDBInputAttribute cosmosAttribute)
+            => targetType switch
+            {
+                Type _ when targetType == typeof(CosmosClient) => CreateCosmosClient<CosmosClient>(cosmosAttribute),
+                Type _ when targetType == typeof(Database) => CreateCosmosClient<Database>(cosmosAttribute),
+                Type _ when targetType == typeof(Container) => CreateCosmosClient<Container>(cosmosAttribute),
+                _ => await CreateTargetObjectAsync(targetType, cosmosAttribute)
+            };
 
         private async Task<object> CreateTargetObjectAsync(Type targetType, CosmosDBInputAttribute cosmosAttribute)
         {
-            if (CreateCosmosClient<Container>(cosmosAttribute) is not Container container)
+            if (CreateCosmosClient<Container>(cosmosAttribute, out ObjectSerializer serializer)
+                is not Container container)
             {
-                throw new InvalidOperationException($"Unable to create Cosmos container client for '{cosmosAttribute.ContainerName}'.");
+                throw new InvalidOperationException(
+                    $"Unable to create Cosmos container client for '{cosmosAttribute.ContainerName}'.");
             }
 
             if (targetType.IsCollectionType())
             {
                 return await ParameterBinder.BindCollectionAsync(
-                    elementType => GetDocumentsAsync(container, cosmosAttribute, elementType), targetType);
+                    elementType => GetDocumentsAsync(container, serializer, cosmosAttribute, elementType), targetType);
             }
             else
             {
-                return await CreatePocoAsync(container, cosmosAttribute, targetType);
+                return await CreatePocoAsync(container, serializer, cosmosAttribute, targetType);
             }
         }
 
-        private async Task<object> CreatePocoAsync(Container container, CosmosDBInputAttribute cosmosAttribute, Type targetType)
+        private async Task<object> CreatePocoAsync(
+            Container container,
+            ObjectSerializer serializer,
+            CosmosDBInputAttribute cosmosAttribute,
+            Type targetType)
         {
             if (string.IsNullOrEmpty(cosmosAttribute.Id) || string.IsNullOrEmpty(cosmosAttribute.PartitionKey))
             {
-                throw new InvalidOperationException("The 'Id' and 'PartitionKey' properties of a CosmosDB single-item input binding cannot be null or empty.");
+                throw new InvalidOperationException(
+                    "The 'Id' and 'PartitionKey' properties of a CosmosDB single-item input binding cannot be null or empty.");
             }
 
-            ResponseMessage item = await container.ReadItemStreamAsync(cosmosAttribute.Id, new PartitionKey(cosmosAttribute.PartitionKey));
+            using ResponseMessage item = await container.ReadItemStreamAsync(
+                cosmosAttribute.Id, new PartitionKey(cosmosAttribute.PartitionKey));
             item.EnsureSuccessStatusCode();
-            return (await JsonSerializer.DeserializeAsync(item.Content, targetType, _serializerOptions))!;
+
+            return (await serializer.DeserializeAsync(item.Content, targetType, default))!;
         }
 
-        private async IAsyncEnumerable<object> GetDocumentsAsync(Container container, CosmosDBInputAttribute cosmosAttribute, Type elementType)
+        private async IAsyncEnumerable<object> GetDocumentsAsync(
+            Container container,
+            ObjectSerializer serializer,
+            CosmosDBInputAttribute cosmosAttribute,
+            Type elementType)
         {
             await foreach (var stream in GetDocumentsStreamAsync(container, cosmosAttribute))
             {
-                // Cosmos returns a stream of JSON which represents a paged response. The contents are in a property called "Documents".
-                // Deserializing into CosmosStream<T> will extract these documents.
+                // Cosmos returns a stream of JSON which represents a paged response. The contents are in a
+                // property called "Documents". Deserializing into CosmosStream<T> will extract these documents.
                 Type target = typeof(CosmosStream<>).MakeGenericType(elementType);
-                CosmosStream page = (CosmosStream)(await JsonSerializer.DeserializeAsync(stream!, target, _serializerOptions))!;
+                CosmosStream page = (CosmosStream)(await serializer.DeserializeAsync(stream!, target, default))!;
                 foreach (var item in page.GetDocuments())
                 {
                     yield return item;
@@ -136,7 +150,8 @@ namespace Microsoft.Azure.Functions.Worker
             }
         }
 
-        private async IAsyncEnumerable<Stream> GetDocumentsStreamAsync(Container container, CosmosDBInputAttribute cosmosAttribute)
+        private async IAsyncEnumerable<Stream> GetDocumentsStreamAsync(
+            Container container, CosmosDBInputAttribute cosmosAttribute)
         {
             QueryDefinition queryDefinition = null!;
             if (!string.IsNullOrEmpty(cosmosAttribute.SqlQuery))
@@ -158,8 +173,10 @@ namespace Microsoft.Azure.Functions.Worker
                 queryRequestOptions = new() { PartitionKey = partitionKey };
             }
 
-            using FeedIterator iterator = container.GetItemQueryStreamIterator(queryDefinition: queryDefinition, requestOptions: queryRequestOptions)
-                ?? throw new InvalidOperationException($"Unable to retrieve documents for container '{container.Id}'.");
+            using FeedIterator iterator = container.GetItemQueryStreamIterator(
+                queryDefinition: queryDefinition, requestOptions: queryRequestOptions)
+                ?? throw new InvalidOperationException(
+                    $"Unable to retrieve documents for container '{container.Id}'.");
 
             while (iterator.HasMoreResults)
             {
@@ -170,23 +187,28 @@ namespace Microsoft.Azure.Functions.Worker
         }
 
         private T CreateCosmosClient<T>(CosmosDBInputAttribute cosmosAttribute)
+            => CreateCosmosClient<T>(cosmosAttribute, out _);
+
+        private T CreateCosmosClient<T>(CosmosDBInputAttribute cosmosAttribute, out ObjectSerializer serializer)
         {
             if (cosmosAttribute is null)
             {
                 throw new ArgumentNullException(nameof(cosmosAttribute));
             }
 
-            var cosmosDBOptions = _cosmosOptions.Get(cosmosAttribute.Connection);
+            CosmosDBBindingOptions cosmosDBOptions = _cosmosOptions.Get(cosmosAttribute.Connection);
             CosmosClient cosmosClient = cosmosDBOptions.GetClient(cosmosAttribute.PreferredLocations!);
 
             Type targetType = typeof(T);
             object cosmosReference = targetType switch
             {
                 Type _ when targetType == typeof(Database) => cosmosClient.GetDatabase(cosmosAttribute.DatabaseName),
-                Type _ when targetType == typeof(Container) => cosmosClient.GetContainer(cosmosAttribute.DatabaseName, cosmosAttribute.ContainerName),
+                Type _ when targetType == typeof(Container) => cosmosClient.GetContainer(
+                    cosmosAttribute.DatabaseName, cosmosAttribute.ContainerName),
                 _ => cosmosClient
             };
 
+            serializer = cosmosDBOptions.Serializer;
             return (T)cosmosReference;
         }
 

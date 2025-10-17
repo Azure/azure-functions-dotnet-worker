96f371aaf88b0f3872360d173835988d9013d019
Jacob Viau <javia@microsoft.com>
2025-10-17 12:01:34 -0700

    Enable IFunctionExecutor in InvocationFeatures (#3200)
    * Enable IFunctionExecutor in InvocationFeatures
    
    * Update release_notes.md
    
    * Fix test & warnings

--- src/DotNetWorker.Core/Pipeline/FunctionExecutionMiddleware.cs
+++ src/DotNetWorker.Core/Pipeline/FunctionExecutionMiddleware.cs
@@ -1,22 +1,24 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker.Invocation;
 
 namespace Microsoft.Azure.Functions.Worker.Pipeline
 {
-    internal class FunctionExecutionMiddleware
+    internal class FunctionExecutionMiddleware(IFunctionExecutor functionExecutor)
     {
-        private readonly IFunctionExecutor _functionExecutor;
-
-        public FunctionExecutionMiddleware(IFunctionExecutor functionExecutor)
-        {
-            _functionExecutor = functionExecutor;
-        }
+        private readonly IFunctionExecutor _functionExecutor = functionExecutor
+            ?? throw new ArgumentNullException(nameof(functionExecutor));
 
         public Task Invoke(FunctionContext context)
         {
+            if (context.Features.Get<IFunctionExecutor>() is { } executor)
+            {
+                return executor.ExecuteAsync(context).AsTask();
+            }
+
             return _functionExecutor.ExecuteAsync(context).AsTask();
         }
     }

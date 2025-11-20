e8da260760b24b00b496f9ca18d6ac0f493b0b88
Jacob Viau <javia@microsoft.com>
2025-11-20 14:21:54 -0800

    [MSBUILD SDK] Migrate SDK to C#14 extension syntax (#3262)
    * Migrate SDK to C#14 extension syntax
    
    * Remove unused usings
    
    * Use Property extension
    
    * Address copilot review

--- test/Azure.Functions.Sdk.Tests/ProjectCreatorExtensions.cs
+++ test/Azure.Functions.Sdk.Tests/ProjectCreatorExtensions.cs
@@ -9,7 +9,7 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.Build.Utilities.ProjectCreation;
 
-internal static class MSBuildExtensions
+internal static class ProjectCreatorExtensions
 {
     private static readonly ImmutableDictionary<string, string> DefaultGlobalProperties =
         ImmutableDictionary.CreateRange(
@@ -20,87 +20,6 @@ internal static class MSBuildExtensions
             KeyValuePair.Create("RestoreSources", "https://api.nuget.org/v3/index.json" )
         ]);
 
-    public static ProjectCreator AzureFunctionsProject(
-        this ProjectCreatorTemplates _,
-        string? path = null,
-        string targetFramework = "net8.0",
-        ProjectCollection? projectCollection = null,
-        IDictionary<string, string>? globalProperties = null,
-        Action<ProjectCreator>? configure = null)
-    {
-        return ProjectCreator.Create(
-            path: path,
-            projectCollection: projectCollection,
-            sdk: "Azure.Functions.Sdk/99.99.99",
-            globalProperties: GetGlobalProperties(globalProperties))
-            .PropertyGroup()
-            .Property("TargetFramework", targetFramework)
-            .CustomAction(configure);
-    }
-
-    public static ProjectCreator NetCoreProject(
-        this ProjectCreatorTemplates _,
-        string? path = null,
-        string targetFramework = "net8.0",
-        ProjectCollection? projectCollection = null,
-        IDictionary<string, string>? globalProperties = null,
-        Action<ProjectCreator>? configure = null)
-    {
-        return ProjectCreator.Templates.SdkCsproj(
-            path: path,
-            targetFramework: targetFramework,
-            projectCreator: configure,
-            projectCollection: projectCollection,
-            globalProperties: GetGlobalProperties(globalProperties));
-    }
-
-    public static ProjectCreator ItemPackageReference(
-        this ProjectCreator project, NugetPackage package)
-    {
-        return project.ItemPackageReference(package.Name, package.Version);
-    }
-
-    public static ProjectCreator WriteSourceFile(
-        this ProjectCreator project, string filePath, string text)
-        => WriteSourceFile(project, filePath, SourceText.From(text));
-
-    public static ProjectCreator WriteSourceFile(
-        this ProjectCreator project, string filePath, SourceText text)
-    {
-        string path = Path.IsPathRooted(filePath)
-            ? filePath
-            : Path.Combine(Path.GetDirectoryName(project.RootElement.FullPath)!, filePath);
-        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
-        File.WriteAllText(path, text.ToString());
-        return project;
-    }
-
-    public static BuildOutput Restore(this ProjectCreator project) => project.Restore(out _);
-
-    public static BuildOutput Restore(this ProjectCreator project, out TargetOutputs targetOutputs)
-    {
-        project.TryRestore(out _, out BuildOutput output, out IDictionary<string, TargetResult>? targetResults);
-        targetOutputs = TargetOutputs.Create(targetResults);
-        return output;
-    }
-
-    public static BuildOutput Build(this ProjectCreator project, bool restore = false, IDictionary<string, string>? globalProperties = null)
-    {
-        project.TryBuild(restore, globalProperties, out _, out BuildOutput output);
-        return output;
-    }
-
-    public static TargetResult? RunTarget(this ProjectCreator project, string targetName, IDictionary<string, string>? globalProperties = null)
-    {
-        project.TryBuild(targetName, globalProperties, out _, out _, out IDictionary<string, TargetResult>? targetResults);
-        if (targetResults is null || !targetResults.TryGetValue(targetName, out TargetResult? result))
-        {
-            return null;
-        }
-
-        return result;
-    }
-
     private static ImmutableDictionary<string, string> GetGlobalProperties(IDictionary<string, string>? overrides)
     {
         if (overrides is null || overrides.Count == 0)
@@ -109,5 +28,93 @@ internal static class MSBuildExtensions
         }
 
         return DefaultGlobalProperties.SetItems(overrides);
+    }
+
+    extension(ProjectCreatorTemplates _)
+    {
+        public ProjectCreator AzureFunctionsProject(
+            string? path = null,
+            string targetFramework = "net8.0",
+            ProjectCollection? projectCollection = null,
+            IDictionary<string, string>? globalProperties = null,
+            Action<ProjectCreator>? configure = null)
+        {
+            return ProjectCreator.Create(
+                path: path,
+                projectCollection: projectCollection,
+                sdk: "Azure.Functions.Sdk/99.99.99",
+                globalProperties: GetGlobalProperties(globalProperties))
+                .PropertyGroup()
+                .Property("TargetFramework", targetFramework)
+                .CustomAction(configure);
+        }
+
+        public ProjectCreator NetCoreProject(
+            string? path = null,
+            string targetFramework = "net8.0",
+            ProjectCollection? projectCollection = null,
+            IDictionary<string, string>? globalProperties = null,
+            Action<ProjectCreator>? configure = null)
+        {
+            return ProjectCreator.Templates.SdkCsproj(
+                path: path,
+                targetFramework: targetFramework,
+                projectCreator: configure,
+                projectCollection: projectCollection,
+                globalProperties: GetGlobalProperties(globalProperties));
+        }
+    }
+
+    extension(ProjectCreator project)
+    {
+        public ProjectCreator ItemPackageReference(NugetPackage package)
+        {
+            return project.ItemPackageReference(package.Name, package.Version);
+        }
+
+        public ProjectCreator WriteSourceFile(string filePath, string text)
+        {
+            return project.WriteSourceFile(filePath, SourceText.From(text));
+        }
+
+        public ProjectCreator WriteSourceFile(string filePath, SourceText text)
+        {
+            string path = Path.IsPathRooted(filePath)
+                ? filePath
+                : Path.Combine(Path.GetDirectoryName(project.RootElement.FullPath)!, filePath);
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+            File.WriteAllText(path, text.ToString());
+            return project;
+        }
+
+        public BuildOutput Restore()
+        {
+            return project.Restore(out _);
+        }
+
+        public BuildOutput Restore(out TargetOutputs targetOutputs)
+        {
+            project.TryRestore(out _, out BuildOutput output, out IDictionary<string, TargetResult>? targetResults);
+            targetOutputs = TargetOutputs.Create(targetResults);
+            return output;
+        }
+
+        public BuildOutput Build(bool restore = false, IDictionary<string, string>? globalProperties = null)
+        {
+            project.TryBuild(restore, globalProperties, out _, out BuildOutput output);
+            return output;
+        }
+
+        public TargetResult? RunTarget(string targetName, IDictionary<string, string>? globalProperties = null)
+        {
+            project.TryBuild(
+                targetName, globalProperties, out _, out _, out IDictionary<string, TargetResult>? targetResults);
+            if (targetResults is null || !targetResults.TryGetValue(targetName, out TargetResult? result))
+            {
+                return null;
+            }
+
+            return result;
+        }
     }
 }

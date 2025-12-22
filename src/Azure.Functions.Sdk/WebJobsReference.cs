0edfeffdd9586c6491ed4b1842d61e01fbb26731
Jacob Viau <javia@microsoft.com>
2025-12-22 10:54:51 -0800

    Fix extensions.json property name

--- src/Azure.Functions.Sdk/WebJobsReference.cs
+++ src/Azure.Functions.Sdk/WebJobsReference.cs
@@ -7,13 +7,13 @@ namespace Azure.Functions.Sdk;
 /// Represents an Azure Functions WebJobs reference.
 /// </summary>
 /// <param name="name">The name of the package reference.</param>
-/// <param name="version">The version of the package reference.</param>
+/// <param name="typeName">The type entry point of the package.</param>
 /// <param name="hintPath">The hint path for the package reference.</param>
-public partial class WebJobsReference(string name, string version, string hintPath)
+public partial class WebJobsReference(string name, string typeName, string hintPath)
 {
     public string Name { get; } = name ?? string.Empty;
 
-    public string Version { get; } = version ?? string.Empty;
+    public string TypeName { get; } = typeName ?? string.Empty;
 
     public string HintPath { get; } = hintPath ?? string.Empty;
 }

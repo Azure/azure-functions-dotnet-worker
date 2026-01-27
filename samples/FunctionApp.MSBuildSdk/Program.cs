e439a9a7652e32d8a8fd120c1a643dfcd2d6398c
Jacob Viau <javia@microsoft.com>
2026-01-27 13:22:51 -0800

    [MSBUILD SDK] Add sample for new msbuild sdk usage (#3313)
    * Add sample for new msbuild sdk usage
    
    * Remove async from sample
    
    * Remove unused usings
    
    * Update readme
    
    * Add publish/deploy steps

---
+++ samples/FunctionApp.MSBuildSdk/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+
+FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+builder.ConfigureFunctionsWebApplication();
+
+IHost host = builder.Build();
+
+await host.RunAsync();

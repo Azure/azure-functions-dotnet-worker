fe5c586514ac17f908367ff7d814e89b0365c116
Jacob Viau <javia@microsoft.com>
2026-02-02 08:32:28 -0800

    [MSBUILD SDK] Add target to publish function app as zip (#3312)
    * Add CreateZipFile task
    
    * Add string, extract property to static
    
    * Seal task
    
    * Remove unused package, remove duplicate test
    
    * Update test logic

--- test/Azure.Functions.Sdk.Tests/MSBuildMoqExtensions.cs
+++ test/Azure.Functions.Sdk.Tests/MSBuildMoqExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Globalization;
 using Azure.Functions.Sdk;
 using Azure.Functions.Sdk.Tasks;
 using Microsoft.Build.Framework;
@@ -24,6 +25,24 @@ internal static class MSBuildMoqExtensions
         else
         {
             mock.Verify(m => m.LogMessageEvent(MatchMessage(message, args)), Times.Once);
+        }
+    }
+
+    public static void VerifyLog(
+        this Mock<IBuildEngine> mock, LogLevel level, string message, params string[] args)
+    {
+        message = string.Format(CultureInfo.InvariantCulture, message, args);
+        if (level == LogLevel.Error)
+        {
+            mock.Verify(m => m.LogErrorEvent(MatchError(message)), Times.Once);
+        }
+        else if (level == LogLevel.Warning)
+        {
+            mock.Verify(m => m.LogWarningEvent(MatchWarning(message)), Times.Once);
+        }
+        else
+        {
+            mock.Verify(m => m.LogMessageEvent(MatchMessage(message, level)), Times.Once);
         }
     }
 
@@ -71,6 +90,57 @@ internal static class MSBuildMoqExtensions
                 && e.Importance == message.Level.ToMessageImportance()
                 && e.Code == message.Code
                 && e.HelpKeyword == message.HelpKeyword
+                && e.Subcategory == null
+                && e.LineNumber == 0
+                && e.ColumnNumber == 0
+                && e.EndLineNumber == 0
+                && e.EndColumnNumber == 0
+                && e.File == null;
+        });
+    }
+
+    private static BuildErrorEventArgs MatchError(string message)
+    {
+        return Match.Create<BuildErrorEventArgs>(e =>
+        {
+            return StringComparer.Ordinal.Equals(e.Message, message)
+                && e.Code == null
+                && e.HelpKeyword == null
+                && e.HelpLink == null
+                && e.Subcategory == null
+                && e.LineNumber == 0
+                && e.ColumnNumber == 0
+                && e.EndLineNumber == 0
+                && e.EndColumnNumber == 0
+                && e.File == null;
+        });
+    }
+
+    private static BuildWarningEventArgs MatchWarning(string message)
+    {
+        return Match.Create<BuildWarningEventArgs>(e =>
+        {
+            return StringComparer.Ordinal.Equals(e.Message, message)
+                && e.Code == null
+                && e.HelpKeyword == null
+                && e.HelpLink == null
+                && e.Subcategory == null
+                && e.LineNumber == 0
+                && e.ColumnNumber == 0
+                && e.EndLineNumber == 0
+                && e.EndColumnNumber == 0
+                && e.File == null;
+        });
+    }
+
+    private static BuildMessageEventArgs MatchMessage(string message, LogLevel level)
+    {
+        return Match.Create<BuildMessageEventArgs>(e =>
+        {
+            return StringComparer.Ordinal.Equals(e.Message, message)
+                && e.Importance == level.ToMessageImportance()
+                && e.Code == null
+                && e.HelpKeyword == null
                 && e.Subcategory == null
                 && e.LineNumber == 0
                 && e.ColumnNumber == 0

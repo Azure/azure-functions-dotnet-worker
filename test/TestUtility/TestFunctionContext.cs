5fc57c75ced744d5aeb650f62a27a43d32f14193
satvu <sarahvu@microsoft.com>
2026-02-09 12:29:02 -0800

    fix tests

--- test/TestUtility/TestFunctionContext.cs
+++ test/TestUtility/TestFunctionContext.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -86,7 +86,7 @@ namespace Microsoft.Azure.Functions.Worker.Tests
 
         public override FunctionDefinition FunctionDefinition { get; }
 
-        public override IDictionary<object, object> Items { get; set; }
+        public override IDictionary<object, object> Items { get; set; } = new Dictionary<object, object>();
 
         public override IInvocationFeatures Features { get; } = new InvocationFeatures(Enumerable.Empty<IInvocationFeatureProvider>());
 

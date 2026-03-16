87edf372bb5daa9df66fc5f0c36f1e18c33bb6b7
Martin Oehlert <453360+MO2k4@users.noreply.github.com>
2026-03-16 21:26:30 +0100

    feat: add Action overloads to ConfigureFunctionsWebApplication for FunctionsApplicationBuilder
    Adds Action<IFunctionsWorkerApplicationBuilder> and Action<HostBuilderContext, IFunctionsWorkerApplicationBuilder>
    overloads to FunctionsApplicationBuilderAspNetCoreExtensions, mirroring the existing IHostBuilder API.
    
    Fixes #3052

--- extensions/Worker.Extensions.Http.AspNetCore/src/FunctionsApplicationBuilderAspNetCoreExtensions.cs
+++ extensions/Worker.Extensions.Http.AspNetCore/src/FunctionsApplicationBuilderAspNetCoreExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Azure.Functions.Worker.Builder;
@@ -18,6 +19,32 @@ public static class FunctionsApplicationBuilderAspNetCoreExtensions
     public static FunctionsApplicationBuilder ConfigureFunctionsWebApplication(this FunctionsApplicationBuilder builder)
     {
         builder.HostBuilder.ConfigureFunctionsWebApplication();
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures the worker to use the ASP.NET Core integration, enabling advanced HTTP features.
+    /// </summary>
+    /// <param name="builder">The <see cref="FunctionsApplicationBuilder"/> to configure.</param>
+    /// <param name="configureWorker">The worker configure callback.</param>
+    /// <returns>The <see cref="FunctionsApplicationBuilder"/> for chaining.</returns>
+    public static FunctionsApplicationBuilder ConfigureFunctionsWebApplication(this FunctionsApplicationBuilder builder, Action<IFunctionsWorkerApplicationBuilder> configureWorker)
+    {
+        ArgumentNullException.ThrowIfNull(configureWorker);
+        builder.HostBuilder.ConfigureFunctionsWebApplication(configureWorker);
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures the worker to use the ASP.NET Core integration, enabling advanced HTTP features.
+    /// </summary>
+    /// <param name="builder">The <see cref="FunctionsApplicationBuilder"/> to configure.</param>
+    /// <param name="configureWorker">The worker configure callback receiving the host builder context.</param>
+    /// <returns>The <see cref="FunctionsApplicationBuilder"/> for chaining.</returns>
+    public static FunctionsApplicationBuilder ConfigureFunctionsWebApplication(this FunctionsApplicationBuilder builder, Action<HostBuilderContext, IFunctionsWorkerApplicationBuilder> configureWorker)
+    {
+        ArgumentNullException.ThrowIfNull(configureWorker);
+        builder.HostBuilder.ConfigureFunctionsWebApplication(configureWorker);
         return builder;
     }
 }
---
+++ test/extensions/Worker.Extensions.Http.AspNetCore.Tests/FunctionsApplicationBuilderAspNetCoreExtensionsTests.cs
@@ -0,0 +1,84 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore;
+using Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore.AspNetMiddleware;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Worker.Extensions.Http.AspNetCore.Tests
+{
+    public class FunctionsApplicationBuilderAspNetCoreExtensionsTests
+    {
+        [Fact]
+        public void ConfigureFunctionsWebApplication_WithAction_RegistersMiddlewareAndAspNetCoreServices()
+        {
+            var builder = FunctionsApplication.CreateBuilder([]);
+            builder.ConfigureFunctionsWebApplication(b => b.UseMiddleware<TestMiddleware>());
+            var host = builder.Build();
+
+            VerifyRegistrationOfCustomMiddleware(host);
+            VerifyRegistrationOfAspNetCoreIntegrationServices(host);
+        }
+
+        [Fact]
+        public void ConfigureFunctionsWebApplication_WithContextAction_ReceivesContextAndRegistersAspNetCoreServices()
+        {
+            HostBuilderContext? capturedContext = null;
+
+            var builder = FunctionsApplication.CreateBuilder([]);
+            builder.ConfigureFunctionsWebApplication((ctx, _) => capturedContext = ctx);
+            var host = builder.Build();
+
+            Assert.NotNull(capturedContext);
+            VerifyRegistrationOfAspNetCoreIntegrationServices(host);
+        }
+
+        [Fact]
+        public void ConfigureFunctionsWebApplication_WithActionCalledTwice_AspNetCoreServicesRegisteredOnce()
+        {
+            var callbackCount = 0;
+            var builder = FunctionsApplication.CreateBuilder([]);
+            builder.ConfigureFunctionsWebApplication(_ => { callbackCount++; });
+            builder.ConfigureFunctionsWebApplication(_ => { callbackCount++; });
+
+            // Check descriptor counts before Build() to catch duplicate registrations
+            var endpointDataSources = builder.Services.Where(d => d.ServiceType == typeof(FunctionsEndpointDataSource)).ToList();
+            var httpProxyingMiddlewares = builder.Services.Where(d => d.ServiceType == typeof(FunctionsHttpProxyingMiddleware)).ToList();
+            var coordinators = builder.Services.Where(d => d.ServiceType == typeof(IHttpCoordinator)).ToList();
+            Assert.Single(endpointDataSources);
+            Assert.Single(httpProxyingMiddlewares);
+            Assert.Single(coordinators);
+
+            // Also verify end-to-end resolution works
+            var host = builder.Build();
+            VerifyRegistrationOfAspNetCoreIntegrationServices(host);
+            Assert.Equal(2, callbackCount);
+        }
+
+        private static void VerifyRegistrationOfCustomMiddleware(IHost host)
+        {
+            Assert.NotNull(host.Services.GetService<TestMiddleware>());
+        }
+
+        private static void VerifyRegistrationOfAspNetCoreIntegrationServices(IHost host)
+        {
+            Assert.NotNull(host.Services.GetService<FunctionsEndpointDataSource>());
+            Assert.NotNull(host.Services.GetService<FunctionsHttpProxyingMiddleware>());
+            var httpCoordinator = host.Services.GetService<IHttpCoordinator>();
+            Assert.NotNull(httpCoordinator);
+            Assert.IsType<DefaultHttpCoordinator>(httpCoordinator);
+        }
+
+        private class TestMiddleware : IFunctionsWorkerMiddleware
+        {
+            public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}

d8b0e7b450e77195f24fa38c0434787cf5ce8368
Tsuyoshi Ushio <ushio@simplearchitect.com>
2026-08-26 19:35:37 -0700

    fix: repair Kafka attribute tests

--- test/extensions/Worker.Extensions.Tests/Kafka/KafkaAttributeTests.cs
+++ test/extensions/Worker.Extensions.Tests/Kafka/KafkaAttributeTests.cs
@@ -1,8 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-    }
-}
+using Xunit;
+
 namespace Microsoft.Azure.Functions.Worker.Extensions.Tests.Kafka
 {
     public class KafkaAttributeTests
